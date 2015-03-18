e2c63c530ebdae843f190688437e6d9c3d1fe9da
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:14:08 -0400

    Create The_True_Void.pl

---
+++ Role_playing/Locations/The_True_Void.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+There is a place where all universes meet, a place where powers contest with each other for ownership of places such as cities, countries, worlds, and universes. They also contest for people, races, and gods. These contests can cause cities to prosper, worlds to be destroyed, or genocide. The contests may last a single moment of time or span eons. One could easily destroy a soul just to have it remade by another. A world could be created just to be destroyed. These powers sometimes cause rifts to form from that gray void in which they live into the unsuspecting universes, and they can bridge between universes.
+One universe is traversed by the use of magic between crystal spheres. Another can only be traversed through a vacuum of space. Others have vastly different rules. There could be a universe that is just one world that is so large that it can never be united, or a universe that is always light or always dark.
+The powers vie for such places. Currently, there are powers that are contesting for the ownership of the universe where magic is plentiful and prevalent, though the magic of that universe means nothing these beings. They have caused many worlds to die, and some other worlds are on the brink of destruction, and others are blithely unaware of their imminent peril. Even the gods in this universe are at a loss to explain it. Most of those gods are too weak to even see what is going on, and even the most powerful ones, considered overpowers, only notice the effects but can not see the cause.
+All powers contest, but some espouse the idea that the universes need protection from those that would use and destroy them. Some even go so far as to take over a person or many people in a universe to stop a competitor from taking it over.
+There is one universe that is considered a backwater place that isn't worth contesting over. On one world the people there are confused about their spirituality, "Is there a God?" is frequently asked. The laws created for that place make traveling through it dull, so dull that the powers would have to change the rules just to make it interesting, but why bother? The power that made this place won't even let its existence be known unless it is really upset, and then it makes a woman pregnent or sends its minions or visions. It has even threatened to destroy the mortals one day. Why go to a place that will be destroyed by the power that created it anyway? That power jealously guards his little universe against intrusion.  The other powers laugh at it since they find the place boring anyway. It was much better before the mortal pests were created. There was a nice little war between the powers, the main power that created the universe and one of its minions. That brought in a few allies of each power, but the minion wasn't powerful enough to get much support. The former minion now subverts the creator's original plan whenever possible.

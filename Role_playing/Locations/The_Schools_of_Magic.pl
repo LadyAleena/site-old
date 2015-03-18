8b5e5566cd527983b666f8cd2906f1357a5671d1
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:13:39 -0400

    Create The_Schools_of_Magic.pl

---
+++ Role_playing/Locations/The_Schools_of_Magic.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+The school of magic in Olakeen is a vast organization lead by Archsorceress Alexana Olara while the school in Willowdale is very small, but growing, with only one master, one journeyman, and three to four apprentices in residence lead by the high priest of Drianna StrongOak-Guard. Baroness Aegyn Valchae teaches a few apprentices in her keep in Valachan (formerly in Ravenloft). Awnday Ar'iemay floats between the schools when she is not adventuring. Lady of Mystery Hylene Serill is the religious advisor to the schools.
+2 Applicants
+Applicants will be put through many trials, like aptitude tests, to determine if the applicants can learn magic. Instructors will try to determine if the applicants really want to become wizards, is willing to put up with the limitations, and if there might be anything that can help the teachers set up a curriculum such as do the applicants want to specialize, are there any religious conflicts, and the like. The fee is 500 gp every half year with room and board, 250 gp without it. Magic items the students bring with them are placed in a vault for safe keeping. No apprentice is allowed magic items while they are training in the school. Applicants who are clerics must obtain written permission from a high priest of their church before entering into the school.
+2 Apprentices
+The curriculum is different for each new Apprentice. They start out under the tutelage of Journeymen. As they progress the apprentice will have the chance to learn other skills. 
+Fledgling mages wishing to specialize in one field of magic will be assigned to a Master. Each Master has his or her own testing for apprentices. Masters can reject apprentices if they do not meet the Master's requirements. Masters will usually find the specialty which best suit apprentices and send the apprentices to the correct Masters or recommend apprentices stay generalized and send them to a general Master. Some Masters do not let misguided or misplaced Apprentices leave empty handed. Some Masters have been known to give apprentices scrolls from the specialty. Some apprentices can add the spells to their spellbooks. 
+The Masters are extremely different. They come from different cultures or just use magic differently. The ones from different cultures usually teach about their type of magic but not spells. They also teach non-magical lore known only in their area of expertise or region of the world. 
+The apprentices also learn other non-magic related skills as well. These skills are learned either through their Master or someone who is assigned by the Master. The Guilds of Olakeen are usually called on to perform this service as part of their contract with the Olara sisters.
+Apprentices are not kept in the school for the entire time of their apprenticeship. Some Masters might feel it prudent for apprentices to acquire practical experience using their knowledge of magic. Masters are not able to keep students who wish to adventure as well. Apprentices who choose to adventure can return to complete their training or gain additional training if they desire, but the cost is the same. The same Master will be assigned to the adventurers even if they are now Journeymen. Wizards who return as Journeymen can gain more training for a reduced fee or for free upon signing a teaching agreement. Master Apprentices can teach the basics under the strict watch of current Masters only.
+In case of emergency, apprentices are expected to use the magical knowledge gained to help in the defense of either Olakeen or Willowdale.
+3 Multiclassed Apprentices
+The Masters are prepared for the uncommon Apprentices who wish to multiclass. The Appretices' Masters will work with the senior clergy of the churches to change the current curriculum for Apprentices who are called to holy orders. Apprentices who wish to be warriors will be sent to the Olakeen miliary if in Olakeen or Brax StrongOak or the SilverLeaf Clan if in Willowdale. Rogue apprentices will have to learn those skills virtually on their own. Apprentices with psionic talents will be helped to find psionic Masters to train them. Other non-magical skills are taught either by one of the teachers or independent study.
+2 Teachers
+Teachers are Journeymen or Masters though in rare instances Master Apprentices will teach. They are thoroughly screened and have extensive contracts. Journeymen and Masters are not allowed to put the apprentices into any harm except through the apprentices' own negligence. The teachers are usually contracted only for a year but can renew their contracts to stay longer if they wish. 
+Teachers set their own hours and schedules. They can also set strict or lenient standards, but those have to be approved by Arch-Sorceress Alexana or the Board of Masters when she is absent. As noted previously Masters can reject any apprentice they feel is unfit for his or her instruction and can dismiss an apprentice for nearly any reason. The apprentice can appeal a Master's decision to the Board of Masters.
+Few teachers live on the campus, but most have homes in the city. Teachers are on call for the defense of the city when the military is out on maneuvers or at war. The Board of Masters is in charge of the school when Arch-Sorceress Alexana is absent. They will be in charge of mounting a magical defense with the Overgolds and High Priests of the Garden if the city is attacked while the military is away. Usually there are at least two companies from the military left to defend the city. The Board of Masters, with the others, will report to the Captains of those companies. 
+Some teachers, mostly generalists and invokers, volunteer to fight in the Arena in magical combat. Most do not since they do not wish to kill in this manner. Other teachers, mostly generalists and diviners, volunteer to help in the trials of the criminals.
+2 The Library
+The Library in Olakeen is free to the students of the school. There is a 5 gp fee per day for the residents of Olakeen, and a 10 gp fee per day for visitors. This fee can be lessened or waved for one day if the person adds one work to the Library it does not currently own or if the book is in high demand. There are currently 10,000 books in the Library.
+There is a special section of the Library for apprentices and teachers use only, which is where all the spellbooks can be found. Apprentices, Journeymen, and Masters can leave copies of their spellbooks here, and they will be handled with the utmost care, to preserve the knowledge therein. Many spells in these spellbooks will not vary from one to the next, but the spells in each can show the history of the apprentice as they grew in magical knowledge. Apprentices who have gone out to adventure are urged to return to add to their spellbooks. In this way, a mage will never loose his or her spellbook entirely. If a spellbook is willed to the School, the original is put into a special vault. To get into that vault, it will cost 100 gp per day.
+There is also a section which contains the Books of Magic. These special books were the gifts from Waukeen to Arch-Sorceress Alexana. To enter this area it will cost 500 gp per day. 
+The rest of the Library is devoted to mundane research. People can use this area without restraint. They can copy as much as they want and stay until the Library closes. 
+One duty of the on-duty Librarians is to make sure that the researchers do not starve, so they make sure that at least once that day the researchers leave to eat. A meal can be bought from the School's dining hall, for 2 sp. Students who eat in the Library are charged only 5 cp, since they paid for their meals with their entrance fees.
+2 Ranks
+The rank system is only applicable in the schools of magic in Olakeen and Willowdale. Aegyn, the Baroness of Valachan, and Awnday Ar'iemay also use this system since they are connected to the School of Magic in Willowdale.
+* Apprentice (1st-3rd Level)
+* Senior Apprentice (4th Level)
+* Junior Journeyman (5th-8th	 Level)
+* Journeyman (9th-12th Level)
+* Master Journeyman (13th Level)
+* Junior Master (14th-17th Level)
+* Master (18th-21st Level , Arch is usually added to the Wizard title at this point.)
+* Senior Master (22nd-38th Level)
+* Grand Master (39th +	Level)

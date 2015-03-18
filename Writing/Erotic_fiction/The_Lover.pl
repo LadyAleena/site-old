1d0b029758c451935a8c4b9ce5cfd87f45b0661a
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:15:11 -0400

    Create The_Lover.pl

---
+++ Writing/Erotic_fiction/The_Lover.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+The phone rang. I ran to answer it. The person was my lover calling to say he would be down in a few minutes. I became very excited. I had a special surprise in store for him today.
+I was wearing the black lace shirt he liked; I knew he became aroused just by seeing it on me, he probably even thinks about it when he is alone. I was not wearing a bra underneath of it. I was also wearing my black skirt, the one that has buttons that unbuttoned all the way up to my waist. I smiled thinking about his reaction to what I was wearing. I was already slightly aroused, just by thinking about the love making that would happen after he arrived. The plans included making him wait for it though. I was going to let him realize one of his fantasies that he had of me today, for today might be our last time.
+Like I always do, I unlocked my front door. When I knew he was coming down, and I was alone, I would let him come into my apartment as if he lived here, even if I still treated him like a guest, a very special guest. When he walked in, I was sitting on the couch, waiting for him. I smiled at him seductively, and got up and gave him a deep passionate kiss, before he could say anything, not even hello.
+"Take off your jacket."  I told him in a soft mysterious voice.
+He removed it, and handed it to me, smiling at me the entire time. He tried to speak, but I put a finger on his lips to silence him.  I placed his jacket on the chair in the corner of my living room, and went back over and surrounded him in a passionate embrace.
+"Come with me back to my bedroom" I said ever so softly, my breath warming his ear, "I think that would be the best place for us today."
+I took his hand, and led him back to my bedroom, without another word. I had told him some few days before that I felt uncomfortable making love to him in my bedroom, but this would be the last time we would use my bedroom.
+Each time he tried to make conversation, I kissed him. It effectively silenced him, which is what I wanted. I was inwardly pleased at that, and pleased that I was the only person he wanted to make love to, other than the one he loved, and not the slut I had heard about. I would never demean what transpired between us. To call it a 'fuck' is an understatement  We made love, and inwardly I knew I did love him, but in a way I could deal with, since I was with another myself.
+"Please sit at the bottom of the bed." I asked quietly.
+"Okay" he said with a shrug. That rather irritated me, but it didn't matter, I was happy he was here..
+I then sat down on the bed in front of him. I leaned sensually back into my pillows, and before he knew what was going on, I was caressing my breasts, through my shirt.
+"So that is what we are doing today."  He said, with a little surprise in his voice..
+I just smiled at him, my little naughty smile.
+I kept caressing my breasts through my shirt. I knew he wanted to touch them, and when he tried, I batted his hand away.
+"Just watch" I whispered to him. "Just watch."
+I slid my hands down from my breasts to my stomach. Then I started to unbutton my skirt. With each button, I flashed a little more of my legs at him. I knew he was getting aroused. I thrilled at that.
+I stopped unbuttoning my skirt in the middle, only up to my knees were exposed.  I went back to caressing my breasts, making my nipples hard. Their hardness could be seen through my shirt, my black lace shirt. I then removed my shirt, and laid it aside so he could see my breasts without anything blocking his view. I saw the smile on his face at that.
+I caressed my breasts some more, until they were nice, red, and pointed. After that, I ran my hands down my stomach, and over my skirt. I unbuttoned the last of the buttons, revealing that I was wearing nothing underneath.
+I touched my clitoris with one finger, and started to rub it. I stopped for a moment to get my dildo out of a compartment on my headboard. I didn't mind that it once was a cheap vibrator for it had served its purpose well. I placed it to one side for now.
+I continued to massage my clitoris, opening my lips so he could see my actions, unimpeded. I wanted him to see everything.
+"Move a little closer," I said to him. "I want to put my legs over your knees."
+He moved to where I requested, and I placed my legs over his.
+"You can touch my legs only up to my knees," I told him. "Let me do all the work."
+I then laid my head back on my pillows. I continued to message my clitoris. I moved my fingers down to my labia, tickling myself in the way I liked. I was very wet and very aroused.
+I reached for my dildo, and inserted it into my vagina. I held it there with my left hand, and massaged my clitoris with my right. I put myself into another world. I let go of my inhibitions then. I felt his jeans on my butt, and that excited me more. In the back of my mind, I hoped that his buttoned and zipped jeans weren't hurting him.
+I was very close to climax, very close. I didn't say a word, but he knew when to take over.
+He rubbed my legs down to my lips, and took the dildo out of me, and threw it onto the floor. He kissed my legs, and my thighs. I went wild. I put my hands on his head, and ran my fingers through his hair. He licked and nibbled on my clitoris, and put me onto anther plateau of pleasure.
+He stopped and kissed me up over my stomach. He took my already erect nipples into his mouth and sucked and nibbled them furiously.
+It was time. This time though, I didn't need any exterior birth control, I was already prepared for him. I moved my clitoris along his jeans. I worked my hands down around his waist, trying to unbuckle his belt. After that was done, I went to work on unbuttoning and unzipping his pants. I finished doing that, after fumbling around, and I slid them down over his hips. I could not do more while I was lying down. I could feel his erection. I toyed with his penis, until it was aroused.
+"Stand up, so I can get your pants off," I told him.
+When he stood up, his pants fell to the floor, and his underwear along with it.
+While he was standing, I took the opportunity to lick his penis. I licked the hole in the tip, and then around the ridge of the head. I licked the glandular area, and then the shaft like an ice cream cone. I opened my mouth and took the whole of it into my mouth. I sucked on it, while ever so gently rubbing my teeth along the whole of it. I was getting more and more aroused. I rubbed my breasts on his thighs. I reached around his hips and clutched his butt cheeks in my now warm hands.
+I continued to suck on his penis. I knew he was close now by the way he was breathing and holding my shoulders, squeezing them rhythmically. I stopped and looked at him with an inviting smile.
+"Now," was all I said.
+He laid me back on the bed and had his penis in me within seconds. He was in a frenzy. Animal passion raged between the two of us. Nothing could have touched us during this moment. All of our cares were banished. We were two beings of light and fire. We continued to make love to each other for a few moments, but what seemed to be an eternity, but I don't know the exact amount of time. Then my vaginal muscles contracted around his penis, my climax intense. He inhaled sharply. He became more frenzied, more intense, and more animal. My climaxes were becoming stronger and stronger. Then it came, my vaginal muscles clenched tight around him, causing the friction to become more and more intense.
+You can finish it however you want to in your mind.
+<!--Dawn M. Burge, February 3, 1996-->

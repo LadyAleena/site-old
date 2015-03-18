a7afd39833c36d1eacd25c9d1f8f9c7d11298545
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:14:48 -0400

    Create The_Assassin.pl

---
+++ Writing/Erotic_fiction/The_Assassin.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+Danielle was excited.  She rode to a nearby tavern with high spirits, knowing that the party was going to be a good one.  She smiled remembering those parties, since they always seemed to bring a certain satisfaction to her.
+Her world had gown a bit dull, and these days they were even worse than dull, they were outright boring.  Later that week she was expecting a visit from an assassin friend of hers, which was not going to be the most pleasant in the world, but she was eager to see her friend nonetheless.  She usually had her guard up, and strict codes of conduct on which she met with her friend, but since her friend hadn't been around for a while, the woman had grown lax, almost forgetful.
+Riding along, she was growing more and more excited, when out of no where, she was pulled from her horse.  Gaining her feet, she turned, sword pulled from its sheath, she looked into the night, looking for the person who had the audacity to pull a stunt like that.  Melting out of the woods, a woman dressed in red appeared.  The woman smirked, and in a blinding movement, she attacked.  There was no parry, no chance to defend from this attack.  The red cloaked woman slashed out with her sword, and hit the fallen in the groin, sending blood spraying from the shallow cuts.  Swearing, the woman got up.
+"How dare you?!" She cried out.
+"I dare, because you failed to protect yourself, and were lax on keeping your guard up, old friend.  It looks like you have been well.  We shall see each other again." came a soft whisper.
+Looking around, the red cloaked woman was gone, only the sound of a trickling stream in the distance could be heard.  Off in the far distance could be heard soft laughter, as well.
+Danielle sighed and looked down at the ruins of her party clothes.  She sighed knowing that the party would have to do without her for a while, until she healed.  She wrapped her slashed legs as best she could.  She crawled to her horse that stayed nearby, but not too close to where the fighting had been.
+Riding home, down spirited and dejected, she went over the list of things that she had to do for her master, things that would have taken only a few moments when she was in better shape, but now would take two to three times as long.  She sighed knowing that she would miss the party, and still have things to do for her master, but for herself as well.  So in pain, body and heart, she entered the house, wrapped her legs up in bandages, and laid down, thinking of the party she missed, and tried to relive the old ones in her mind.

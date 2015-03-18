ad9ba371539060efa56f534b8c9ca295be7aa93a
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:40:26 -0400

    Create The_Star_Tribe.pl

---
+++ Role_playing/Player_characters/Stories/The_Star_Tribe.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib "../../../files/lib";
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+His name was HighBranches, but his tribe eventually called him StarCrazed. He was a forest elf who didn't look at the trees but looked up into the starry night-time sky. He sat in the tree tops and marveled at the stars and their movements through the heavens. Eventually he was driven from his tribe, and he left without so much as a backwards glance. He did not travel far from his tribal lands but far enough away to be ignored by them. He did not resent them for he knew that he was somewhat different. After a while a few of his former tribemates sought him out and after a longer while, they left with him to a new area of the world.
+He kept the name of StarCrazed, and his followers took on similar names adding them to the names given to them at birth. After a few hundred years, these elves became slightly segregated from each other forming loose clans, within each Clan are Families. The clans are the Sole Star Clan, the desendents of StarCrazed; the Bright Star Clan; the North Star Clan; and the Moon Clan. The Sole Star Clan has always been in ascendance in the tribe, and it doesn't look like it will fall under any other clan soon even if the others decide to make a bid for power.
+The Bright Star Clan has the Bright Stars, the Distant Stars, the Red Stars, the Far Stars, the Traveler Stars, and many others.
+The North Star Clan has the fewest families with only the North Stars, the South Stars, the East Stars, and the West Stars.
+The Moon Clan is a new clan within the tribe. The clan was formed when another tribe fell onto hard times and asked to join the Star Tribe. The notable families within this clan are the Full Moons, the Half Moons, and the New Moons. There are a few other lesser families.
+Through their history they have changed a little for they are not as afraid of outsiders, at least outsiders who are of elven heritage. They hate the dark elves with a passion equal to that of any other elven community. It was after the death of StarCrazed when the other elves joined them. At first the tribe was wary of these elves even through they are distant kin, but over time they relented in allowing them inside tribal borders, and sometimes, one of the tribe would take a stranger into their family. Some were high elves, and there is even rumor that grey elves have come to this special tribe of forest elves and mated.
+Some of the Star Tribe leave, for a time, to look out in the world for new star positions. Some stay to live out their lives watching the stars in their own familiar night skies. Six women have left the tribe to go look at new skies. They face adventure and, hopefully, the stars will guide their steps. Each went their separate way, looking for different skies. Most have left to serve a church. A<Verago Brystar|href="../#Verago_Brystar"> left to serve Mystra. A<Oalirya Disstar|href="../#Oalirya_Disstar"> left to serve Tyr, her sister A<Myleara Disstar|href="../#Myleara_Disstar"> following behind later on to learn more of magic. A<Tandria Goldmoon|href="../#Tandria_Goldmoon"> left to serve Tempus. A<Faerinna Esstar|href="../#Faerinna_Escar">, known outside the tribe as Faerinna Escar, just wanted to get out to find her fortune. A<Lestra Newmoon|href="../#Lestra_Newmoon"> has never been in her tribal home. She does not even know she belongs nor where they are.
+One day the stars will lead them home...

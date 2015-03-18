5a459df24a47d3385231b0a39b1c364367c7585e
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:22:37 -0400

    Create Bags_and_bottles.pl

---
+++ Role_playing/Magic_items/Bags_and_bottles.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+use Base::LineMagic qw($line_magic);
+
+html( 'heading' => 'Magical containers', 'code' => sub { story(*DATA, { 'line magic' => $line_magic }) });
+
+__DATA__
+2 Always Full Container
+The Container will fill up with whatever is put in it last. If dirt was the last thing put in it, it will always be full of dirt. If a gold piece was put in it, it will always be full of gold pieces. What is put into it will be copied exactly. If the item has a flaw, every item that comes out will have the same flaw.
+The Container is very difficult to move. The base is extremely heavy and can only be lifted by three people with 16 strength or above. Not only is it heavy, it is extremely bulky. The base is 5' by 5' by 2'. The opening is on the side of the item, and the space within appears to be small.
+2 Box of Walls
+This is a Box that has 1d12+1 slats in it. When one of these slats is taken out and placed on the ground, a magical Wall will be created. There is only one of each Wall in the Box, though the Box can have some missing. The Walls that can be in the Box are:
+*| three
+* Wall of Bones (Bone)
+* Wall of Fire (Red)
+* Wall of Fog (Light Grey)
+* Wall of Force (Blue)
+* Wall of Gloom (Black)
+* Wall of Ice (White)
+* Wall of Iron (Orange)
+* Wall of Paper (Ivory)
+* Wall of Sand (Yellow)
+* Wall of Stone (Dark Grey)
+* Wall of Water (Cyan)
+* Wind Wall (Purple)
+* Prismatic Wall (Rainbow)
+When the last Wall is used and dissipates, the Box will explode causing 1d10 damage to anyone within a 10' radius.
+2 Bucket of Ugly
+by ^Gabriel Elias^
+This looks like an old, worn, and rusted bucket with the word "water" labeled on the outside and a fill line on the inside. When filled with water (and only water) to the fill line, it magically transforms the water into an "ugly potion". The water need only be applied to the chosen creature to act. The victim is transformed into an ugly version of himself in 1d4 days, roll percentile; 01 is not too bad to 100 which is hideous. The recipient does not see the effects of the magic only the opposite gender.
+When applied in minor amounts (with a rag to clean the wearer's arm) magic lasts 1d4 days. If water is dumped onto a victim, a percentile is rolled to determine body coverage. Divide percentile in half and that is how many days magical effects last. If water is consumed from the bucket, the same effects apply as if water were dumped, make a save versus magic, or it lasts twice as long. The effects do not start to wear off until after 1d4 days and transformation is complete.
+A remove curse must be cast to negate spell.
+2 Can of Wup Ass
+by ^Gabriel Elias^
+This magical item was created by the same gnome that created the Dinner Plate.
+In appearance this item is a 16 oz. can that is red in color with the picture of an ass on the label. It has magical writing on it that any one can read saying "Hungry? Open me."
+Upon opening the can with its convenient "pop-top", a whirlwind appears and an invisible attacker attacks the opener for 1d8 rounds. The Can has a THACO of 10 and 5 attacks a round, save versus spell for 1/2 damage. Causes 1d6(+12) pts of damage.
+The Can can not be re-used.
+2 Case, Frizzan’s of Darkness
+by Johnny Brant
+XP Value: 500 GP Value: 2,500
+This magical case, usable only by the Drow, protects and acts as a storage case for any and all bows, cross bows, and any other armor or weapon that does not use a sheath. It even stores up the radiation and darkness of the Underdark itself in a way that protects an item kept in the case of darkness for up to 1 hour for every two hours it is stored in this case. Otherwise, this case can hold up to 40 pounds worth of items much like that of a Bag of Holding or a Portable Hole; although nothing bad will happen should the case be placed within any dimension space of magical item.
+Thus a drow fighter could carry his drow sleep poison, 20 bolts, and twin hand crossbows to the surface easily, and they would then remain fully functional and usable even in the light of midday as long as they didn’t remain out of the case for more than 1 hour for every 2 hours they were stored in Frizzan’s case of darkness. The noble drow archmage Frizzan was renowned for his manufacture and selling of these potent items, especially to drow settlements close to slaving routes and Skullport.
+2 Living Hole
+by Johnny Brant
+This potent item is much like a portable hole. There are in fact two types of living holes, the small version (80%) and a much larger version (20%). Each door within a living hole locks as per the wizard lock spell (at the 20th-level of ability); and any occupants of the rooms can cause the doors to lock and assign a password for the lock, although the owner of the living hole can open this lock at any time. A living hole determines its owner, as the person to own and possess it for an entire year, thereafter that particular person is the owner of the living hole until another owns and possesses it for a year. All areas of the living hole also generate its own clean, fresh air at all times. A large living hole is worth double in both XP and GP value.
+When unfolded the small version of the living hole has a 4-foot square opening. This hole opens up into a 24' x 12' rectangle that leads into two smaller rooms of 12' x 8' size, each with their own door. There is also a small 6' x 3' alcove off of the main room that has a fountain with constantly flowing clean, semi-hot water behind the third door off the main room.
+When the large living hole is unfolded it opens up to a 6-foot square opening. This portal leads to a 30' x 20' rectangle that bears six doors. There is also a small 6' x 3' alcove off of the main room that has a fountain with constantly flowing clean, semi-hot water behind the third door off of the main room. Like the smaller living hole above, one door leads to a cleaning alcove that is 8' x 4' and has a fountain with constantly flowing clean, semi-hot water. Three doors lead into three smaller rooms each of 20' x 10' size and each with their own door. A study or war room complete with solid oak desk and two chairs is held behind a iron bound chime oak door and is 10' x 10' in size; shelves and a clothes rack are also located one to each side of the door. Finally the last room, an equipment room is 10' x 5' in size and bears racks adorning its four walls that shape change to hold anything that the owner and any others who enter this area wish to store here (This door is always locked and only the owner can open it without magical aid).
+2 "Magic" Water Bottle
+by ^Gabriel Elias^
+This bottle is a plastic green bottle with orange letters spelling "Gator-Aid", holds 32oz of magical water, and does NOTHING at all. It is a cursed item that makes the user (or recipient of the water) believe that complete healing has happened. It can be internally or externally applied/consumed. When the bottle is empty it is just a cool looking water skin.
+2 Portable Hiding Place
+|* Lining|Space
+|+ Iron|10'×10'
+|+ Copper|20'×20'
+|+ Silver|30'×30'
+|+ Electrum|40'×40'
+|+ Gold|50'×50'
+|+ Platinum|100'×100'
+There are many versions of these boxes. The interior size varies depending on the construction and materials used. Each has a metallic interior with a wooden exterior. The metal used for the interior is what determines the amount of space within. When the command word is spoken, a ladder appears on one of the interior sides, and people can climb down easily. Please note, these hiding places are extradimensional spaces, so Portable Holes and Bags of Holding would be destroyed if taken inside along with the box itself.
+If lead is used in the construction, the interior space can not be scryed. Also, the more ornate the construction, the larger the possibility that the hiding place is furnished. The box can be moved from place to place while people are hiding within, and upon non-magical inspection of the interior, it will look like a plain, if expensive box. Items can be placed in the interior without going into the extradimensional space, which includes Portable Holes and Bags of Holding. Some of these boxes come with Lock spells attached. Just be cautious, you never know who might be hiding in the box.
+2 Sheath of Darkness
+by Johnny Brant
+XP Value: 300 GP Value: 1,500
+This magical sheath, usable only by the Drow, protects and acts as a storage case for a sword. It even stores up the radiation and darkness of the Underdark itself in a way that protects an item kept in the sheath of darkness for up to 1 hour for every two hours it is kept sheathed. Otherwise, this sheath can hold the same type of weapon as the sheath of holding charts indicate in The Complete Fighter’s Handbook; although nothing bad will happen should the case be placed within any dimension space of magical item.
+Thus a drow fighter could carry his drow long sword +3 to the surface easily, and it would remain fully functional and usable even in the light of midday as long as it didn’t remain out of the sheath for more than 1 hour for every 2 hours they were stored in the sheath of darkness. The noble drow archmage Frizzan was renowned for his manufacture and selling of these potent items, especially to drow settlements close to slaving routes and Skullport.
+2 Transport Hole
+by Johnny Brant
+XP Value: 1,600 GP Value: 8,000
+This strange item appears to be a Portable Hole, although close examination proves that it merely creates passage or a hole through substances similar, but not exactly as per the 5th-level wizard spell Passwall; therefore anything that prevents Passwall may not stop the Transport Hole unless it also prevents all forms of such travel. Thus a person could throw up the Transport Hole and jump through a wall of solid stone and then grab it back from the side he immerged from and continue on his way.

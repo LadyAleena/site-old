ab68165cc81dc809f6998c5c787fac554a99eb41
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:39:29 -0400

    Create The_Sisters'_Dilemma.pl

---
+++ Role_playing/Player_characters/Stories/The_Sisters'_Dilemma.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib "../../../files/lib";
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+2 The Disstars' story
+A<Oalirya|href="../#Oalirya_Disstar"> and A<Myleara|href="Myleara_Disstar"> grew up under the protection of their parents. Their parents were so over protective that the girls felt strangled. When they were still adolescents, they ran away from home. They ended up in a town that was unfamiliar to them. They walked into town and got a room at a small inn. The inn wasn't the nicest place in the world, but it would do. Myleara went downstairs to get something to eat for them both. That is when Oalirya heard commotion. She ran downstairs to see what was going on only to see Myleara being lead out of the building by two rough looking human men. She was terrified.
+Oalirya followed to see where they were taking Myleara. Myleara was being put into a building with a lot of bars in the windows. She tried to go inside to see why Myleara was being put there. Myleara was accused of stealing an expensive gem from another customer of the inn. Oalirya tried to tell the men that they had just gotten their rooms, but they wouldn't listen to her.
+Oalirya was desperate for help. Myleara was not even going to be given a trial. She was going to be sentenced the next day. They were days away from home, so getting help from there was not an option. Oalirya was in the center of town, sat on a set of steps, and cried. She didn't know what to do. Someone saw her crying and came over to see what was the matter. This man was not as harsh as the others were. He was interested in her story. She was so terrified for her sister that she didn't see his armor or weapons. She did not notice that others were gathering around them, but staying a good distance away to let them talk in peace. He listened and told her he would do what he could to help. It was his duty. That is when she came to her senses and noticed she was talking to a priest of Tyr, the god she herself venerated.
+Meanwhile, Myleara was stuck in a cell. She was scared out of her wits. There was another person in the cell with her. She was dressed in some weird looking robes. Myleara kept saying "I didn't do anything." with no results. The woman sat up and said "Ah, another who understands what it means to be misunderstood." Myleara turned to look at the woman. She was human. The woman smiled and patted the seat next to her.
+"Dear, it looks like these people just don't like anyone that is different from themselves. I, too, have been arrested on a trumped up charge."
+They talked for a while, and Myleara found that this woman was a wizard. Magic always intrested her, but no one in the tribe seemed to have time to teach her. The talk of magic kept Myleara from sinking into dispair. The woman offered to teach Myleara if they got out of their incarceration alive and in one piece. Myleara was overjoyed!
+The next day dawned brightly. When Myleara was brought out for the execution of her sentece a man stepped forward and started to speak. The townspeople listened and released Myleara. The wizard was also released. Oalirya ran up to Myleara and embraced her tightly. Myleara introduced the wizard, and Oalirya introduced the priest who had gotten them freed.
+Oalirya told Myleara that she was going to go with the priest. Myleara then told Oalirya that she was going off with the wizard. They laughed. They went their seperate ways then and there. Oalirya was to become a priestess of Tyr, and Myleara was off to train as a wizard.

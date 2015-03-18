cbac38c90728e5962ff07d0f40b72e044ca99b91
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:01:14 -0400

    Create Recruiting_Day.pl

---
+++ Writing/Recruiting_Day.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+Lady Aleena stood on her dias in the main courtyard of her castle late in the night. Behind her stood five people in their formal attire which is much lighter than their normal armor, and in one case, cleaner. The Preceptor of the Order of the Sapphire Cross, the commander of her knights, stood grasping the handle of his ceremonial sword. The Preceptor of the Order of the Golden Shield, the commander of her archers, whispered orders to one of her sub-officers. The Preceptor of the Order of the Moon, the commander of her spies, always kept the hood of his cloak up to cover his face. The Preceptor of the Order of the Sun, the commander of her sentinels, scanned the crowd looking for spies never letting her guard down. The Mistress of Mines stood with her everpresent tallies in hand.
+The first group of seventy arrived to serve Aleena at midnight on the first day of every month. These were children of her people who would be staying in Aleenia. They were sworn into service and sent off with her quartermaster. Until placement into whichever service they were needed, they were housed in temporary quarters. The quartermaster usually did the initial assessment of these men and woman. The man was hardly ever satisfied with any of them.
+Aleena looked out onto the next group of people who gathered to hear her morning announcments. A little after midnight the announcements were always the placement of the children of her citizens into other armies of the realm. Three hundred and fifty of them, chosen by the lottery, would go out into the night never to return to their childhood homes. She knew that only a small percentage of them would go into the armies of friends. Most would go into the armies of those who may put them on the front lines of their various battles.
+The young men and women stood there awaiting their postings. Lady Aleena called out those who would go into the armies of her allies. There was only little relief on the faces of those who were to go and serve their Lady's allies. There was no relief on the faces of those who were being sent into the lands and into the commands of strangers. She knew some of the commanders' names from her previous sendings, but there were those who she had never met before. She even felt the irony that some of those that she sent out were to go into the armies she vanquised or of the victorious against her.
+The next group of approximately fifty youths to be sent out were those that would serve her allies only. The youths from the first group that were going to serve her allies were always held back so that the two companions would travel together. These youths were always volunteers.
+Lady Aleena stood there listening to the fathers whispering to their children to be careful and to the tears of the mothers who did not want their children to be sent. She called the assembly to order and gave the youths their orders. She wished each youth good luck and sent them onto their new homes.
+After the ceremony was over and the crowd dispersed, Lady Aleena gathered her guards to venture into the mercenary camp. It is a rough place, but men and women go there in hopes that she would hire them. Her Master-at-Arms and her Spy Master always go with her to interview the propective candidates. After the candidates are hired, they are sent off with whichever man they will serve under to begin the most intensive training.
+Aleena retired to her chambers to a restless sleep. With each dispersal of her citizens, she knew that one day father could fight son or brother against sister. That day she dreaded but knew could come eventually. When she finally fell into a more restful slumber, she still had dreams of the battles yet to come.
+br
+This story was inspired by the MMORPG A<Dark Throne|href="http://www.darkthrone.com">.

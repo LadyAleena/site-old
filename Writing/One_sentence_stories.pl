39805d6116737c05e002ee901daa214fb86af794
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:00:27 -0400

    Create One_sentence_stories.pl

---
+++ Writing/One_sentence_stories.pl
@@ -0,0 +1,143 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+After almost three and a half years, she wondered if she would have the courage to expose herself again to a stranger, yet she had to do it to keep her sanity even if she again gets nothing but praise in her e-mail inbox expressing shock she had entered only one last time; so now she reenters her last entry and adds seven more in hopes that she might see her name while her monitor bathes her face with light one dark and lonely night.
+On this ancient morning, the sun rose over the rooftops and cast the shadow of power lines through the blinds of my bedroom window causing another broken shadow to appear on the wall which framed the hole created by the bullet that will go and has gone through my grandmother's head; I hate temporal instability, but it's time to get up and watch the show again.
+The cigarette dangled between the fingers on her right hand as she pondered the question, not for the first time, and the answer was to take another drag.
+In the long distant future-past, there [will be/was] a [(not so) great] [fiefdom/empire] named Alphmega which [will be/was] founded by Janic the First and Last and [will/has] last(ed) for millenia; and grammarians [will/have] hate(d) it for the headaches it [will/has] create(d) for them.
+The victim shouldn't be screaming for the words have told her that the victim should no longer be screaming yet it persists on and on so how could the words be wrong since she learned from her mother to trust the word, and so she did, but the victim is still screaming even when the television monitors show the victim is quite dead.
+She stood on the balcony and felt good about herself and her life, while people who walked by would suddenly glance up at her, some walking away quickly while others could not look or move away; and she did not know why until she saw in her peripheral vision that her clothes were still on the bed.
+Evil Robert sees Trebor live.
+Sitting in the sofa with his stockinged feet up in the coffee table in front of the fire place, he reads a book propped on his wife's legs astride his lap when a knock on the sliding glass door leading to the back porch changed his life for a night.
+2 Stories written for Twitter
+He caressed her back & then kissed her neck. She whispered, "I'm yours!" His reply: "I don't want you anymore." - by A<nixskits2|href="https://twitter.com/#!/nixskits2">
+To surprise her lover, she slid his favorite flavor candy inside of her so he would have a tasteful experience.
+She was in so much pain that while waiting for the pain killers to kick in, she masturbated to release more endorphins.
+He was so horny that when he inserted the fuel pump into the gas spout, the slight erection he was hiding got worse.
+The actors didn't tell the crew that the carefully choreographed sex scene was about to turn very real.
+They're both geeks, so while he was enjoying her with his tongue; he used a miniature illuminated light saber inside of her.
+Hadley packed all the manuscripts and the carbons in the suitcase that was lost. - by A<stevepoling|href="https://twitter.com/#!/stevepoling">
+The assassins allowed the happy couple one last night of pleasure before killing them.
+Jack & Jill went up a hill to fetch a pail of water; Jill got wet, & in Jack went; & a baby was 9 months after.
+The lace of her bra tickled her nipples every time she moved which kept her horny for the whole evening.
+She was dressed to thrill, but the first man who looked at her had a heart attack making her dressed to kill.
+He kissed the dancer with heart shaped pasties then went back to the poker table and got a royal flush in hearts.
+Lisa stumbled into Betty, which landed Betty straddling Lisa's head who saw nothing between her and Betty; so Lisa took a little taste.
+Liz & Beth were both extremely horny; but because neither said anything, they went to sleep that way.
+It was a dark and stormy night, and that was the only excitement they got.
+As her masturbatory orgasms subsided, she looked out her window and saw a note across the way "Thanks for the show."
+On the darkest of nights, Sleeping Beauty sneaked into the evil fairy's bedroom, woke her w/a kiss, & inserted the spindle.
+Snow White fashioned a golden rod from the dwarves mining and used it to subjugate the evil queen.
+After Cinderella learned the joys of the wand from her fairy godmother, she used it on her evil step-mother & step-sisters.
+He had no idea a single line of text could affect him to the point where he had to go home from work early.
+Their passion was so cold that it took 3 days to orgasm; but when they finally felt the joy, the house caught on fire.
+Zoe & Abby didn't realize what they were doing to each other as they read their favorite erotic story.
+On the longest day of the year, Emma & Debi made love from sunrise to sunset.
+Dawn, Day, & Eve made the knight blush crimson when he happened upon their pleasures in the forest.
+The witch took every precaution, but a tornado spread her love potion across 8 counties in 3 states.
+The sentient trees were in need of human mates, so they released the pheromones & lured the people of Ashville into the woods.
+The pressure of her loneliness & depth of her boredom created a creature that kept her aroused for the rest of her life.
+Lily focused her mind on the avatar of her favorite follower and telekinetically brought the woman to orgasm.
+After finishing a bottle of wine, Betty used it to give her more pleasure until her girlfriend got home & finished the job.
+One drunken night a group of graduate science students accidentally brought magic back into the world, but the cost was high.
+It was love at first sight when Hugo, gypsy born and bred, spotted Beatrice, an Amish farm girl, as their carriages passed.
+The world of Other was in turmoil, but the Over Council took care of it just in time to stop an underground turf war.
+The rules had been broken, as she faced one of her enemies, the other took her head. #Highlander
+Hungry vampires fought against the alien invaders to protect their food supply.
+Those who were the 1 in 7 people who were immune to the pandemic virus where known as Saturday's children.
+A vampire got caught in a time distortion field & arrived as the first human evolved, but was so hungry that he ceased to exist.
+The time traveling team was lost forever in time because of an error in the decision data, a 1 was written as a 0.
+Little did she know that her life was entertainment for 2 alien civilizations, & that if she did the dishes, Earth would be spared. 
+She felt funky so she moseyed into the bathroom and took a shower while she grooved to to music on her nifty radio.
+By the end of the night, Eve and Jane had changed the batteries twice.
+Lucy and Edith ate melting kisses as they dribbled out of each other.
+With their husbands out with the kids, Beth & Lydia wiggled inside each other while watching their favorite romantic comedy. 
+Viola was a wreck, so Diane put her hand up V's skirt for a little pick-me-up while they were sitting at the bar.
+Her mind drifted while she was folding sheets and touched the mind of a telepathic alien on a distant world.
+She went to the party expecting to get laid, however, she didn't expect it to turn into an orgy.
+Bea, Dee, Kay, and Elle told Jay to go away since they were having more fun without him.
+April, May, and June spurned August to have a threesome of their own.
+She opened her cloak to hide him, and upon finding her naked underneath, he rewarded her with the best orgasm she ever had.
+His ghostly form overlapped her flesh, and she had the best orgasm in her life.
+After the zombie plague passed and the bodies healed, she gave into the temptation and screwed one of her mindless patients.
+When it became illegal to marry the undead, she presented her neck to her vampire lover and died of a broken heart.
+She wanted to be a dancing queen, however all she ended up with is a sprained ankle and a broken lamp.
+It was a night like any other until she opened her mind to the universe and was transported to another world.
+The alien marauders knew not their prey were volunteers, bioengineered to be deadly, until it was too late. - by A<neuromastic|href="https://twitter.com/#!/neuromastic">
+100s appeared to participate in a flash mob they heard about on Twitter, but it was an alien conspiracy to kill them.
+She feared for her sanity until she realized that it was just PMS.
+She found a strange ugly book on her shelf, so she threw it away without opening it saving the world from eternal grief.
+She was always disappointed that all of her showers ended without incident.
+She looked all over her house for the source of the rapping noise she was hearing, then she turned off the radio.
+She had the strangest feeling someone was behind her, though when she turned to look, no one was there.
+With a sigh he wondered what the future held, recalling all the decisions that brought him to the present. - by A<buickid|href="https://twitter.com/#!/buickid">
+Terrans were spared an alien invasion once the leader of their armada realized they were off course.
+When the motion sensor tripped the light in her backyard, & she called the cops when saw a strange person back there.
+The rain fell hard and washed away everything except his pain. - by A<neuromastic|href="https://twitter.com/#!/neuromastic">
+They partied in the spooky cabin in the woods, but all they got were hangovers.
+"I've seen Evil Dead, you idiots," she said exasperated, "you read that book & we'll all die!"
+"I'm not that desperate to lose my virginity," she told her now ex-boyfriend, after he wanted to do it in the haunted house.
+Even though there was no way she would survive the night, she fought back & made her attacker pay dearly for killing her. 
+"Ah screw it, I'm not nuts" she thought before telling her friends the weird thing she just saw saving all of their lives.
+The kids stayed together & even called the cops, so when the night was over, they all partied as the masked killer went to jail.
+After starving her for days, when he thrust his dick into her mouth, she couldn't help but to take a bite.
+Her car stalled between two new & strange street lights that teleported her to another reality.
+When she lost a bet, she had to lose her shirt because of a loose bet.
+When a loose hook finally snapped off and got lost, she decided she could lose the bra. 
+She didn't want to lose the package for a deadly virus would get loose in the world and all would be lost.
+They thought the Gravitron had just been updated to a new design, they didn't know it was a spaceship in disguise.
+The time traveler knocked on the door of his parents house and ceased to exist.
+In 6 million years, the binary of an email she sent to her mother saved a planet when it was accidentally released like a virus. 
+Hands trembling she opened the mysterious box that came in the mail & was swept up in cosmic events she hadn't known about.
+In the silence, she made a plan to destroy the world then went to make dinner.
+"Is it love or lust?" she thought when she saw the images of a couple friends she followed in her timeline.
+She tried to write an interesting one sentence story but failed utterly.
+With one click of her mouse, she resolved over 100 parallel universes.
+The greatest love in the world died when he handed her the bill.
+In the silence of the night, she tread through the house listening to the fabric of her dressing gown sliding on her skin.
+When the world shook, and the rains fell; she dreamt of being trapped in her washing machine.
+She was completely unaware that the Sandman & the Grim Reaper were doing battle over her while she was doing the dishes.
+She thought she was going crazy when every door in her house opened at the same time until the ghost appeared.
+Nine months later, she gave birth to an android after sitting on her washer during the spin cycle.
+No one felt pity when she cried for they could not see her tears.
+She took off her glasses and saw a hidden message in the haze that filled her with joy.
+A child in Idaho never knew that she talked in an alien that Christmas Eve when she called NORAD Tracks Santa. #Stargate
+She got home and lost her mind.
+She sneezed so violently that she ended up in another reality.
+The man of her dreams was never around when she needed him most.
+She lost her mind while reading tweets due to the extensive spelling and grammar errors to which she was exposed.
+She found GOD on the bottom of her soup bowl.
+No man who gave her an orgasm would love her because the local witch cursed her for being a slut.
+She opened the refrigerator door and saw a demon. (Wait! That was already done in #Ghostbusters. Darn it!)
+A new life form was created from the dust in her vacuum cleaner.
+A wormhole opened when she turned on her garbage disposal.
+The alien invasion began when she popped a zit on her forehead.
+She cut a frayed thread from her blanket and destroyed an alternate timeline.
+She went away for the night to sleep with hopes of good dreams.
+She was terrified that night when she made love to her husband because her prescription for the pill had run out.
+Nothing extraordinary happened that morning as she left her office.
+She took Déjà Vu out on a date, but she forgot it would be double.
+She took Time out on a date, and he was right on himself.
+When death came over for a visit, she was too busy on Twitter to answer the door.
+Little did the poor woman know that one of her tweets started a bar brawl on the other side of the country.
+She destroyed a mountain on a planet on the other side of the universe when she flattened a crease in the bed sheet.
+Little did the poor housewife know that waking up an hour later to do laundry one day caused trillions to be late for work.
+In a weird confluence of events, her life was saved when she took a sip of her iced tea.
+The world ended when the dryer stopped.
+She took off her glasses for only a moment but found out that she was surrounded.
+Playing music from her past transports her to the future.
+She washed the blood off of her hands with her own tears.
+After opening the dishwasher to release the steam, she opened the freezer for some ice & caused a rain storm near the floor.
+The spin cycle of the washer combined with the warm air of the dryer and the steam from the dishwasher caused an explosion.
+Her boredom was so intense that it opened a rift to another world that left her speechless.
+A night at the movies reveals a decades old conspiracy.
+She opened her mouth to scream then saw the smiley face sticker on the windshield & fainted.

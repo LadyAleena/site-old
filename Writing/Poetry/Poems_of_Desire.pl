af98ff1ed07e69e571834163663f826d185084b1
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:18:30 -0400

    Create Poems_of_Desire.pl

---
+++ Writing/Poetry/Poems_of_Desire.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+2 To all the men I have loved before
+stanza I know that we have had fun|Fun with our bodies|Fun with our senses
+stanza I know that at one time I gave|Gave you my body|Gave into the fun
+stanza I know that I said I love you|You had my body|You had my soul
+stanza I know that I left you all alone|All alone without my body|All alone without my soul
+stanza I know that you left me alone|Alone without your touch|Alone without your love
+stanza I am with someone else now|He fills my senses with passion|He fills my heart with love|He fills my body and my soul
+I won't leave him nor he me.
+2 Secret Love
+stanza My heart is misplaced|My mind acknowledges
+>6 my pain.
+stanza My body felt his touch|My soul went up
+>6 in flames.
+stanza I see his face constantly|I hear his voice inside
+>6 my head.
+stanza I feel his hands on me|I must know why I
+>6 feel dread.
+stanza Heart broken I stand.|Soul pierced I fall
+>6 broken.
+stanza Eyes full of many tears.|Mouth closed many words
+>6 spoken.
+stanza Passion wracks my body.|Love wavers in my heart
+>6 secretly.
+stanza A spoken word can kill|A part of me dies whenever
+>6 we meet.
+2 Illicit Love
+stanza I desire your touch on my skin|I want to feel you inside of me again.|I long for your caresses on my flesh|I need you to love me again.
+stanza I miss your arms around me still|I cry not, with only strength of will.|My heart grows colder with time.|My love for you I must kill.
+stanza I crossed that line not knowing.|I hide it within me not showing|I love you against the rules we set|Yet I found it inside of me growing.
+stanza Once I almost said those words.|Once I was tempted with the absurd.|I know you will never be mine.|I am entangled in love's cords.
+stanza I let my heart freedom to roam|And now it won't come back home.|I regret only one thing in this affair.|That I can't have you for my own.

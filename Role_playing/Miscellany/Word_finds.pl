b147a5b7712fd5623dff57b52a295452bca074e9
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:32:55 -0400

    Create Word_finds.pl

---
+++ Role_playing/Miscellany/Word_finds.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+use File::Basename;
+use HTML::Entities qw(encode_entities);
+
+use lib '../../files/lib';
+use Base::HTML qw(html);
+use Base::Menu qw(file_menu);
+use RolePlaying::WordFind qw(print_word_find);
+
+my %finds = (
+  'Monsters Galore' => 'HUMAN',
+  'Undead'          => 'UNDEAD SLAYER'
+);
+
+my $cgi = CGI->new;
+my $select = encode_entities($cgi->param('word find'),'<>"');
+my $head = $select && $finds{$select} ? $select : undef;
+html( 'heading' => $head, 'file menu' => file_menu('word find', [sort keys %finds], $select), 'code' => sub {
+  print_word_find($select,$finds{$select}) if ($select && $finds{$select});
+});

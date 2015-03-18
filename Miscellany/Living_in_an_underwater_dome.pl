4e54bf7aa52cd6e6d05f27a1ea103bc0df311c65
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 14:46:43 -0400

    Create Living_in_an_underwater_dome.pl

---
+++ Miscellany/Living_in_an_underwater_dome.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA); });
+
+__DATA__
+2 Oxygen
+Oxygen will be taken from the water. The creation of man made "gills" will provide the oxygen needed. Also a pipe going to the surface of the water will provide some breathable air for the inhabitants for the first few months. The air will always circulate out of the dome so that the new clean air will be circulate.
+2 Water
+Water wil be pummped into a large water purifier. The purifier will be working constantly. It wil be shut down one hour a week for cleaning. During that hour all inhabitants will be put on water saving alert at least five hours before the purifier is turned off.
+2 Electricity
+Electricity will be aquaired by floating solar panels. The panels will be on the water in a protective shell so that the energy will be safely transferred to the dome.
+2 Light
+Light will be aquried by fiber optic cables attached to the platform. Many of the cables will be connected to the dome so that there will be an illusion of sunlight.
+2 Waste
+In the dome will be recycling stations for paper, plastic, and some metals. Body waste will be passed through a waste purifier before being passed to the ocean floor. Food will not be wasted because of the limited supply of it at the start.

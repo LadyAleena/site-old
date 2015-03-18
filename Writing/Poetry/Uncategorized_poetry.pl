15160245f0d5f1edcfd2d2c9fc9608e51661827a
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:21:12 -0400

    Create Uncategorized_poetry.pl

---
+++ Writing/Poetry/Uncategorized_poetry.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+2 All
+stanza There will be no sad good-byes|None at all|I will you through the gate|Down the hall|No good-byes just love.
+2 Messed Up
+stanza My life is a mess.|I don't know what to do.|I need a love.|I want it to be you.|I want to be yours,|and you mine.|I have to say this.|Will you be my Valentine?
+2 Knights of Old
+stanza From the wall of Malakar|The knights did reign|They grew old|And never seen again.
+stanza Then one day the hero came|He brought back glory|But the kinghts of old knew|That could not be the story.
+stanza They sent him on a quest|Surly to kill him|Unbeknownst to the knights|He was a strong jyn.
+stanza The hero quested far|almost to his death|He found the sacred item|and took it to the rest.
+2 unnamed (1)
+stanza I want a good life|I need a break|But now I know|My life was a mistake|I need a good rest|In a friendly abode|But I carry around|a horrid mental load|My name is opposite|of my true life|I need to know if|I am doing it right.
+2 unnamed (2)
+stanza He walks in darkness|I walk in light.|Highs and lows are he|The middle ground is my plight.|Crooked and broken his soul|Straight and true are mine.
+2 unnamed (3)
+stanza He is the dam that holds my emotions at bay|He the one that had come and saved my day
+stanza Until the end of time that's how strong my love is|I'm on the line until that time comes in
+stanza I can't hold my love back it might bring on madness|I don't know whose it could be his or mine.
+2 unnamed (4)
+stanza I hope my guardian angel can see|the happiness between Richard and me.
+stanza I feel complete when he is around|I know my mental state is safe and sound.
+stanza When I'm laying there in his arms|My mind is still, with no cause for alarm.
+stanza The futre is clounded, opaque to me,|but I have a strong feeling it will be happy.
+2 Outcome
+stanza For every ping|there is a pong
+stanza For every right|there is a wrong
+stanza For every push|there is a pull
+stanza For every genious|there is a fool
+stanza For every mountain|there is a dale
+stanza For every breeze|there is a gale
+(unfinished)
+2 unnamed (5)
+stanza One phrase knocked me down|It stormed into my heart|I was left without|I didn't know where to start.
+stanza My heart divided|My mind racing faster|But what can be said|That won't be disaster.
+stanza I said I love you|Did I make a bad choice|I hope I did not|My hope I had to voice.
+stanza Choose now what you want|But to yourself be true|When you say the words|"I love you."
+2 unnamed (6)
+stanza My heart bleeds|behind the walls|I built tall|around my heart.
+stanza Calls of love|lay on the ground|I take it.
+stanza Inside the room|I walk staring|at the love|not sharing it.
+stanza Love sits silent|on the shelf where|it yells out|"It is you I love"
+2 unnamed (7)
+stanza Take me home!|Oh, please take me home.|I don't want to be with you anymore.
+stanza I don't care about you,|You hurt me too much|I need some time to breathe|and you won't give it.|Give me time to think|and I might come back.
+stanza Take me home!|Oh, please take me home.|I don't want to be with you anymore.
+stanza Don't follow me around,|that tells me you don't care.|You don't care that I might|break when I see you again.|Seeing you hurts don't you see|You hurt me!
+stanza So please...take me home!|Oh, please take me home.|I don't want to be with you anymore.

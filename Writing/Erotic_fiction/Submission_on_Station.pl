e7eec14627c46e297a3893fc2e3ede76720ab5cb
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:13:33 -0400

    Create Submission_on_Station.pl

---
+++ Writing/Erotic_fiction/Submission_on_Station.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+The message arrived on your personal console while you are working as an engineer in the Hydroponics Division of Vega Station 3. The message is from your "friend" who works on Vega Station 8. You discreetly read the message, since you can't tell your co-workers what you do with your downtime.
+The message reads...
+"Things for you to do before contact tonight:
+# Shave your balls bare. Make sure that the last shaving stroke is one hour before contact.
+# Put on the blue tutu.
+# Make sure that the red ballet shoes are laced properly.
+# When you make holographic contact, be holding your dick up so that I can examine your balls.
+If you fail in any of these tasks, I will be very disappointed and will hang up."
+The last line sends a chill down your spine. You can't have her hanging up on you. The last time she did, it was two agonizing weeks before she contacted you again. You were so despondent that your job suffered. If you were shipped back to Earth, the relationship would be permanently terminated, and you couldn't bear that.
+That night you do everything as ordered. When the holographic link is established, you are sitting there with your dick in your hand so that she can see your freshly shaven balls. You were so careful this time that her holo image smiled. The joy at seeing her smile made your heart want to burst from your chest.
+You automatically go to sit down in the chair when she nods to you. You were such a good boy is what her look told you. When she looked down at your feet, you froze halfway seated. The lace on the left slipper had slipped out of place. You start sending impulses through your physlink of you kissing her feet that you hope she will forgive you.
+Her slightly cross look makes you even more anxious. You want to make her happy. You send impulses of a massage through the physlink. The all over massage takes you a while, and she has not given you permission to finally seat yourself. After an exhausting hour of physlinking a massage she finally relents and lets you sit. She physlinks a light massage to your tight back muscles. You even feel a light brushing feeling on your freshly shaven balls. You know you are forgiven the transgression. In your relief you allow yourself to relax control of your impulses. So what if you will spend 20 minutes after contact is severed cleaning your cabin again? Mistress is happy, and that is all you care about right now.

647772666f2a3937b2899c44707c06b3b9d0280e
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:37:39 -0400

    Create Spellbooks.pl

---
+++ Role_playing/Player_characters/Spellbooks.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+use File::Basename;
+use HTML::Entities qw(encode_entities);
+
+use lib '../../files/lib';
+use Base::Data qw(data_directory get_directory);
+use Base::HTML qw(html);
+use Base::Menu qw(file_menu);
+use Util::Sort qw(name_sort);
+use Util::Convert qw(textify filify);
+use RolePlaying::Spellbook qw(print_spellbook);
+
+my $directory = 'Role_playing/Player_characters/spellbooks';
+my @selects = sort { name_sort(lc $a,lc $b) } map { textify($_) } get_directory(data_directory($directory));
+
+my $cgi = CGI->new;
+my $select = encode_entities($cgi->param('spellbook'),'<>"');
+my $head = $select && grep(/\Q$select\E/, @selects) ? "$select\'s spellbook" : 'My characters\' spellbooks';
+html( 'heading' => $head, 'file menu' => file_menu('spellbook', \@selects , $select), 'code' => sub {
+  print_spellbook($directory, filify($select).'.txt') if ($select && grep(/\Q$select\E/, @selects));
+});

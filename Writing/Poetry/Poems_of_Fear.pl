ac90f9cdf79e073bc89aff79deb4cfbaa2725caa
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:18:54 -0400

    Create Poems_of_Fear.pl

---
+++ Writing/Poetry/Poems_of_Fear.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+2 The Black Deed
+stanza Out of the shadows|walks a man|in a dark cloak|through the mists|Touched by madness|haunted by his past|he strides quickly|past the light.|I call to him|hold my arms open|for his embrace|to heal him.|He sees me|stalks me in the night.|He enters my embrace|but takes my soul.|He left me for dead|in the gutter|filthy from his deed|yet I live.|In the place of|my soul|he left his instead.
+2 Broken Promises
+stanza Shattered dreams are what I cherish.|My hope and dreams have been burned.|My fears lay ahead of me.|My nightmares have returned.
+stanza I fly from shadows and light.|My soul reaches out for aid.|Warped and twisted is my heart.|The light in my eyes will fade.
+2 Child's Nightmare
+stanza Bash boom bang|It's killing time.|Slash vroom clang,|Death is mine.|Blow, shrug, roar,|Weapons out.|Slow, mug, bore,|All will shout.|Crunch, stomp, splat,|Knives are out.|Scrunch, tromp, scat,|Blood all about.|Wail, howl, moan,|Terror reigns.|Hail, scowl, drone,|All in pain.|(Sounds of terror)|(Sounds of fear.)|(Sounds of death.)|(But no tears.)
+2 Fever Dream
+stanza Fire everywhere|Throats parched|Confusion|Despair
+stanza See the fires|Smoke everywhere|Sooty walls|Silenced screams
+stanza Fallen bodies|Children dead|Weeping women|Sense of dread
+stanza Barking dogs|Pyres piled high|Burning bodies|Darkened sky
+stanza Mute people|Throats closed|Unsteady walking|Death in throes
+stanza Desperate people|No hope|Limp babies|Mothers cope
+stanza Black smoke|Roaring fires|More dying|Bodies piled higher
+stanza Someone screaming|I drink from a cup|A fever dream|I wake up.
+2 Soul's Salvation
+stanza My heart bleeds|cold on the ground.|My fear mounts,|past demons abound.|My eyes dim,|the darkness falls.|My ears ring|I hear Chiron's calls.|My hands shake,|the terror rises.|My heart pounds,|the horrid surprises.
+stanza I need to be found|My life, bleeding, palls.|My body, mind, soul dies.
+2 Unnamed
+stanza The night so old-and deep|My dreams troubled-in sleep|Frontiers unknown-strong fears|Sun so bright-will sear.|Trudging through snow-so cold|My body fragile-so old|Storms rising-so fierce|Slowly I weaken-I curse|I die.
+2 The Huntress Seductress
+stanza I am on the prowl|My methods most fowl|Words are my weapon|Hearts they do threaten
+stanza I choose words as lures|I make a tight net|They can not escape|The trap has been set.
+stanza I tease them to me|Seeing all of my charms|Quieting their fear|Quelling their alarms.
+stanza They pay a high price|For my attention|They give me their heart|Deadly attraction
+stanza Minds become obsessed|Falling into my trap|Their souls are mine|Their wills I will sap
+stanza They chase lusting for me|Hungrily I wait|The closer they come|My hunger they will sate
+stanza They offer me love|I nod and accept|I laugh as they cry|I hold them in contempt
+stanza My heart is stone cold|Their bodies they give|Their love passes through|My emotional sieve
+stanza Their tears mean little|To my stone cold heart|I feed on their love|They try to depart
+stanza When finally snared|Tightly in my grasp|I stop and stare|Hurting they gasp
+stanza They beg me and plead|For release from the pain|I refuse and taunt them|They struggle in vain.
+stanza I stand back looking|At what I have done|I crumple inwards|But tears will not come
+stanza I look at the dead|Hearts piled up high|I take knife to wrist|Why not just die
+stanza The knife falls limply|Out of my shaking|Hand, I fall on it|My heart still beating.
+stanza I cry at the moon|I yell at the sky|My heart still beating|I will not just die.
+stanza My life without love|No one touching my soul|My heart is not dead|I will never be whole.

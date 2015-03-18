87cc2f8ecb633571bcc0b7bad37cef138a39910a
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:42:30 -0400

    Create Expanded_alignments.pl

---
+++ Role_playing/Reference_tables/Expanded_alignments.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib "../../files/lib";
+use Base::HTML qw(html story);
+use Base::HTML::Element qw(table);
+use RolePlaying::CharacterBuilding::Alignment qw(expand_alignment);
+
+my @y_axis = qw(L+ L L(N) -C N(L) N N(C) -L C(N) C C+);
+my @x_axis = qw(G+ G G(N) -E N(G) N N(E) -G E(N) E E+);
+
+my $x_num = 1;
+my $y_num = 1;
+
+my @rows;
+for my $x_axis (@x_axis) {
+  my @row;
+  for my $y_axis (@y_axis) {
+    my $class = undef;
+    if ($x_num % 4 == 0 || $y_num % 4 == 0) {
+      $class = 'divider';
+    }
+    if (($x_num == 2 || $x_num == 6 || $x_num == 10) && ($y_num == 2 || $y_num == 6 || $y_num == 10)) {
+      $class = 'core';
+    }
+    my $alignment = $y_axis.$x_axis;
+       $alignment =~ s/^NN$/N+/;
+    my $title = expand_alignment($alignment);
+    push @row, [qq(<abbr title="$title">$alignment</abbr>), { class => $class }];
+    ++$y_num;
+  }
+  push @rows, [@row];
+  ++$x_num;
+  $y_num = 1;
+}
+
+my $doc_magic = { alignments => sub { table(3, { class => 'expanded_alignment', rows => [['data',\@rows]] }) }};
+
+html( code => sub { story(*DATA, { 'doc magic' => $doc_magic }) });
+
+__DATA__
+Are there times when you can't choose an alignment for your character with so few choices? That has come to an end. Here are 112 new alignments to choose from. A (+) next to an alignment means your character is true (alignment). A (-) next to an alignment means your character is just not that alignment. An alignment in parentheses means your character has those tendencies.
+This chart has been copied and modified A<here|href="http://easydamus.com/alignmenttracking.html">.
+2 The Alignments
+& alignments

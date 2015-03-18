8a786a922e7f3cd7be742fe92da23076327943a6
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:20:06 -0400

    Create Serious_Poetry.pl

---
+++ Writing/Poetry/Serious_Poetry.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+2 Jesus
+stanza To the people out there,|I bring you glad tidings.|A child hath been born|He will start a lot of fighting.|You, the people, will praise him|and you shall glorify His name.|When the Pharoses doubt Him,|you will follow and do the same.|You will spit on Him, mock Him,|Saying vile and evil things.|Why did this have to happen|to One for whom angels sing?|He was hung from a cross|same as a murderer or thief would get|He died a painful, hurtful death|no one would ever forget.
+2 My September 11, 2001
+stanza It was a day like none other in the States;|a day too many people met dire fates.
+stanza As he listened, the President's eyes went blank,|the horror of the event, did not flinch just shrank.
+stanza As I sat and watched the Twin Towers fall|and saw the fires in the Pentagon walls.
+stanza My eyes teared up and I did not want to see|the videos that would record history.
+stanza I was attracted to the horrible views.|Scared, I did not know what I should or could do.
+stanza I called my mom, dad, sisters, brother, and love.|I was afraid of what could come down from above.
+stanza Yet, I had thought that the skies were cleared and empty|Then the jets and 'copters came, there were plenty.
+stanza I looked to the north, a hop, skip but not a jump.|They flew in that direction, my throat, a lump.
+stanza The rest of that day and into that dark night,|I watched those searching, I watched their plight.
+stanza And during that beautiful day, I shed tears.|For days after I continued to have fears.

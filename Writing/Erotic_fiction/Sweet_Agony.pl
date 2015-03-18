46ee51366c61d5f0124f380389bc62a0bcf481d5
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:13:56 -0400

    Create Sweet_Agony.pl

---
+++ Writing/Erotic_fiction/Sweet_Agony.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+He walked in the room right after dinner. I decided to clean up later. I wanted to lie down because that day had been exhausting even though I did nothing at all that day. Maybe that was why he was so upset with me. I had done nothing at all. I was not feeling my best.
+He threw himself down on the bed beside me and started to rub himself. That was a switch. He usually would play with me and that annoyed me for some reason. He just laid there beside me and played with himself. I like to fantasize about that, but actually seeing it was another thing all together.
+He just rubbed himself with his hand. I watched this with some suspicion. I got up onto my knees and went to unbutton his shirt, but he batted my hand away. I was really puzzled. I lay back down and watched him. I saw that the spot where his hand was getting wet. I know that he has a lot of precum, and when he does cum that it would be a major mess. I wanted to tell him to get out of his clothes at least, but when I opened my mouth, he gave me that look to tell me to keep quiet.
+Well, all of this had some affect on me. I started to rub myself. He saw that, and batted my hands away from my private parts. He then got up and tied my hands to the bedposts with the strips of silk cord I had bought for the first time he tied me up.
+"Don't bother me again," he said, harshly.
+I shut up quickly. I didn't know what was going on, but I didn't like it at all. I mean, he never used to masturbate before, that I knew of, but here he was, his hand on his shaft. I was just sitting there unable to do anything at all. I only just got him to tie me up a few days ago, but this is the first time he did it without me asking. I was worried about him, since this was definitely not normal.
+He must have noticed that his pants and underpants were getting very wet, because he took them both off, grabbed himself, and started to go faster up and down his cock. I was mystified. What has caused this drastic change in him? Did he have a lover I didn't know about? Is that why he has changed so much? All of these questions were going through my mind.
+The precum oozed from his erect penis, and I just wanted to touch him, but couldn't. I squirmed in my bonds, but I couldn't do anything. He looked over at me, and that look stopped me from squirming. I just had to sit there and watch. I wanted to close my eyes, but I didn't because of my fascination with my lover's startling change.
+I felt wetness on my own underclothes. I wanted so much to relieve myself of this sexual tension. The sweet agony was overwhelming. I was so aroused by this that I just couldn't help but to try to get myself into a position that I could at least get some gratification, but that wasn't enough.
+He came unexpectedly and shot it into the air above him so that the cum would land back down on himself. His penis went limp, and he sighed with relief. He got up, cleaned himself off, and lay down again. Soon he was fast asleep. I just sat there all night tied up to the bed while he slept. The whole night I wondered what had caused him to do this and even to this day, I do not know, since he refuses to talk about it.

5f06cb949c60c2664d1e9f5bba1e3e54716a591b
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:40:47 -0400

    Create Two_Worlds.pl

---
+++ Role_playing/Player_characters/Stories/Two_Worlds.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib "../../../files/lib";
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+A<Lamaria|href="../#Lamaria_Peacyle"> and A<Yenedra|href="../#Yenedra_Peacyle">, a human and an elf, a priestess and a thief, opposite in almost every way.
+These women are sisters by a marriage neither of their races would condone.
+Lamaira's father Henrick, and Yenedra's mother Allarian, married to stop a war. Henrick, the son of a powerful duke, had seen Allarian, the daughter of an elvish lord, across the battlefield. Henrick and Allarian's eyes met, and in the meeting an understanding was reached. Both knew love was impossible, but both were high-ranking enough they could end the conflict without further bloodshed. They strode onto the battlefield with flags of truce, and in the middle of that bloody field they became betrothed. They called for their fathers who went to find out what had happened. When informed they called to their kings. Their kings agreed a marriage between these two would indeed stop the war. Henrick and Allarian were of royal blood but far removed from the thrones.
+The marriage and peace had a price. Both kingdoms had laws against such a marriage, and the penalty was banishment of the couple and all of their descendants. As it happened Henrick and Allarian had children by spouses lost in the conflict. These girls were forced to give up the splendor they lived in to move away. Though their parents were banished, they did not leave empty-handed.
+Lamaria was bitter about leaving the land of her birth. She sought solace in her faith. During her search she became interested in helping others, and she joined her church as a cleric. She now travels with her step-sister Yenedra who has caused her a great deal of grief. Knowing Yenedra would live longer than she did has always been her private pain, and the beauty and grace of Yenedra has been hard for her to accept. She does not hate her step-sister, she just envies her, and she is working to get rid of that envy.
+Yenedra was angry with her people for their short-sightedness and also angry with her mother for her decision to marry, of all things, a human. She hated banishment, and she started making her mother's and step-father's lives miserable by stealing. She became very good at it, but she knew she needed to go out in the world. She went to the church of her step-sister Lamaria and talked her into travel. She knew she would not make it alone, and Lamaria was the perfect person to go along just in case any trouble arose from her pilferage.
+Unknown to anyone, there is another in their family, A<Drianna|href="../#Drianna_StrongOak-Guard">. She is the daughter of one of Yenedra's paternal first cousins, and Lamaria's maternal grandmother. They died in the battle, never knowing what their daughter would become one day.

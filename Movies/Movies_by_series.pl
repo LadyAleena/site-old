c7e5597f82e15bb37619966eb05c25f06ef260f1
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:07:54 -0400

    Create Movies_by_series.pl

---
+++ Movies/Movies_by_series.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+use HTML::Entities qw(encode_entities);
+
+use lib '../files/lib';
+use Base::Data qw(alpha_array);
+use Base::HTML qw(html);
+use Base::HTML::Element qw(section heading paragraph list);
+use Base::Menu qw(file_menu alpha_menu);
+use Util::Sort qw(article_sort);
+use Util::Columns qw(get_columns);
+use Movies::Display qw(print_series print_program);
+use Movies::LookUp qw(options);
+
+my $cgi = CGI->new;
+my $select = encode_entities($cgi->param('series'),'<>"');
+my $series_select = options('series');
+my @selects = sort {article_sort(lc $a,lc $b)} keys %$series_select;
+my $file_menu = file_menu('series', \@selects, $select);
+my %alpha_list = alpha_array(\@selects);
+
+my $head = $select && $series_select->{$select} ? qq(<i>$select</i>) : undef;
+html( 'heading' => $head, 'file menu' => $file_menu, 'code' => sub {
+  if ($select && $series_select->{$select}) {
+    if ($series_select->{$select} eq 'series') {
+      print_series(2,1,$select);
+    }
+    else {
+      print_program(2,1,$select);
+    }
+  }
+  else {
+    section(3, sub {
+      for my $alpha (sort keys %alpha_list) {
+        heading(5, 3, $alpha, { 'id' => $alpha });
+        list(6, 'u', file_menu('series', $alpha_list{$alpha}), { 'class' => get_columns(3, scalar @{$alpha_list{$alpha}}), 'style' => 'font-size:smaller' });
+        alpha_menu( 6, \%alpha_list);
+      }
+    }, { 'heading' => [2, 'Pick a series'] });
+  }
+});

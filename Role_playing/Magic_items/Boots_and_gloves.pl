d0a7e20f46f2bd97814123abbf664578ec4eddc0
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:23:24 -0400

    Create Boots_and_gloves.pl

---
+++ Role_playing/Magic_items/Boots_and_gloves.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+use Base::LineMagic qw($line_magic);
+
+html( 'heading' => 'Magical boots and gloves', 'code' => sub { story(*DATA, { 'line magic' => $line_magic }) });
+
+__DATA__
+2 Slim's Aquatic boots
+by Rich Rayburn
+There is only one pair of these boots in existence right now, but Slim is considering making more or trading the directions on how to produce them in exchange for more ho's.
+The boots, while not specialized for the Pimp Mage, will most likely only be worn by a Pimp Mage due to their appearance (see below).
+Slim's Aquatic Boots grant the wearer the same effects of Boots of Striding and Springing, but upon landing from a jump, the boots must save as enchanted glass +2 versus crushing blow or shatter, and the wearer must make a dexterity check or trip; after all, they are six-inch platform boots.
+The Boots additionally bestow on the wearer a power boost to any "person affecting" charm spells, causing the recipient(s) to make their saving throws at a penalty of -2, this is cumulative with any other penalties accrued.
+These high-hard boots have a six-inch hollow glass platform, filled with water and three magical fish apiece. These fish are in a kind of stasis, and require no food, fresh water, or cleaning of their "tank". It is the fish that give these boots their power, and if the fish should die, all magical effects of the boots will cease. Slim has already attempted and failed to cast a Glassteel and Permanency on the boots. If  Crystalbrittle is ever cast on these boots, they automatically shatter losing all magical properties. These boots save versus magical fire at a -2, and on a failed roll, the fish are cooked. The uppers of the boots are made of brightly dyed silk and exotic animal fur, and reach to two inches below the knee.
+2 Bracers of Shielding &amp; Striking
+by Johnny Brant
+These bracers are commonly bestowed by rulers as a sign of recognition to distinguished champions of great battles and are often ornately engraved with the owners rank and the insignia of his company of the military force he belongs to. In the rare case that a person not from a military force should be awarded these tokens of the ruler, they are always engraved with the person's personal or family symbol(s). 
+These bracers appear to be made of burnished steel, inlaid with small emeralds to form runes. These runes are readily identifiable as those used to mark enchanted magical items made by bards and wizards. The bracers also show the presence of invocation magic if detected for. Once per day, the user may choose to invoke either the defensive or offensive power of the bracers. Used defensively, the bracers provide a magical +1 to the wearer's Armor Class and form a I<shield> spell around the wearer for the duration of the day. If used offensively, the bracers allow the wearer to create 5 I<magic missiles>, which may be used all at once or on separate occasions throughout the day. In addition, any melee weapon the wearer of the bracers is wielding gains the magical abilites of a +1 weapon. If the weapon they wield is already magical, it instead gains an additional +1, up to a maximum of a +5 magical weapon. 
+If the effects of the bracers is dispelled for any reason, they cease to function until the next day. The same effects of the bracers cannot be used for more than two consecutive days. After this time the power chosen will be the opposite of what what used the day before.
+There are also rumors that a more powerful version of these bracers exist. The owner is sorceror who ruled states and countries. These bracers would instead bestow a +2 bonus and would have the powers of fire shield and I<fireball>.
+2 Gauntlets of Giant Power
+|| rolls
+|* d100|Giant|XP value|GP value|Str.
+|+ 1-20|ogre|1000|10000|18(00)
+|+ 21-35|hill|1250|12500|19
+|+ 36-50|stone|1500|15000|20
+|+ 51-65|frost|1750|17500|21
+|+ 66-75|fire|2000|20000|22
+|+ 76-85|cloud|2250|22500|23
+|+ 86-95|storm|2500|25000|24
+|+ 95-100|titan|2750|27500|25
+These gauntlets are like the Gauntlets of Ogre Power in the I<Dungeon Masters Guide> for Advanced Dungeons and Dragons 2nd edition. The giants from the Girdle of Giant Strength have been added plus titan.
+2 Drianna's Glove of Wands
+This Glove enables the wearer to wield four wands at one time, the wands are connected to the fingertips of the glove. It allows the wearer to use two of the wands simultaneously by telepathy negating the initiative modifier. The Glove is made of a fine leather with silk stitching. There are four fingers to the Glove but no thumb piece. On the palm of the Glove, there is the sigil of Drianna of Willowdale, a weeping willow with 14 weeping branches.
+2 Gloves of Empathy
+by ^Gabriel Elias^
+These white cotton gloves of chaotic neutral alignment that do not emanate magic and appear, at a glance, to be plain white gloves. Upon closer inspection there is a label on the inside with washing instructions, cold water wash; wear dry, and a warning, THESE GLOVES ARE CURSED! DO NOT PUT THEM ON! Inspected by Evil Mage #12, to the wearer.
+When worn they automatically start to feed off of the wearer's emotion(s). For example, if the wearer is insulted by another character, they will magically pull the wearer to the insulter and force them to Beat the opponent for 1d4 of non lethal damage. If the wearer sees something that he/she must have, the gloves take him or her and go get it.
+Once the wearer puts the gloves on, they do not come off. To try to control these gloves, the wearer must make a charisma check at half once a day. They are a cursed item and only a remove curse may remove them.
+They also act as gloves of free action.
+2 Gloves of Metallic Touch
+by Nathan Brown
+Much like the touch of the Midas legend, while wearing these gloves, anything the gloves touch becomes its respective metal. The gloves may have a limited number of charges. Only after these are expended may the gloves be removed. Should the gloves have unlimited power, they can only be removed by a Wish, or by a Remove Curse from a priest of at least 17th level.
+* 10% Gold touch
+* 20% Copper touch
+* 30% Crystal touch (I know it's not a metal)
+* 40% Iron touch
+br
+* 10% unlimited use
+* 20% 50 charges
+* 30% 20 charges
+* 40% 10 charges
+Living creatures touched save versus petrifaction at a -4 penalty. An alteration of Stone to Flesh may be used to return victims to their normal state. A special god-given quest may be necessary.
+2 Gloves of Savagery
+by Joshua Moore
+This pair of gloves can bestow on a warrior incredible power in combat—for five rounds the warrior's every hit is critical (by house rules). After the five rounds the warrior collapses helpless for five rounds. Five rounds is just the maximum—if they are removed after three rounds the collapse will be for three rounds.

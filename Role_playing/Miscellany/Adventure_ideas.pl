8f4819b3b467d0490607f52b1d038479838e5f93
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:30:41 -0400

    Create Adventure_ideas.pl

---
+++ Role_playing/Miscellany/Adventure_ideas.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+2 The Phlostigen Is Burning
+An insane wizard is setting fire to the phlostigen by attaching time delayed fire spells to crystal spheres. The first explosions have been small, but they are increasing in magnitude. 83 crystal spheres have been destroyed, 47 severly cracked, 78 partially damaged, 108 inaccessable from the outside, and 359 sealed. How many more are in peril? No one other than the madman knows. Can the party stop the madman in time, and can they find all of the spells yet to be detonated?
+2 Nevermore Heights
+Evil is disappearing all over the Prime Material plane. Large population centers are there one day, gone the next. The evil powers are baffled, the good powers are shocked, and the neutral powers are worried. Also, weird things are happening around the areas vacated. People are anxious around shadows, odd mutations, and more. The high priests of the various powers are contacted and told to find mortals who may be able to solve the puzzle.
+With the powers blind to what is happening, the clergy of the powers are terrified.
+Another question, when the cities are found, will it mean a war between overpowers to destruction of an entire plane?
+2 The Lady of Pain meets the Lord of Pleasure...
+Who knows what will happen next in the Outlands.
+2 Future Earth - Armageddon Group (AG)
+Ever since Revelations was written, organizations dealing with Armageddon have come and gone. Some only watched for the end, while others actively tried to make it happen one way or another. When it didn't come in the last millennium, most of the organizations disbanded, while a few remain.
+These organizations are diverse in their activities. The leader of one from the latter group has been bringing these separate and sometimes disparate groups together under one banner since the last millennium ended. The AG is now world-wide. They are no longer have just a Christian centric view of the end of the world to spread fear and panic; they have dug into other religions and practices as well.
+Various political organizations have been trying to find out who the leader and the other main figures are in the AG for the last several years. Many acts of terrorism have been attributed to the AG: biological attacks, contamination of resources, mind control, the theft of numerous types of weapons, and more. Some say that the AG controls every organization regarding Armageddon. Whether or not this is true is unknown.
+Other groups that study Armageddon have begun to respond to the threat the AG poses. Some within the Armageddon community fear that a war is brewing between the AG and other organizations. With the secretive operations of both sides, no one really knows what is happening.
+Recently several credible theologians and secular researchers have disappeared. The populations of some small towns and villages around the world have been decimated. New forms of graffiti have appeared all over the world. And this is just the beginning.
+3 Groups in fiction bringing about Armageddon
+* The Millenium Group
+* Wolfram & Hart

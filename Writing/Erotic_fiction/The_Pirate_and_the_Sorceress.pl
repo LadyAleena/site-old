c1d53c3d6ff67a396b134b979bf4a83e6633b453
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:15:41 -0400

    Create The_Pirate_and_the_Sorceress.pl

---
+++ Writing/Erotic_fiction/The_Pirate_and_the_Sorceress.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+In a cabin of a ship a man and woman can be seen talking. The man wears flamboyant clothes, the outfit of the classical pirate. The woman is in a floor length skirt and voluminous blouse, and they looked finely woven, and on them both are mystical symbols. They appear to be talking about the pirate trade. They seem to know each other well, very well, since the woman doesn't seem to flinch from some of the things being said to her. She nodded her head a lot and just took the words in. There’s a mischievous gleam in her eyes though, and he doesn't seem to realize it yet.
+hr
+I am that woman, and this is my story with my pirate lover...
+hr
+He cocked his head to the side and grinned. "Well it depends on the lass that I kidnap and rape. If she is sturdy and can take it then I am rough, but if she is delicate and tiny I have to use less force to get what I want." he smiled.
+"That is very good thinking, you wouldn't want to break the merchandise would you?"  I smiled and my eyes gleamed brightly.
+He smiled mischievously, "So which are you Love? A strong and able Lass, or a delicate petite thing?"
+"I am somewhere in the middle."  I shrugged and my breasts bounced when I did.
+He watched the bouncing and raised an eyebrow.
+"Love, you keep that up and I may have to take, now, what I wanted to savor later." He grinned.
+"That might be the general idea, my friend."  I smiled naughtily and I shrugged again, making my breasts bounced again. I moved in my chair letting my skirt fall away from my legs.
+He laughed evilly and shook his head.
+"You think that you can tempt me when I have less then fifteen minutes before I have to set sail out of here?" He reached out quickly and roughly pulled me into his arms. "Well you are right."
+"I know."  I said as I looked daringly into his eyes. "That was the general plan when I found out you were here."  I smiled and my dimples showed.
+He nipped at my neck and pressed himself onto me. I yelped slightly as he bit at my neck, yet my breathing got shallower. My breasts swelled and my nipples hardened under my thin shirt. I whispered back.
+"I could not resist you. You make me lose my self control."  I pressed my body on his so that he could feel my need.
+He lifted me into his arms and carried me over to his chair. Pressing me back into it he slipped between my thighs spreading them with his hips. His lips found mine as he kissed me hungrily, crushing them beneath his. Pulling back slightly he looked into my eyes, desire and lust shining hot.
+"Loose control?" he demanded in a harsh whisper. "Your loss of control as made me lose mine."
+"I know." I whispered hoarsely under his lips. My legs trembled around his hips. My eyes showed my lust. "I could not help myself, as you should well know." I continued.
+He pulled my skirt up and bunched it up around my hips as he reached down to release his cock.
+"Is this what you crave Woman?"  He lowered his lips to my neck as his hands reached up and pulled down on my blouse, tearing it and freeing my breasts.
+Moving his lips to my breast he sucked one nipple into his mouth, chewing on it lightly.
+I looked down between my legs, and seeing the object of my desire, I smiled.
+"Oh yes, that is it."  My head fell back on the chair as I moaned loudly when he sucked on my breasts, and my hands wandered down his back and across his shoulders.
+He looked up, over my body, and out of the cabin window.
+"It looks like you have me for thirty minutes Love. And I intend to take those minutes from you."  He said seriously. I do not know what changed his mind, and stay longer, but I was happy he did.
+"Oh you will, will you?"  I smiled slyly.
+He guided his cock to my wet lips and with one hard thrust he pushed all the way into me, sinking hard. His tongue rubbed hard over my nipple as he began chewing lightly. His other hand moved up to my other breast as he massaged it roughly, pinching the nipple between his fingers.
+I screamed loudly as he pushed himself deep within me. My legs trembled from the harshness of his thrust. I breathed shallowly; my hands ran all over the parts that I could reach.
+"Ohhhhhhhh, yesssssssss," I breathed.
+He began thrusting hard and fast, each time sinking deep into me. He released my breast from his hungry mouth and crushed his lips to mine, his tongue thrusting deep into my mouth. One hand slipped down to my hips and slipping around he grabbed my ass firmly, pulling me into each thrust.
+"You are mine for these next twenty-five minutes Wench."
+I cried out at every thrust. My body shuddered under each blow. My hands moved over his chest. My lips were crushed under his. I grabbed his ass too, feeling it, squeezing it. I then went back up to his chest.
+"I am all yours then," I whispered coarsely.
+He continued to pound into me, shoving me back into his chair, his hand slipping further around until he slowly pressed his finger against my tight asshole. He moved his lips to my neck, biting firmly as he growled softly in my ear.
+"This is what a slave of mine could expect from time to time," he chuckles.
+I moaned with each thrust, my whole body became aroused. I took a deep breath as he fingered my virgin ass. I winced as he bit my neck, yet it aroused me further. I moaned. My breasts swelled more, and my nipples got harder.
+"I can't be a slave," I panted.
+"I know." he growled as he thrust hard, his finger slipping slowly into my ass. He released my neck and returned to my breasts, chewing and nibbling all over them until he finally took one nipple into his mouth and sucked hard, rubbing his tongue roughly over it.
+I writhed under his mouth. My chest heaved up and down. My hands caressed him as far as they could, over his shoulders, his back, his neck, and his hair. My legs shook from his pounding. My whole body shook. I breathed very shallow. My breasts ached in his mouth and my nipples became as hard as cherries and just as big.
+"You want me to be your slave don't you?" I gasped in his ear.
+He moaned as he heard my words and felt his balls getting tight as he got close. He picked up the pace, thrusting hard and deep still and faster. His finger in my ass soon began thrusting slowly in and out once for every two thrusts of his cock. He lifted his head and looked deeply into my eyes.
+"You have always been one of my favorites Love," he gasped.
+I gasped again as he picked up the pace. I held back as much as I could. I started to lose control as I felt the finger in my ass. I felt so full, and my lust for him became evident when my hands went to his chest and I started to pinch his nipples roughly.
+"I didn't know that," I panted.
+He cried out as he could hold back no longer and thrusting hard and fast, slamming me hard into the chair and thrusting his finger faster in my ass, I felt his balls grow tight. His cock exploded deep inside of me as he lowered his lips to my neck, biting my neck hard. One arm held me to him as he continued to fuck hard into my lovely body.
+"Yes and you are one that I would consider Love."
+As he slammed into me, my body with perfect harmony with his exploded in climaxes beyond belief. My vaginal walls clamped tightly around his shaft. My ass clamped around his finger. I screamed as he bit my neck. I climaxed long and hard making him unable to leave my body just yet.
+"Do you regret that I can't be a servant?"  I gasped in his ear.
+He moaned as I held him inside of me, and he laid down onto me, pressing me firmly into the chair. He slowly released my neck as his body shivered softly, and he gasped for breath. He softly kissed my lips and looked deeply into my eyes before whispering almost out of breath in my ear.
+"I regret that I can not own you as completely as I wish. I have one slave, and she is mine completely and I regret that I can not make you the same," he sighed softly, "but I do enjoy being with you Love."
+My body started to get floppy. My back rested on the chair. I returned his soft kiss, and my eyes looked into his with some regret as well. I held him to me and wished this moment could last forever.
+"I am not for sale, but if you ever need me, my dear, all you have to do is ask," I sighed on his neck. "I enjoy your body as much as I hope you enjoy mine," I sighed again, sending my breath down his back, "I just wish you didn't have to leave so soon."  I held him closer.
+He held me close and then pulled back, lifting me into his arms to carry me over to the bed. He put me down on the bed and lay there next to me. I curled up with him under the covers and he kissed me softly. "I did not say for sale. But I understand Love and I thank you. I do have to go, five minutes ago in fact."
+"I understand, and you go before you see me cry. I want you now more than ever, my pirate."  I embraced him close before he left. "Just ask, and I will be yours."  I curled up and hid my face when he left to board his own ship and sail away, maybe forever.

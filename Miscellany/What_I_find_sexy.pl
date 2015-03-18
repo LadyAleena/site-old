32aa186d35ceed70338f9074b3af2d23ed707f08
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 14:49:44 -0400

    Create What_I_find_sexy.pl

---
+++ Miscellany/What_I_find_sexy.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+People are so much sexier to me when they wear buttoned shirts, because I think of unbuttoning them one by one, slowly. Zippered shirts are a little less sexy than buttoned shirts, t-shirts less sexy than that unless scissors are involved. Being initially bare chested is not as sexy.
+Piercings on men are definitely not sexy. Piercings for women are all right if they one pair for earrings is okay, otherwise piercings are not sexy. When it comes to tattoos, one is acceptable, but it better be meaningful. No matter how much a Bruce Campbell fan I am, tattoos of his characters are not sexy. I get a little squishy when it comes to body hair. Body hair on men is okay as long as he isn't a walking carpet, whereas on women, well, I do prefer shaved legs.

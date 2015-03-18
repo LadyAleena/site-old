642d0a89aa6aa9f85f92be6bb02957e102067482
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:23:02 -0400

    Create Dwayne_Pagnotto.pl

---
+++ Writing/Poetry/Other_poets/Dwayne_Pagnotto.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+Send a message to A<Dwayne Pagnotto|href="mailto:shortbread4@yahoo.com">.
+2 Lightning Caress
+stanza Rain, i'm choked in it, soaked in it|I wanna play in it, pray in it|Just spend all day in it.
+I can't get enough of this fabulous stuff.
+stanza As we ran from shelter to shelter|One day after school|During a thunderstorm...i felt her.
+We danced around naked under the cover of trees
+As refreshing drops fell from the leaves.
+stanza Goosepimpled flesh|We were now getting cold.
+stanza We made love in the grass|Now feeling bold.
+stanza Then as the rain stopped falling|I kissed her all the way home.
+That day we made a rainbow of our own.

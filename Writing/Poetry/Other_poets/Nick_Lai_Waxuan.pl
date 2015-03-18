93b0d59d4c048008be9b88aa16da56edfa38f8cf
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:24:42 -0400

    Create Nick_Lai_Waxuan.pl

---
+++ Writing/Poetry/Other_poets/Nick_Lai_Waxuan.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+Send a message to A<Nick Lai Weixuan|href="mailto:lai_weixuan@hotmail.com">.
+2 Unnamed (1)
+stanza Crystal clear and deep wide eyes|So beautiful that it made the fey cry|Gentle hands smooth and fair|Constantly adjusts and smoothes her hair.
+stanza Coyness exudes an air of innocence|In a garden of peach blossom fragrance|She walks by them, and the branches shiver|The falling petals, a sight that made her quiver
+stanza Guided by inspiration, a dance improvised|Took my breath away, no mortal words can devise|I sketch her portrait, on the other side|Unknown to her, I sketch a work of pride
+stanza Such beauty that my words cannot describe|I sketch her beauty, and wish for her to be my bride
+2 Unnamed (2)
+stanza Soft blankets pulled overhead|As two figured lay together on bed|Quivering lips locked in passion|One's deep lust makes a confession.
+stanza Two bodies joined as one|In a dance that is exciting and fun|The heat of the moment is overwhelmingly good|After this bout they are still in the mood.
+stanza The scent of love is in the air|The skin of the maiden beautiful and fair|Her hair long and soft, silky and smooth|The alluring swing each time she moves.
+stanza This consumation of love both hold dear|As long as they are together, there is nothing to fear.

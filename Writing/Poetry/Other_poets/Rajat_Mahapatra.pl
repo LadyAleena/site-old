2e2e709435e171f1934e32cefab231c00f3ead0f
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:25:13 -0400

    Create Rajat_Mahapatra.pl

---
+++ Writing/Poetry/Other_poets/Rajat_Mahapatra.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+Send a message to A<Rajat Mahapatra|href="mailto:raj4591@yahoo.com">.
+2 Unnamed
+stanza i have none|cant show|where is the root
+stanza of the link that shows|the bottom
+stanza the damned and the dear|clustered together|bound by the freedom
+stanza want some of the strange need|i love to offer|to share and shame
+stanza fly beneath the eyes of fear|blood-curdled|factored
+stanza unheard and turned unreal|far from me and yoy
+stanza in that embrace|we call the curse of love.

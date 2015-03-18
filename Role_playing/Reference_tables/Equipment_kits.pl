514771f7ed1b3917e81c26f6fa885e62ca808754
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:42:09 -0400

    Create Equipment_kits.pl

---
+++ Role_playing/Reference_tables/Equipment_kits.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+use Base::HTML::Element qw(definition_list);
+use Base::Data qw(get_array);
+
+my @def_headings = ('cost', 'weight', 'items included');
+my @definition_list = get_array( 'headings' => ['term', @def_headings] );
+my $doc_magic = { 'equipment' => sub { definition_list(4, \@definition_list, { 'headings' => \@def_headings }) }};
+
+html( 'code' => sub { story(*DATA, { 'doc magic' => $doc_magic }) });
+
+__DATA__
+Equipment kits are a way to save space on your player character worksheet and to save you time. The prices may vary by dungeon master or game edition.
+2 Equipment
+& equipment
+SMALL<Prices here are from Dungeons and Dragons, 2nd Edition.>
+2 Clothing
+Clothing sets cost around 3 gp. A set is a shirt, a tabard or vest, and pants or a skirt. Often the pieces are the same color or at least coordinated.

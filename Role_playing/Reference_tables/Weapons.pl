b74b0f7e0f1558254ece717e87ceddea5207bae1
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:44:29 -0400

    Create Weapons.pl

---
+++ Role_playing/Reference_tables/Weapons.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+use HTML::Entities qw(encode_entities);
+
+use lib '../../files/lib';
+use Base::HTML qw(html);
+use Base::HTML::Element qw(section table);
+use Base::Data qw(get_hash);
+use HTML::Forms qw(tiny_select);
+
+my $cgi = CGI->new;
+my $broad = $cgi->param('broad') ? encode_entities($cgi->param('broad'),'<>"') : '';
+my $tight = $cgi->param('tight') ? encode_entities($cgi->param('tight'),'<>"') : '';
+
+my @headings = ('Weapon','#AT','Dmg(S/M)','Dmg(L)','Range','Weight','Size','Type','Speed','KO');
+
+my %weapons_list = get_hash( 
+  'file' => ['Role_playing/Reference_tables','Weapons.txt'],
+  'headings' => [@headings,'broad group','tight group','value']
+);
+
+my @rows;
+for my $weapon (sort { $a->{'Weapon'} cmp $b->{'Weapon'} } values %weapons_list) {
+  next if $broad && (!$weapon->{'broad group'} || $weapon->{'broad group'} ne $broad);
+  next if $tight && (!$weapon->{'tight group'} || $weapon->{'tight group'} ne $tight);
+  push @rows, [map($weapon->{$_} ? $weapon->{$_} : '',@headings)];
+}
+
+my $head = $broad  ? $broad : $tight ? $tight : undef;
+html( 'heading' => $head, 'code' => sub {
+  section(3, sub {
+    tiny_select(4, { 'location' => 'Weapons.pl', 'file' => ['Role_playing/Reference_tables','Weapons_select.txt'], });
+    table(4, { 'class' => 'proficiency', 'rows' => [['header',\@headings],['data',\@rows]] });
+  });
+});

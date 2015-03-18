a520e94c04c9efb33d70f8c510791544d202c214
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:23:30 -0400

    Create Marcia_Probst.pl

---
+++ Writing/Poetry/Other_poets/Marcia_Probst.pl
@@ -0,0 +1,143 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+Send a message to A<Marcia Probst|href="mailto:lady_acoma@yahoo.com">.
+2 The Damn
+stanza When I got the news fear of the unknown made me cry.|But the rain was behind shameful hands which trembled.|I saw rocks around me, and though I felt like flowing sand in the sea I fought to hold to my place.|I was not going to die they said.  I could help myself and live a normal life.|And yet it has invaded every part of me|An infestation of myself runs rampant.
+Needles, food, and blood are drowning me everyday.
+stanza The call came in while I was away the day my brother and I became one in our eternal plague.|I tore and screamed and shouted aloud for all to hear and witness.|My rage was a flood cascading over everything in it's path.|He was not supposed to feel my pain and sorrow.  I was to bear it alone, as always before, while he lived a happy, healthy life.|I told him he always took my glory over the phone.|This time instead of hands, the phone hid my torrent of tears.
+Together we are plunged into murky depths.
+stanza Grandma is being pulled in too, but won't admit that she is standing knee deep in our pool of anguish.|I watch her deny it all each time a new wave laps around her feet.|Someday I feel the undertow will drag her, and pull her to us.|When it does I don't think she will make it.
+Trickles are breaking out anew all around me.
+stanza My mother at least sees the storm coming at her.|She is checking her levies, they are leaking so she is teaching herself to swim before the flood.|Her awareness adds another layer to her damn.|But will it keep it all from falling down and sweeping her away?
+How did I open the floodgates on everyone I know?
+stanza Who else will join us as we kick and paddle in the flow?|Will they be ready?|Run from me, I've broken the damn!
+Needles, blood, and food are drowning me everyday.
+2 The Edge of Insanity
+stanza If I were falling off the edge of insanity who would be the one they sent to save me?|Would it be the fastest in fear that I would cascade at any moment?|Or the smartest, who could devise a plan to safely put me in the correct position?|Maybe the gentlest, who could bring me back with love and affection?|Would the strongest be sent to drag me back kicking and screaming?|Maybe a fool to show me what I may be and change the error in my ways.|Or would there be anyone at all?
+2 A Fish Named Darwin
+stanza I have a fish named Darwin.|I like my fish.|I feed him, provide him a home, change his water.|I wonder what it would be like to watch him in the wild.|Would his characteristics change?|I named Darwin after the scientist.|I never really found what he said wholly true.|Of course to be fair I never really studied him much either.|All the same, I like holding Darwin's life in my hands,|Or bowl actually.
+2 The Guise of a Man
+stanza Smile little boy.|Don't hide your fears behind the guise of a man.|If you can't let me hold you or listen, then at least let me sit near by.|You don't have to share it out loud.|But don't try to block it all in either.|You don't always have to be as strong as you want to be.|Just be as strong as you are.|In the end you will be twice the man you are trying so hard to be.
+2 Holding Arms
+stanza You held me last night and it felt good to be in your arms.|For those moments were special to me.|I know you don't feel the same way I do.|That's okay.|I know you have feelings for others.|That's okay.|I'll just let you hold me in your arms all the same.
+stanza I held you within my own arms last night.|It felt good to know that for a moment I was the right person for you.|I know I won't always be that one.|That's okay.|I know this hasn't changed how you feel.|That's okay.|I'll just hold you until you don't need me anymore.
+stanza We held each other in warms arms and cried tonight.|I was there for you and your were there for me.|I know we will probably never do it again.|That's okay.|I know it will be others who hold your heart.|That's okay.|I'll have other arms too.
+2 I Dream Alone
+<div class="center">
+stanza When I sit alone at night I dream.|Sometimes blood curdling horrors run from the corners|And threaten to tear the flesh back from my|Mutilated soul.
+stanza Sometimes I just ache with the strain|Of all of life's little whims and wants.  Feeling each fiber of|Each muscle groan at the abuse it has tolerated|One more day.
+stanza An excellent bliss sometimes over comes me,|And I cannot help but smile at the ceiling in all it's glory as I|Ponder the magnificence around me. I even go so far as to|Giggle with the walls at the supreme wonder of|It all.
+stanza Many times I will snuggle into the warmth|And security of my own bed. Drifting off satisfied with|How I have lived that day alone|At the very least.
+stanza But a lot of the time I wish I didn't have|To dream alone|At all.
+</div>
+2 I Hate
+stanza I hate.|Why can't you feel it's burn?|Don't cling to me and try and shape me with your love.|I hate.|Why don't you feel it consuming everything good?|Don't hold onto me and try to pull me back from my stupor.|I hate.|Why can't you see me eating your heart, consuming your soul?|Don't cry out for me and morn my losses.|I hate.|Why don't your realize I am ravaging your mind and body easily?|Don't look at me or touch me please for,|I hate.
+2 Keeping the Bad Things Away
+<div class="center">
+When I was a little girl my mommy tucked me in at night to keep the bad things away.
+When I was a teenager monsters within myself began to bump and grind causing confusion and disarray.
+As a young woman I am trying to see through the gray for to where the black and white lines used to stay.
+And I am beginning to understand why my mom wants to tuck me in to this day.
+</div>
+2 Knock at the Door
+stanza Take a deep breath, how do you feel?|Is the world reaching and clawing at your toes?|Do you find yourself wondering if it is even real?|Blink and look around you, tell me did you see friends or foes?
+stanza Knocking at my door the devil comes to call my name.|His face is pure and sweet, no flaw to point out it's wrong.|As the angel he was does he simply point out all of my shame.|I weep with the loss of what is not to be found and a wail of remorse is now my song.
+stanza Holding up his arms he offers innocence for my devotion and love.|Standing aside he shows me his multitudes, behind him a sea.|A beautiful smile at the head of many, how am I to see he's not from above?|And with shame such as mine is it right for me to question in truth?
+stanza Another knock at the door which opens to reveal the Father this time.|A grimy face looks at me in the clothes of a thug,|Filthy hands reach out to ask for my hard earned dime.|Through my mind slides real of violence past and yet he asks me to gift it all with a hug.
+stanza Take a deep breath, how do you feel?|Is the world reaching and clawing at your toes?|Do you find yourself wondering if it is even real?|Blink and look around you, tell me did you see friends or foes?
+2 Laughter and Smiles
+You aren't attracted to my charm and my wit.
+>4 You don't notice my infectious smile,
+>8 Or the way I laugh out loud.
+Have you ever seen the way people flock to me?
+>4 They come to me for love, advice, for many things.
+>8 Why can't you come to me at all?
+I won't chase after you and tell you how much I care.
+>4 If someday you see me, I'll look back,
+>8 And if someday you love me, I might do the same.
+But till then I'll go back to my laughs, smiles, and friends.
+>4 Maybe someone will notice my charm and wit.
+>8 Or like my laugh and my smile.
+And maybe I'll love them and share with them all my secrets.
+>4 They might be the one whom I turn from the flocks for.
+>8 The one who will hold my laugh and my heart.
+Then you, who didn't ever see me, will never feel my love,
+>4 Know my depth and the real reason for my laugh and smile.
+>8 And you will never know what it truly measures
+<p class="i12">For me to hold you tight.</p>
+2 The Mayor
+I was with the future mayor last night.
+>4 Not that he will be the mayor tomorrow but he's trying.
+>8 He's young.
+>4 And he was drunk.
+But he was a good man.
+>4 It was funny,
+>8 I, a girl not used to much attention,
+And him, someone who one day, no is, important.
+>4 We had fun.  We danced.
+>8 We kissed.
+>4 Nothing more ever has to come.
+For that night he made me feel sexy and wanted.
+>4 I was with the future mayor last night,
+>8 But he made me feel like he was with me.
+2 My Name
+stanza My name is dirt, kick me off your shoes.|I am trash, to be thrown out and left behind.|My name is mud, push me aside to build yourself a world.|I am shit, to be flushed away and forgotten as refuse.|Kick me, throw me out, push me aside, forget me.|It's what I want, crave, desire, deserve.|Please just do as I ask and don't give me a cause to hope.|Hope is my bane, hope will kill me, hope has driven me mad.|My name Is dirt, kick me off your shoes.|Let me live satisfied with what you throw aside,|For what you throw aside is what I am.
+2 Promise To Hold Me
+stanza Last night you were lonely and drunk.|So you held me.|Last night she didn't want you.|So you held me.|Last night she tore you apart blindly.|So you held me.
+stanza I know why you held me,|I know you don't want me.|Please just promise to hold me again.
+stanza Last night your realized you were leading me on.|You walked away.|Last night you remembered  I care for you like you do for her.|You walked away.|Last night you didn't want to hurt me.|You walked away.
+stanza I know why you left me,|I know you don't want me.|Please just promise to hold me again.
+stanza Last night you made me feel funny.|This is why I love you.|Last night you made me feel smart.|This is why I love you.|Last night you made me feel beautiful.|This is why I love you.
+Please just promise to hold me again.
+2 Pulling Wool
+stanza I'm pulling the wool over my own eyes.|Make believe was always my favorite game.|Now I just have a different reason to play.|Deception and denial are truly such cruel words.|Living with depression is only a delusion that drags you down.|Everyone always told me I was creative.|And imagination was one of my favorite things.|So what do I care if I play hide and seek with the truth?|Just be glad that I am pulling the wool over your eyes too.
+2 Remembering
+stanza I stopped caring a long time ago.|I stopped being, living, breathing to save myself.|I stopped, and now I just want to stop again.
+stanza I believed I was beyond love in my agony and pain.|I believed tomorrow was just another step toward the release of the end.|I believed, and now I just want to truly believe again.
+stanza I embraced the hollow husk I was as the last refuge of me.|I embraced my hurt and scorn as the only things I would ever know.|I embraced, and now I just want to embrace it all again.
+stanza I cried to look like the normal person they all wanted to see.|I cried to hide the fact that I exalted in what they were morning.|I cried, and now I wish my tears were only lies again.
+stanza I stopped. I believed. I embraced. I cried.|And now I just wish I didn't remember.
+2 Sometimes All It Takes
+stanza Sometimes all it takes is a little touch.|You held me hand for an instant at a time.|Not the firm grasp of someone who is unsure in themselves
+>4 And wants to make a big impression.
+stanza Or the meek grasp of one who is to shy.|Just a light pressure to let me know you wanted me there.
+stanza Sometimes a slight glance is enough.|Not a headlong stare designed to test wills.|Or a gaze deep into my eyes with all the love you
+>4 Can muster from deep within your core.
+stanza Just the look of a friend is all you need.|And sometimes this is enough.
+2 Trains
+>2 I don't think about trains,
+>4 It's never really appealed to me.
+>0 I'd rather have a drunk railway conductor then a pilot.
+>2 Fewer directional skills needed.
+>0 Although I guess in the end it doesn't really matter to much to a dead
+>2 Person what is was they crashed and burned in.
+>4 I have an image of conductors and pilots.
+>0 I think if I had to see either one drunk I'd rather it be the pilot.
+>2 Just because my conductor image is to "grandpa" ish. 
+>4 It rather funny though.
+>0 I never knew either grandfather, and neither was a conductor.
+>2 One of the coolest things I ever saw
+>4 Was a guy who had built a HUGE
+>0 Railway set that encompassed his entire basement.
+>2 Each train had a tiny conductor.
+2 What I Love
+I love with all my heart the memories of warmth I have of days gone past.
+I love the tears I have shed unashamed in front of the harshest crowds.
+I love the way a smell can take you back to grandma, or sunny days, or rainy nights.
+I love the way my dad has always been a kid at heart and my mom a hopeless romantic.
+I love the way the world looks with your head tilted to the side, and the smiles that look back.
+And I love that I will find a million more things to love before I am through.
+2 When You're Happy
+<div class="center">
+stanza It's funny when you're a happy person there's not much to write about.|People in love can ooze about this or that.|Mad or angry people can ramble on for hours about their rages.|Depressed people often tend to get more depressed from writing it down, and so then have much more to write about.|Although what doesn't depress depressed people?|But if you're just happy it's like your thoughts just aren't nearly good enough to put down on paper.|Or maybe it's just that we don't want the mad and depressed people to get madder and more depressed because of our bliss.|Maybe we just don't want them to write anymore.
+</div>

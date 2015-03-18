4d61ce95c3e75ba7b113d973b163ec52430590a3
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:38:33 -0400

    Create Daughter_of_the_Vampire.pl

---
+++ Role_playing/Player_characters/Stories/Daughter_of_the_Vampire.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib "../../../files/lib";
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+2 The Kyllias' story
+One dark night, A<a woman|href="../#Myra_Kyllia"> rode into a town near the sea. Her eyes looked at everything around her with a dead stare. She rode past a jail near the edge of town and looked in. She smiled cruelly when she saw A<the morsel|href="../#Risa_Kyllia"> confined inside. The girl looked to be no older than twelve, and she was pretty. The woman crept into the building and charmed the guards to let her into the cell. Once inside the girl roused quickly and rolled to her feet in a defensive stance. The woman looked at the child with delight knowing that this child's terror will be heightened when she revealed herself to the waif.
+She stalked the girl around the cell and then leaped quickly to the child, but the child opened her mouth to scream yet nothing issued from those lips. The mouth had no tongue in it. For a moment a dull memory flooded into the mind of the vampire, a memory that lead her down a bitter road of deceit and lies. She looked at this child and saw something that reminded her all to painfully of the life she once had. It was in that moment that the vampiress decided that this morsel is no morsel at all. This child would grow up to not be bitter, if she is allowed to grow up at all.
+In the morning the woman went to the local authorities and tried to get the girl released. The men just looked at her with disgust.
+Q<Are you nuts! Lady, that girl got a whole bunch of adults killed out there in the woods. We won't let her go, she is to die for her crimes.> A couple of days earlier, Risa's party got killed except for her. She was left tied up and unconscious, but she was found without the bonds and in the midst of the dead party. She was accused of their deaths and sentenced to death.
+That set the woman off. She used her charms to get the girl released into her custody. Somehow that girl would grow up to love, something the woman was never allowed to do. Finding a loophole in the town's law, the woman got the girl released. She took the girl away from there. The girl never realized how close to death she was on two counts. The girl does not remember the incident in the cell, and she never found out that she was sentenced to death. She now has someone she is told will be her mother which suited the girl just fine.

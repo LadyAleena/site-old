e8eaaf24daef1b894855fa513ac51fd92ffa6947
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:22:33 -0400

    Create Brian_Hardie.pl

---
+++ Writing/Poetry/Other_poets/Brian_Hardie.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+Send a message to A<Brian Hardie|href="mailto:rose_angeles@hotmail.com">.
+2 Tomorrow Is Yesterday Is Today
+stanza Maybe the under garment was a diluted|Invite to sit in the rain. I'll take the gift|And be on my way to the anti-suppressant|Seminar. Tomorrow and the recent past are only|Of great consequence in the meaning of truth. Tall tales|Collide when my hair is below my eyes, requesting|Permission to be blown in the wind. Reduce chest pains|By taking the anemic side.|Replace the rubber love boat by sneaking up on the great white.|I'll sit on your porch and linger until a visitor comes to take|The silence away. Bring on the melodies under the surface,|And conduct an action of superior social livelihood. Bring down|The buildings, who ask the heavens for a little breathing space.|Walk past the racial differences in this October summer cold.|Bypass on feelings that concentrate on a friendless man in|Desolate bindings.|Forget punctuation and write a masterpiece.|Building blocks to a new failing future rip up the pages|That carries a childhood scent. Make public the little boy|On the sidewalk that opens his arms to any windshield wiper.|I'm too small to see when the resident explains.|Walks right through me and|The daily paper notes. I'll fast until I have to leave this|Criminal state of bliss. Walk to the empty room and leave the badge|Behind. Scribble your thoughts and keep a divine presence. Maybe the clouds|Will clear for my journey home.
+2 Affronted, Northeast Portland
+stanza Even, the basement was empty, the|Sounds of trickling would inject into the|Gutters. The sounds unpleasant to the leafless eye, apart|From the needs of yours truly, Mr. Destruction Enthusiast. What are these lips|Trying to say? Frankly, the elucidation does not ratify your sermon.|But who is to blame in the first place? Your Soul Provider? The crooning|Homeless just blocks away from your temporary residence?!|…I cower|…I crouch|…I crawl
+stanza Alone in the grass hills, the wall returning the ball. You|Call out to imaginary players, wishing, "vamoose, vamoose!" as|They randomly drop anchor.
+stanza Making headway, pushing on from unwashed carpets. If you feel|So obliged to understand, remember this: You are burning away|In a land of guilt, your feet are a Roman spring in the sand, all but to|Petrified to swim away. My friend, do not feel accompanied by gentle hands.|Exhale with half a breath and let the tide weep in your wake.
+2 Toxin-Laced Letter
+stanza Step into the light once|More for the benefit of your own speculation.|Down under the lights on the street fabricate|Your dawn-to-dark, destined to be taken in the mask|Of a growing dependency. Observe much? Shed the|Twilight in the making of times. Memories|To play for, when there are no more clouds to bombard.|I take an Image and make the weight gain more |to wake up the|Connecting temptation.|This slope of a process.|A spiral buried in the sand.|Troublemaker's sight|The gesture and awaken to the fountain of youth,|Fixing their tries. Are you ever inquisitive about what you channel?|The escape of a lost cause, defining the moments in unison, hanging up.|I encountered the same situation tonight under a black dome. Little holes,|Spectacles of light.|I came home a simple man.
+2 Earthbound Ghost Need
+stanza Incognito, with long hair in a green caravan.|She sits steady and speaks fast.|Building her future in front of a paused moment,|In the past.|The pale moist contains the key to a short life.|No more food to hide.|Witnesses perform a circus beyond the grave.
+stanza Glazing defeat.|Discovering the next country that will embrace the beast.
+stanza Chocolates tarnish the path to where I write.|I hold it to no god to inject fathom and insight.|"Live, and let live," deemed the up-town character.|"A whistle will clear the hills of killers you'll try to beat."
+stanza Rise above the subsided constraint, believe|You will keep the bar straight.|Cast aside the flame of doubt, and keep|The ashes to dream about.
+2 Eating Raw Bread With A Mammoth
+stanza The kitchen floors are covered with my memories.|Like possessions I left behind for friends to endure.|Their reasons unsure to me,|Though perfect legacies for their tough|2 bedrooms.|Oh, those thoughts could burn out|The lamp on those latent, patient fiends.|Springing a concern only to compose a lie.|Animal instinct almost hid the gun.|A models figure saved facts,|To sprout violence on low income.|Vanilla candles wont cure those tattoos.|Only if saving time leads inclination|To thirst for truth.|A seed, symmetrical of walls, dividing planted emotion.|Time lasts the time of losing weight.|Rib cages are puppets for my stomachs stress intake.|Transition needed on my peeling skin.|Wasting another take as I wade my mates in.
+2 Attracting Worms On July Something…
+stanza There's always an agenda in the Willow|Springs, there in Sin Territory.
+stanza It's not another drug bust,|My darling,|Been reading on the top,|Now scarring.
+stanza Taste the beatings of random visits;|I hear their delight with tragic, positive wits.
+stanza They watched from an angle,|To put us in the clear.|"Her bus stopped in L.A.,"|Some said, around here.
+stanza History is repeating itself, monotonous.|Trust there will be gay men adoring the both of us.|Trust itself…|Learn from the need of drugs.|Who is willing to walk to the corner of|6th and Pseudo Thugs?
+stanza The environment smells of new breeds in the back.|She would pull that knife out before she had more|Worms attract.
+stanza Sure thing to be the rose.|To castrate paranoia wont appetize, I suppose.
+2 Democratic Rash
+stanza I'm marinating naked, in the early mourning,|Preparing the frost to bite into the future|Of my brides trauma.
+stanza Leaking.|Yes, a word must describe the synthesis|Of my possessed polarity, scratching notes from a Polaroid era.
+stanza Since history has set their feet into|The Throne,|It hibernates. We can make|The stature racist and ill tempered|A sad willow in the wind.
+stanza Democratic Rash- I'll scratch if you spread.

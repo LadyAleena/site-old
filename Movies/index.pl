4a056809144d321b5bf45425a572b1a25a986260
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 15:06:08 -0400

    Create index.pl

---
+++ Movies/index.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+#This is the Movies index.
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+Hello and welcome to my lists of movies. My fiancé or I like some of them and might want to add them to A<our collection|href="../Collections/Movies.pl">. Others are those which I just felt like listing here. If I did not list a film, miniseries, or television series which you think I should have, contact me through one of the sources above.
+2 What is a movie?
+A movie is any motion picture no matter its length. A film is a movie which is normally 88 to 120 minutes long (without commercials). A miniseries is a movie shown in two or more parts with each part as long as a film, though not always. A television series is an episodic movie with each episode being under 50 minutes in length (again without commercials). A commercial is a movie which advertises a product or service. There are plenty of other movie forms such as music videos, webisodes, full web series (which I include in television series for now), and more. So if the picture is in motion on your screen, it is a movie.
+2 Movies by genre
+3 Family and children's movies
+I do not consider family or children a genre so movies which would be categorized as such elsewhere are not listed in those categories here. Those films will be mixed in with more adult movies.
+3 Horror movies
+You will see a lot of horror movies in this list. I like to read the stories in horror movies but don't watch many. My fiancé has watched more horror movies than I could handle. I prefer to just read the story lines especially in series of horror movies. You can read more about horror and monster movies and me A<here|href="Movie_opinions.pl#The_horror_genre_and_me">.
+3 Monster movies
+I have created a genre for just monster movies. Monsters are in so many speculative fiction genres, it is hard to say a monster is purely horror or fantasy or science fiction, with some monsters as super heroes. The monster genre is a mix of any paranormal or supernatural creatures from angels to zombies.
+3 Reality television series
+I do not like most of the reality television series today. My fiancé does, so I have added those he likes to my lists here. In my opinion most reality series today should be considered soap operas. Some of them started as a special on one channel or another, and they should have stayed that way. A once-off special of some of these subjects would interest me, however, a series of the same thing week after week gets old quickly especially if the series strays from the topic and focuses more on the people.
+2 Movies by series
+There are a mix of film series and television series in the lists on my A<movies by series|href="Movies_by_series.pl"> page. Some series include films, miniseries, and television series.
+3 Night of the Living Dead
+I feel the need to make a special note about the I<Night of the Living Dead>. With the original I<Night of the Living Dead> being public domain, many have tried to connect their films with it. Using the work of others, I have attempted to just make lists of the various series. Only three series are connected to the original I<Night of the Living Dead>. I collated my I<Night of the Living Dead> from the following sources: "A<Genealogy of the Living Dead: UPDATED!|href="http://horror-movies.livejournal.com/2106870.html">", "A<The Zombification Family Tree: Legacy of the Living Dead|href="www.popmatters.com/column/159439-legacy-of-the-living-dead/">", and "A<Zombi series|http://en.wikipedia.org/wiki/Zombi_series>" on Wikipedia.
+3 Remakes
+If a film is a remake of a film in a series, it is not added to the series since it does not share the same cosmology. A remake will be in a series if the remake has its own sequel or prequel. I may one day add lists of whole franchises I am interested in in the future.

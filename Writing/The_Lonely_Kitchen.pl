fd77d22875b463a00877c404a563fffdb9b7ab32
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:01:44 -0400

    Create The_Lonely_Kitchen.pl

---
+++ Writing/The_Lonely_Kitchen.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+Years ago I was preparing a belated Christmas dinner for my fiance and me. The kitchen was very quiet, and the lonelyness of that silence set into me. I really wanted to have someone to talk to then. I stood there chopping the onions while the stuffing heated in the microwave which hummed quietly behind me. I felt a great wave of sorrow come over me. Usually the silence doesn't bother me, but that day, while making a special occasion dinner which usually involves family, I had no one around. My fiance wasn't due home until late that night, which made it even sadder.
+I continued on with dinner, but with each new task the house seemed to close in around me. At one point I wondered why I was doing it at all. Why was I making this big dinner that will take us a week to really finish? (It did take us a week, with only 2 meals that were different a couple of nights.)
+There are times where being alone but for a significant other can be overwhelming, but the hardest times are those special occasions which are family centric. Am I the only lonely house"wife" that sometimes gets lonely on those days?

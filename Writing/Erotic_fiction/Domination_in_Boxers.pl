ef745c7bd3d4178e9183d4994962a092da513233
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:11:58 -0400

    Create Domination_in_Boxers.pl

---
+++ Writing/Erotic_fiction/Domination_in_Boxers.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+As she walked into the house, she noticed that the place was very clean. She nodded with satisfaction that her order had been obeyed. She had traveled far and did not want any disappointment. She looked her lover up and down to check if he had obeyed her in clothing choice as well. He did. He was wearing loose pants and a loose shirt. She nodded with approval and smiled up at him. She did not say a word. She didn't have to say anything at all. She walked briskly to a chair and sat down.
+Immediately he kneeled in front of her, took her shoes off, and started to massage her feet. She smiled down on him again. She laid her head back and enjoyed the massage.
+Without warning, her head shot up.
+"Stand beside me here." She said, pointing right beside the chair.
+"Now, take your penis out and let me suck on it for a while. Remember, if you allow even the least bit of liquid to come out of the tip, you will be punished."
+With her order he took his penis out of his pants and presented it to her. She took it in her mouth and sucked on it. That is all she did was sit there and suck on his penis for about one minute, and in that minute, it had become hard. She let it slip from her mouth, and she turned her head away from it. Nothing had come out, and she was satisfied.
+"Very good. Now, what have you made for us to eat this evening?" She asked.
+He told her that he had made a fine dinner of ham and other things. She was just barely listening. She extended her hand, and he helped her to her feet. He guided her to the chair at the head of the table and pulled the chair out for her. She sat down gracefully, and he poured her a glass of wine. She sipped on it while he brought the food out to the table. She watched him closely, scrutinizing every move he made. His penis was still hard but she could see it was going down.
+"Come over here and let me have your penis again before dinner."
+He obeyed instantly. She sucked on it until it was hard again, to her satisfaction, and he had yet to disobey her command. She let it slide out of her mouth slowly, and he started to serve the meal. While they ate dinner, they made polite conversation, and she asked how he was doing at work and with his part time schooling. She was impressed with his grades and when she had heard he got a promotion, she applauded him. He smiled with pleasure and blushed a light pink.
+After dinner they went back into the living room, and he took her bags to her room, while she was staying there. After he came back out to the living room, she sucked on his penis some more, and made sure it stayed nice and hard. He went into the kitchen got her another glass of wine, while he cleaned up the kitchen.
+When that was done, he sat on the floor next to her feet. She looked down at him and liked the way he was being tonight. He was very thoughtful and very deserving her praise.
+"I am going to get comfortable, and I would suggest you do the same." she said when she was done with her wine. He stood up first, and gave her his hand. She really didn't need help up, it was one thing he was trained to do. He lead her to her room, and then he went to his.
+He came out a few minutes later with his white boxers on, and she came out wearing only her robe. She sat down again, but before he could, she grabbed his arm, and made him stand next to her and present his penis to her again. It had gone down while he was changing, and she didn't want that to happen. She sucked on it for a few minutes, when cum dribbled out of it. She swallowed it, but she hit his butt to indicate that he was being punished for letting himself cum in her mouth. She then spread her lags, and he knelt between them. He gently brushed her pubic hairs away from her opening and lowered his head to pleasure her with his mouth.
+He took her clit into his mouth first and sucked on it until it was hard. He then licked it, making her gyrate in her seat. He could not touch her with his fingers, just his mouth and tongue. He let his tongue lick all over her sensitive area. He stuck his tongue inside of her and drank her juices flooding out of her. He lapped the juices up as if they were precious to him. He then made sure she was totally satisfied, licking her clit over and over again. During this time she played her foot over his penis, he had to stay hard. She came finally, and she gently pushed him away. He knelt in front of her, and she dismissed him to wash his face and mouth.

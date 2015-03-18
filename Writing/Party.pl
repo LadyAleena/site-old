fcc17f543dc4326d99510c3793bf3361c7c233af
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-18 16:00:49 -0400

    Create Party.pl

---
+++ Writing/Party.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA) });
+
+__DATA__
+It was October 29th around eight o'clock at night and a Holloween party appeared to be in progress. Many people were arriving. Vicky was talking to her best friend, Mirrium. The party was being thrown by her step-father, Earl; her mother; and her. The people were friends of her parents and some of her's too. The television was on in the living room showing horror movies, and spooky music was being played in the dining room. All the food was and most of the conversation were in the dining room and kitchen.
+Vicky and Mirrium went upstairs to Vicky's room. Mirrium said she really needed to talk with Vicky where they could have some peace and quiet. They were up there for about half an hour when Vicky screamed. The scream wasn't an ordinary scream of terror. It was a nerve racking, bone-chilling, ear-piercing scream that meant some horrific death had been dealt to the person who had screamed. One of the girls downstairs fainted. A few of them could not even move for they had become petrified in fear of what happened.
+Panicked, Vicky's parents ran up the stairs to try to find out what had happened. They reached to open the door, but somehow the knob was very hot. They started pounding on the door and calling for Mirrium to answer them, but they got no response. When the door knob finally cooled, it was locked. Vicky's mother started jerking at the door to get in but with no success.
+Mirrium could be heard crying quietly. It was as if she were unable to speak or even breathe. Mirrium then called out, "Tell everyone to get away from the door and out of the house." She stopped talking as if she were cut off.
+Earl called, "Why? What'll happen if we don't?"
+"I will die," Mirrium said.
+"Who'll kill you?" You're are in there all alone. I know that!"
+"You're wrong about being alone. This man here will kill me if you don't do as he wants. He already killed Vicky! Do you want me to get killed too?!"
+"No!"? wailed Vicky's mother. "Vicky can't be dead! She is so young. She coudn't've done anything to get her killed. We'll do anything he wants so I can see my daughter!"
+"Please go now! He's getting a little jittery with his knife."
+"All right, all right. We'll do it!" screamed her mother.
+So everyone went outside and to the side of the house. Nothing happened. They waited for an hour it seemed. They heard the door open, and someone walked out. The sound was not Mirrium's boots, but it sounded like Vicky's sandals. The sound of Mirriums boots followed. No one knew what was going on. Mirrium came around the corner; she looked paler than she normally did.
+A car door closed. He's escaping thought Earl. I can't let him get away.
+The car was started. Everyone rounded the corner to see Vicky at the wheel waving a sad good-bye to the world. Then she disappeared.
+The people went inside to find the body of the man who killed Vicky.

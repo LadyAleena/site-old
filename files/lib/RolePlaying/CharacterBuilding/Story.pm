6629dbc115131d04dc0dd0e6f076b5e70ed0ee62
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-20 01:35:23 -0400

    Create Story.pm

---
+++ files/lib/RolePlaying/CharacterBuilding/Story.pm
@@ -0,0 +1,30 @@
+package RolePlaying::CharacterBuilding::Story;
+use strict;
+use warnings;
+use Exporter qw(import);
+our @EXPORT_OK = qw(print_participants);
+
+use File::Basename;
+
+use Base::HTML::Element qw(list anchor);
+use Base::Data qw(get_hash);
+use Util::Convert qw(textify idify);
+use Util::Columns;
+
+my %stories = get_hash( 'file' => ['Role_playing/Player_characters','Stories.txt'], 'headings' => ['+'] );
+
+sub print_participants {
+  my $story = textify(basename($0));
+
+  my @participants = @{$stories{$story}};
+  my $number = get_columns(3,scalar @participants);
+  my @people;
+  for my $participant (@participants) {
+    my $participant_link = idify($participant);
+    push @people, anchor($participant, { 'href' => "../#$participant_link"} );
+  }
+
+  list(4,'u',\@people, { 'class' => "$number" });
+}
+
+1;

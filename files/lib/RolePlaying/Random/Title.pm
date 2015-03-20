edf9a686932baac7d40390f96344442d7abe2c3b
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-20 01:24:21 -0400

    Create Title.pm

---
+++ files/lib/RolePlaying/Random/Title.pm
@@ -0,0 +1,24 @@
+package RolePlaying::Random::Title;
+use strict;
+use warnings FATAL => qw(all);
+use Exporter qw(import);
+our @EXPORT_OK = qw(random_title);
+
+# part of the 'random' suite from RolePlaying::Random
+use RolePlaying::Random qw(random);
+
+my %titles = (
+  'noble male'   => [qw(emperor king prince archduke duke count viscount baron squire master lord)],
+  'noble female' => [qw(empress queen princess archduchess duchess countess viscountess baroness dame mistress lady)],
+  'military'     => [qw(marshal general colonel major captain lieutenant sergeant corporal private)],
+  'naval'        => [qw(admiral captain commander lieutenant ensign seaman)],
+  'government'   => [qw(president secretary governor commissioner mayor administrator manager)],
+);
+
+sub random_title {
+  my ($user_title) = @_;
+  my $title = random(\%titles, $user_title);
+  return $title;
+}
+
+1;

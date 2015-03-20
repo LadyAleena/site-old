8a9d68b0fafaca8638a64cb68d0e21702d0123df
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-20 01:24:41 -0400

    Create Water.pm

---
+++ files/lib/RolePlaying/Random/Water.pm
@@ -0,0 +1,22 @@
+package RolePlaying::Random::Water;
+use strict;
+use warnings FATAL => qw(all);
+use Exporter qw(import);
+our @EXPORT_OK = qw(random_water);
+
+# part of the 'random' suite from RolePlaying::Random
+use RolePlaying::Random qw(random);
+
+my %waters = (
+  'running'  => [qw(spring streamlet rivulet run brook creek stream estuary fjord river)],
+  'standing' => [qw(drop puddle pool pond lake lagoon bay sea bight sound ocean)],
+  'precipitation' => [qw(rain snow sleet hail)]
+);
+
+sub random_water {
+  my ($user_water) = @_;
+  my $water = random(\%waters, $user_water);
+  return $water;
+}
+
+1;

dfa9ac706a61e274745f9f3f5763b5b5f54f011a
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-20 00:52:33 -0400

    Create Number.pm

---
+++ files/lib/Util/Number.pm
@@ -0,0 +1,28 @@
+package Util::Number;
+use strict;
+use warnings FATAL => qw( all );
+use Exporter qw(import);
+our @EXPORT_OK = qw(commify round pretty_number);
+
+# commify, round, and pretty_number all make my numbers more readable.
+# commify was found in the perldocs to put commas in numbers.
+sub commify {
+  local $_  = shift;
+  1 while s/^([-+]?\d+)(\d{3})/$1,$2/;
+  return $_;
+}
+
+# sprintf written by james2vegas on PerlMonks.
+sub round {
+  my ($precision,$number) = @_;
+  my $rounded = $number =~ /\./ ? sprintf("%.${precision}f", $number) : $number;
+  return $rounded;
+}
+
+sub pretty_number {
+  my ($precision,$raw_number) = @_;
+  my $pretty_number = commify(round($precision,$raw_number));
+  return $pretty_number;  
+}
+
+1;

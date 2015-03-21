c4236a59638df2db555b8045c9beeaca88fc66fd
LadyAleena <LadyAleena@users.noreply.github.com>
2015-03-21 16:28:36 -0400

    Create Creche_protectors.pl

---
+++ Role_playing/Miscellany/Creche_protectors.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::HTML qw(html story);
+
+html( code => sub { story(*DATA); });
+
+__DATA__
+Crèche protectors are people who devote their lives to the care of children within their community. They often raise the children from infancy to adulthood. Since they stay within their communities, they are non-player characters usually. Crèche protectors who are not in their crèche are often on missions to retrieve children who have been kidnapped. Once the children are rescued, the crèche protectors will return to their crèche and stay there.
+Crèche protectors have an instinctual need to protect any child. If a protector is captured and placed amongst the captors' children, they will not be able to leave the children unguarded.
+One would think that being around children all day would make sex the last thing on crèche protectors' minds. The opposite is usually true. They are more libidinous, and when coupling they are far more sensual. In some societies, they are highly prized lovers.
+Crèche protectors can be any class except priests unless there is a god of protection, children, or a combination of the two. Those who are wizards usually study when the children are asleep or back with the parents. It is rare for crèche protectors to attain high levels.
+2 Crèche matron
+A crèche matron works within the crèche as a nurse and general caregiver of the children in the community. Women who leave their children here can not breast feed them, so a crèche matron will. These women become attached to the children, but not so attached the mothers have any worries. Crèche matrons are usually unmarried.
+These women are also the last line of defense of the children. When the crèche guards fail, it falls to the crèche matrons to protect the children. Sometimes they use spells which can make the children silent and invisible. They are not passive. Once spells are cast, they are fierce fighters. They will do whatever it takes to make sure the community's next generation is safe.
+2 Crèche guard
+A crèche guard protects the children of the community. These men and women are trained to do whatever it takes to make sure the children are safe. They are dirty fighters when it comes to tactics; however their kills are mostly bloodless. This is to insure the children remain as innocent as possible for as long as possible. They will learn the ills of life soon enough, but hopefully not this soon. Protecting the children in every aspect are these peoples lives. Some of them get so protective abusive parents know their wrath.
+Crèche guards have personalities ranging from strictly authoritarian to something akin to being a favorite uncle or aunt who makes the children laugh. Some are great entertainers, juggling or just telling silly jokes.
+2 Tutor defender
+These men and women are somewhere in between the matrons and the guards. They teach the older children in the crèche and are part of the crèche's defensive force.
+2 A story
+Xyrella wiped the blood and gore from her sword while sitting on a rock. The battle with the orcs had been fierce, but they kept them back. She heaved herself up to get new orders from the commander. What she really wanted to do was to go home and retrieve her daughter from the crèche. Upon further reflection, she may just leave Mordira there for the night when she gets back. She had no concerns over her daughter's safety. They have crèche matrons and guards. Not much could get through them.
+Back at the stronghold however, another battle had raged. Fralk, the orog in charge of the maneuver putting the fighting force of the stronghold out of position allowing him to take the place, strode though the hallways. The sounds of battle were everywhere, but it was heaviest around the children. Those bastards would not give easily. Some of them who were thought to be too frail to be a bother turned out to be the most dangerous. One old granny killed five of his men with a volley of magical missiles, and a walking "stick" seared four more of his men with a flattened plane of fire.
+He was very perplexed since he could not find the hostages he needed to get the humans to give him everything he wanted. Where were the children? He couldn't find them anywhere. He was frustrated, and in his frustration he absently kicked the foot of a dead orc. He looked at the body and couldn't find the way the orc died.
+He heard a moan behind him. He turned to see who it was. It was a human male with a wicked looking weapon. Fralk vaguely remembered something about some sort of guard these humans had. Something to do with their children. He decided he wanted to questions this one, so he had some men tie the human up to take back to the question later.
+When he heard the alarm the humans were returning, he grabbed up the unconscious human, and the orcs all retreated.

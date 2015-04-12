e6addeda397f27c0f7cc40067c4d90993899eccd
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-12 17:34:03 -0400

    Update Textbox.pl
    html function to page

--- admin/Textbox.pl
+++ admin/Textbox.pl
@@ -3,10 +3,10 @@ use strict;
 use warnings;
 
 use lib "../files/lib";
-use Base::HTML qw(html);
+use Base::Page qw(page);
 use Base::HTML::Element qw(form fieldset textarea);
 
-html( code => sub {
+page( 'code' => sub {
   form(3, sub {
     fieldset(4, sub {
       textarea(5,'', { name => 'the_text', rows => 25, cols => 0, style => 'margin:0; border-width:1px 2px; padding:0; width:99%; height:100%;' });

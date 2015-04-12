612d731ee6c2a1f776b6248a7f5b32a289adf225
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-12 17:23:02 -0400

    Update Uncategorized_poetry.pl
    html function to page

--- Writing/Poetry/Uncategorized_poetry.pl
+++ Writing/Poetry/Uncategorized_poetry.pl
@@ -5,9 +5,9 @@ use warnings FATAL => qw( all );
 use CGI::Carp qw(fatalsToBrowser);
 
 use lib '../../files/lib';
-use Base::HTML qw(html story);
+use Base::Page qw(page story);
 
-html( code => sub { story(*DATA) });
+page( 'code' => sub { story(*DATA) });
 
 __DATA__
 2 All

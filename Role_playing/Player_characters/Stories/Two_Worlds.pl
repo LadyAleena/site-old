657485e4aeb3150bb285c7c5331fe1e7f0637a89
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-12 16:06:03 -0400

    Update Two_Worlds.pl
    html function to page

--- Role_playing/Player_characters/Stories/Two_Worlds.pl
+++ Role_playing/Player_characters/Stories/Two_Worlds.pl
@@ -5,9 +5,9 @@ use warnings;
 use CGI::Carp qw(fatalsToBrowser);
 
 use lib "../../../files/lib";
-use Base::HTML qw(html story);
+use Base::Page qw(page story);
 
-html( code => sub { story(*DATA) });
+page( 'code' => sub { story(*DATA) });
 
 __DATA__
 A<Lamaria|href="../#Lamaria_Peacyle"> and A<Yenedra|href="../#Yenedra_Peacyle">, a human and an elf, a priestess and a thief, opposite in almost every way.

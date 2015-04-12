9b67a4d516104d2d88946ff3a5b75a9d21109661
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-12 15:41:13 -0400

    Update Clothing.pl
    html function to page

--- Role_playing/Magic_items/Clothing.pl
+++ Role_playing/Magic_items/Clothing.pl
@@ -5,10 +5,10 @@ use warnings FATAL => qw( all );
 use CGI::Carp qw(fatalsToBrowser);
 
 use lib '../../files/lib';
-use Base::HTML qw(html story);
+use Base::Page qw(page story);
 use Base::LineMagic qw($line_magic);
 
-html( 'heading' => 'Magical clothing', 'code' => sub { story(*DATA, { 'line magic' => $line_magic }) });
+page( 'heading' => 'Magical clothing', 'code' => sub { story(*DATA, { 'line magic' => $line_magic }) });
 
 __DATA__
 2 Cloak of Darkness

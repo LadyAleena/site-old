4651dff1046303c57c13a6d79157fd154bf7e974
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-12 15:50:47 -0400

    Update Weapons_of_the_Planes.pl
    html function to page

--- Role_playing/Magic_items/Weapons_of_the_Planes.pl
+++ Role_playing/Magic_items/Weapons_of_the_Planes.pl
@@ -5,10 +5,10 @@ use warnings FATAL => qw( all );
 use CGI::Carp qw(fatalsToBrowser);
 
 use lib '../../files/lib';
-use Base::HTML qw(html story);
+use Base::Page qw(page story);
 use Base::LineMagic qw($line_magic);
 
-html( code => sub { story(*DATA, { 'line magic' => $line_magic }) });
+page( 'code' => sub { story(*DATA, { 'line magic' => $line_magic }) });
 
 __DATA__
 by Robert Anderson and me

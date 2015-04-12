6b477655973663cd4307c2d5ff2b4b09992e9c41
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-12 17:07:25 -0400

    Update Dixon's_Night.pl
    html function to page

--- Writing/Erotic_fiction/Dixon's_Night.pl
+++ Writing/Erotic_fiction/Dixon's_Night.pl
@@ -5,9 +5,9 @@ use warnings FATAL => qw( all );
 use CGI::Carp qw(fatalsToBrowser);
 
 use lib '../../files/lib';
-use Base::HTML qw(html story);
+use Base::Page qw(page story);
 
-html( code => sub { story(*DATA) });
+page( 'code' => sub { story(*DATA) });
 
 __DATA__
 Anne invited Dixon over to her apartment to have dinner. Dixon didn't realize how tired he was & fell to sleep right after dinner. When he woke up he found his wrists tied to Anne's head board. He saw her sitting naked at the foot of the bed. As soon as she saw him focused on her, she stood up and slid a garter belt up her legs & over her hips, stockings followed. He told Anne that he loved the look of garter belts & stockings; it seemed Anne was granting him his wish to see them on her. Dix watched as Anne snapped the clips into place on the back of the stockings, but she never got around to snapping the ones in front. Anne crawled onto the bed over him allowing her breasts to slide up his legs. She kissed the tip of his growing phallus on her way up. The tip of his phallus traced a nearly straight line from her mouth, over her chin, down her neck, between her breasts, down her torso. Anne giggled a little bit as she wiggled the tip of his phallus in her belly button. She continued to let it slide over her tummy. Anne kissed Dix's lips before she sat up on his legs, letting his phallus rub gently on the hair surrounding her nether lips. Anne leaned over and kissed Dix's nipples, after each kiss attaching a garter belt clip to them. The clips gently pulled on them. As the clips pulled on his nipples, Dix hardened so fully he began to ache. Anne felt how hard he was and slid right down over him. When Anne rocked her body over his, the straps on the garter would become taut pulling on Dix's nipples making him pull on the cords. The sight & feel of Anne's stockinged legs on his & the clips attached to his nipples, Dix wasn't going to last long. Dix began to buck against Anne to penetrate her more deeply. They came together with erotic slaps as their flesh came together. Dix & Anne began to moan in unison. Anne's vagina began to squeeze Dix's phallus, lightly at first, but with every other stroke, Anne's vaginal contractions got harder & her vagina became tighter. Dix's phallus throbbed inside of her preparing to fire.

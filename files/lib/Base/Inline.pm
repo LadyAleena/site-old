f3f0897707f9b9c1207df5858ddde386824fa852
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-15 16:29:46 -0400

    Update Inline.pm
    clean out code which doesn't work in Perl 5.8.8

--- files/lib/Base/Inline.pm
+++ files/lib/Base/Inline.pm
@@ -4,6 +4,8 @@ use warnings;
 use Exporter qw(import);
 our @EXPORT_OK = qw(inline);
 
+# see also an alternative written by an Anonymous Monk on PerlMonks (http://www.perlmonks.org/?node_id=1028699)
+
 use Text::Balanced qw(extract_bracketed extract_multiple);
 
 use Util::FancySplice;
@@ -50,79 +52,3 @@ sub inline {
 }
 
 1;
-
-=cut
-# Written by an Anonymous Monk on PerlMonks (http://www.perlmonks.org/?node_id=1028699)
-
-use Test::More qw' no_plan ';
-use Regexp::Common qw/ balanced /;
-
-use vars qw/ $re_balanced_angles /;
-
-sub TRACE; sub DEBUG;
-*TRACE = *DEBUG = sub { print STDERR @_,"\n" };
-
-our $re_balanced_angles = qr{\<(?:(?>[^\<\>]+)|(??{ $re_balanced_angles }))*\>}x;
- 
-our $allowed = join '|', qw[
-A ABBR ACRONYM B BIG
-CITE CODE DFN EM I KBD Q SAMP SMALL
-SPAN STRONG SUB SUP TT VAR
-];
-
-sub inline {
-    local $_ = $_[0];
-    my $dent = $_[1] || 0;
-    pos = 0;
-    my $ret = "";
-inlineLOOP:
-    while( length > pos ){
-        m{\G(\s+)}gcsx and do {
-            $ret .= $1;
-            next inlineLOOP;
-        };
-        m{\G( $allowed )( $re_balanced_angles )}gcsx and do {
-            TRACE "# $dent allowed<> { $1 ( $2 ) }";
-            $ret .= inline_allowed( "$1" , "$2" , $dent );
-            next inlineLOOP;
-        };
-        m{\G([^<]+\s)}gcmx and do {
-            TRACE "# $dent text { $1 }";
-            $ret .= inline_text( "$1" );
-            next inlineLOOP;
-        };;;
-        m{\G([\<\>])}gcmx and do {
-            TRACE "## $dent error-stray<> { $1 } at pos(@{[pos]})";
-            last inlineLOOP;
-        };;;
-        m{\G(\S)}gcmx and do {
-            TRACE "# $dent inch-forward { $1 }";
-            $ret .= inline_text( "$1" );
-            next inlineLOOP;
-        };;;
-    }
-    $ret;
-}
-
-sub inline_allowed {
-    my( $tag , $stuff, $dent ) = @_;
-    $stuff = $1 if $stuff =~ m{^<(.*)>$}gs;
-    
-    my $ret = "";
-    $ret .= "<\L$tag\E" if $tag;
-    
-    $stuff =~ s{\|([^<>]+)$}{$ret .= " $1"; "";}gsex if defined $stuff ;
-    ## inline_allowed_atts($tag,$1);
-    $ret .= ">" if $tag;
-    
-    if( defined $stuff and length $stuff and $stuff =~ m{[<>]}g ){
-        $ret .= inline( $stuff , $dent+1) ; ## recurse
-    } else {
-        $ret .= $stuff;
-    }
-    
-    $ret .= "</\L$tag\E>" if $tag;
-    $ret;
-}
-
-sub inline_text { join'',@_ }

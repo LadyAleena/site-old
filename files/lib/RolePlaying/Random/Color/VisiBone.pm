aaffe73d1936fc29daa4b5714e212e47156b6fc2
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-20 16:48:58 -0400

    Update VisiBone.pm
    added the use of RolePlaying::Random::random, quoted unquoted items

--- files/lib/RolePlaying/Random/Color/VisiBone.pm
+++ files/lib/RolePlaying/Random/Color/VisiBone.pm
@@ -4,16 +4,20 @@ use warnings;
 use Exporter qw(import);
 our @EXPORT_OK = qw(VisiBone_color);
 
+# part of the 'random' suite from RolePlaying::Random
+use RolePlaying::Random qw(random);
+
 # HTML color from Web Designer's Color Reference Poster by VisiBone (http://www.visibone.com/color/poster4x.html)
 my %VisiBone_colors = (
-  brightness => [qw(white pale light medium dark obscure black)],
-  vividness => [qw(vivid hard faded dull weak grey)],
-  hue => [qw(red orange yellow spring green teal cyan azure blue violet magenta pink)],
+  'brightness' => [qw(white pale light medium dark obscure black)],
+  'vividness' => [qw(vivid hard faded dull weak grey)],
+  'hue' => [qw(red orange yellow spring green teal cyan azure blue violet magenta pink)],
 );
 
 sub random_VisiBone_color {
-  my $name = shift;
-  return $VisiBone_colors{$name}[rand @{$VisiBone_colors{$name}}];
+  my ($user_VB_color) = @_;
+  my $VB_color = random(\%VisiBone_colors, $user_VB_color);
+  return $VB_color;
 }
 
 sub VisiBone_color {
@@ -24,7 +28,7 @@ sub VisiBone_color {
   if ($brightness eq 'white'||$brightness eq 'black') {
     return $brightness;
   }
-  elsif ($vividness eq "grey") {
+  elsif ($vividness eq 'grey') {
     return "$brightness $vividness";
   }
   else {

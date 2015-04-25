c0c4262ddb8b67c31fbe17d40b7e4e82e611061d
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-25 18:57:35 -0400

    Update SpellList.pm
    get_hash returns a ref now

--- files/lib/RolePlaying/SpellList.pm
+++ files/lib/RolePlaying/SpellList.pm
@@ -12,7 +12,7 @@ use HTML::Elements qw(section rparagraph list);
 use Util::Convert qw(textify filify);
 
 my @headings = qw(name school level range duration area_of_effect components casting_time saving_throw description);
-my %spells = get_hash( 'file' => ['Role_playing','Spell_list.txt'], 'headings' => \@headings );
+my $spells = get_hash( 'file' => ['Role_playing','Spell_list.txt'], 'headings' => \@headings );
 
 sub print_spell {
   my ($heading,$spell) = @_;
@@ -20,23 +20,23 @@ sub print_spell {
   my @items;
   for my $stat (grep($_ !~ /(?:name|description)/,@headings)) {
     my $stat_text = ucfirst textify($stat);
-    push @items, qq(<strong>$stat_text:</strong> $spells{$spell}{$stat});
+    push @items, qq(<strong>$stat_text:</strong> $spells->{$spell}{$stat});
   }
 
   my $spell_file = filify($spell);
-  my $spell_description = $spells{$spell}{'description'} ?
-                          join("\n",map(rparagraph(5,$_),split(/\//,$spells{$spell}{'description'}))) :
+  my $spell_description = $spells->{$spell}{'description'} ?
+                          join("\n",map(rparagraph(5,$_),split(/\//,$spells->{$spell}{'description'}))) :
                           read_file(data_file('Role_playing/Spell_descriptions',"$spell_file.txt"))."\n";
 
   section(3, sub {
     list(5,'u',\@items, { class => 'spell_stats' });
     print $spell_description;
-  }, { 'heading' => [$heading, $spells{$spell}{'name'}] });
+  }, { 'heading' => [$heading, $spells->{$spell}{'name'}] });
 }
 
 sub print_spell_list {
-  my ($heading, $spells) = @_;
-  print_spell($heading, $_) for @$spells;
+  my ($heading, $ispells) = @_;
+  print_spell($heading, $_) for @$ispells;
 }
 
 1;

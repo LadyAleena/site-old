82ae4c48dc290021402e5745aa3e2506cff4c23d
LadyAleena <LadyAleena@users.noreply.github.com>
2015-04-26 16:19:26 -0400

    Update ExternalLinks.pm
    added title option to external_links and processing title

--- files/lib/Util/ExternalLinks.pm
+++ files/lib/Util/ExternalLinks.pm
@@ -7,7 +7,6 @@ our @EXPORT_OK = qw(external_link);
 
 use Base::Data qw(get_hash);
 use HTML::Elements qw(anchor);
-use Util::Convert qw(textify);
 
 my $external_links = get_hash( 'file' => ['Util','external_links.txt'], 'headings' => ['site','base link','after link'] );
 
@@ -19,7 +18,7 @@ sub external_link {
   my $link = $base_link.$page_link.$after_link;
   my $title = $link_title ? $link_title : undef;
   
-  return anchor(textify($site), { 'href' => "http://$link", 'title' => $title, 'target' => 'ex_tab' });
+  return anchor($site, { 'href' => "http://$link", 'title' => $title, 'target' => 'ex_tab' });
 }
 
 sub external_links {
@@ -27,8 +26,10 @@ sub external_links {
   
   my @external_links;
   for my $link (@$links) {
-    my ($site, $page_link) = @$link;
-    push @external_links, external_link($site, $page_link);
+    my ($site, $page_link, $title) = @$link;
+    next if !$external_links->{$site};
+    my $link_title = $site eq 'Google' ? "search Google for $title" : "$title on $site";
+    push @external_links, external_link($site, $page_link, $link_title);
   }
   
   return '<small class="links">('.join(', ',@external_links).')</small>';

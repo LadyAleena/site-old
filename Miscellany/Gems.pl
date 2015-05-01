01a11af7f198972d5e5820e164765c34303b72f7
LadyAleena <LadyAleena@users.noreply.github.com>
2015-05-01 15:48:54 -0400

    Update Gems.pl
    Added paragraph to print $alpha_menu

--- Miscellany/Gems.pl
+++ Miscellany/Gems.pl
@@ -8,7 +8,7 @@ use lib '../files/lib';
 use Base::Data qw(data_file alpha_array);
 use Base::Page qw(page);
 use Base::Menu qw(alpha_menu);
-use HTML::Elements qw(section list);
+use HTML::Elements qw(section paragraph list);
 use Util::Convert qw(searchify);
 use Util::Columns;
 use Util::ExternalLinks;
@@ -18,19 +18,19 @@ my @gems = <$gems_file>;
 chomp @gems;
 
 my %alpha_gems = alpha_array(\@gems);
-
 for my $gems (keys %alpha_gems) {
   my @list = @{$alpha_gems{$gems}};
   $alpha_gems{$gems} = undef;
   @{$alpha_gems{$gems}} = map { "$_ ".external_links([['Wikipedia', $_, $_],['Google', searchify($_), $_]]) } sort @list;
 }
+my $alpha_menu = alpha_menu(\%alpha_gems);
 
 page( 'code' => sub {
   for my $alpha (sort keys %alpha_gems) {
     my $class = get_columns(3, scalar @{$alpha_gems{$alpha}});
     section(3, sub {
       list(5, 'u', $alpha_gems{$alpha}, { 'class' => $class });
-      alpha_menu(4,\%alpha_gems);
+      paragraph(5, $alpha_menu, { 'class' => 'alpha_menu' });
     }, { 'heading' => [2, $alpha, { 'id'=> "section_$alpha" }] });
   }
 });

d94b4e6537993a65ed0e2f11c33bcc99eac0857e
LadyAleena <LadyAleena@users.noreply.github.com>
2015-05-01 15:50:40 -0400

    Update Module_list.pl
    added paragraph to print $alpha_menu

--- admin/Module_list.pl
+++ admin/Module_list.pl
@@ -9,53 +9,42 @@ use lib '../files/lib';
 use Base::Data qw(alpha_hash);
 use Base::Page qw(page);
 use Base::Menu qw(alpha_menu);
-use HTML::Elements qw(anchor section heading list);
+use HTML::Elements qw(section paragraph list anchor);
 
 my $filename = 'files/data/module_list.txt';
-
 my %files = (
   'fantasy' => "http://fantasy.xecu.net/$filename",
   'user'    => "http://users.xecu.net/fantasy/$filename",
-  'local' => "/home/me/Documents/fantasy/$filename",
+  'local'   => "http://localhost/$filename"
 );
 
 my %modules;
 for my $file (keys %files) {
-  my $work_file;
-  my @modules;
-
-  if ($file ne 'local') {
-    $work_file = get($files{$file});
-    @modules = split(/\n/,$work_file);
-  }
-  else {
-    open($work_file,'<',$files{$file}) || die "Can't open $files{$file}. $!";
-    @modules = <$work_file>;
-    chomp (@modules);
-  }
-
-  for my $module (@modules) {
-    push @{$modules{$module}}, $file;
-  }
+  my $work_file = get($files{$file});
+  my @lines = split(/\n/,$work_file);
+  push @{$modules{$_}}, $file for @lines;
 }
 
-my %styles = ('one' => 'fcc', 'two' => 'cfc', 'three' => 'ccf');
-
+my %styles = ('one' => 'none', 'two' => '#efe', 'three' => '#eef');
 my %alpha_modules = alpha_hash(\%modules);
+for my $alpha (keys %alpha_modules) {
+  my @items;
+  for my $module (sort keys %{$alpha_modules{$alpha}}) {
+    my $style = $styles{NUMWORDS(scalar @{$alpha_modules{$alpha}{$module}})};
+    my $link = anchor($module, { href => "http://search.cpan.org/perldoc?$module", target => 'ex_tab', title => "$module on CPAN" });
+    my $places = join(', ',sort @{$modules{$module}});
+    push @items, ["$link - $places", { 'style' => "background-color:$style" }]; 
+  }
+  $alpha_modules{$alpha} = undef;
+  $alpha_modules{$alpha} = \@items;
+}
+my $alpha_menu = alpha_menu(\%alpha_modules);
 
 page( 'code' => sub {
   for my $alpha (sort keys %alpha_modules) {
-    my $section_name = $alpha eq uc $alpha ? "$alpha" : "l$alpha";
-    my @items;
-    for my $module (sort keys %{$alpha_modules{$alpha}}) {
-      my $style = $styles{NUMWORDS(scalar @{$alpha_modules{$alpha}{$module}})};
-      my $link = anchor($module, { href => "http://search.cpan.org/perldoc?$module", target => '_blank', title => "$module on CPAN" });
-      my $places = join(', ',sort @{$modules{$module}});
-      push @items, ["$link - $places", { style => "background-color:#$style" }];
-    }
     section(3, sub {
-      list(3,'u',\@items);
-      alpha_menu(3,\%alpha_modules);
-    }, { 'heading' => [2,$alpha, { id => "section_$section_name" }]});
+      list(5, 'u', $alpha_modules{$alpha});
+      paragraph(5, $alpha_menu, { 'class' => 'alpha_menu' });
+    }, { 'heading' => [2, $alpha, { id => "section_$alpha" }]});
   }
 });

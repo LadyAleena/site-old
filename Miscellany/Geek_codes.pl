a4ed18d834d072379e14070a4342fddfbc5285a0
LadyAleena <LadyAleena@users.noreply.github.com>
2015-05-02 23:47:50 -0400

    Update Geek_codes.pl
    reworked the code a bit

--- Miscellany/Geek_codes.pl
+++ Miscellany/Geek_codes.pl
@@ -6,6 +6,43 @@ use CGI::Carp qw(fatalsToBrowser);
 use File::Slurp qw(read_file);
 
 use lib '../files/lib';
+use Base::Page qw(page story);
+use Base::Data qw(data_directory get_directory data_file);
+use Base::LineMagic qw($line_magic);
+use HTML::Elements qw(pre);
+use Util::Convert qw(textify);
+
+my $directory = 'Miscellany/Geek_codes';
+my @files = get_directory(data_directory($directory));
+
+my $doc_magic;
+for my $file (@files) {
+  $doc_magic->{textify($file)} = sub { pre(5,sub { print read_file(data_file($directory,$file)) })}
+}
+
+page( 'code' => sub { story(*DATA, { 'doc magic' => $doc_magic, 'line magic' => $line_magic }) });
+
+__DATA__
+Along with the ^Geek Code^ written by Robert Hayden, here are a few additions, use the standard pluses or minuses in each group.
+2 Babylon 5
+& Babylon 5
+2 Buffy the Vampire Slayer
+& Buffy the Vampire Slayer
+2 Star Trek
+& Star Trek
+2 Stargate
+& Stargate
+2 The X-Files
+& The X-Files
+2 Relationships
+& relationships#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+use File::Slurp qw(read_file);
+
+use lib '../files/lib';
 use Base::Data qw(data_directory get_directory);
 use Base::Page qw(page story);
 use Base::LineMagic qw($line_magic);

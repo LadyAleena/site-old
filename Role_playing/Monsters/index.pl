a9fc460c504f38f8b39ede9f35185d55579f76a2
LadyAleena <LadyAleena@users.noreply.github.com>
2015-05-06 03:32:15 -0400

    Update index.pl
    removed subroutines which should have been in modules and clean up

--- Role_playing/Monsters/index.pl
+++ Role_playing/Monsters/index.pl
@@ -5,7 +5,6 @@ use warnings FATAL => qw( all );
 
 use CGI::Minimal;
 use CGI::Carp qw(fatalsToBrowser);
-use File::Basename;
 use HTML::Entities qw(encode_entities);
 use Lingua::EN::Inflect qw(A);
 
@@ -15,38 +14,19 @@ use Base::Menu qw(file_menu);
 use HTML::Elements qw(nav section paragraph list anchor);
 use Util::Convert qw(textify searchify);
 use RolePlaying::Monster qw(print_monster);
-use RolePlaying::Random::Monster qw(random_monster);
+use RolePlaying::Random::Monster qw(random_monster random_monster_list);
 
 my $random_monster = A(random_monster);
 my @selects = ('Chaos elemental-kin', 'Daemar', 'Dark centaur', 'Demi-lycanthrope', 'Dragod', 'Eldent', 'Gobpry', 'Phase elemental', 'Rainbow dragonette', 'Throglin', 'Tralg', 'Trobold', 'Twarg', 'Zarden', 'Random monsters');
-my %multi_monsters = (
-  'Throglin' => ['Throglin','Freshwater throglin','Saltwater throglin'],
-  'Tralg'    => ['Arctic tralg','Desert tralg','Two-headed tralg'],
-  'Twarg'    => ['Hill twarg','Mountain twarg','Jungle twarg'],
-);
-
-sub monster {
-  my @monsters = ('<strong>Random monster:</strong> '.random_monster);
-  for my $monster (
-    'amorphous monster', 'amphibian monster', 'antherion', 'avian moster', qw(beholder centaur construct dragon),
-    'equine monster', qw(faerie gargantua giant goblinoid grell gremlin hag harpy humanoid illithid), 'insectoid monster',
-    'lycanthrope', 'medusa', 'planar creature', 'reptilian monster', qw(sphinx undead), 'underwater monster'
-  ) {
-    my $monster_text = textify($monster);
-    push @monsters, "<strong>Random $monster_text:</strong> ".random_monster($monster);
-  }
-  return \@monsters;
-}
 
 my $cgi = CGI::Minimal->new;
 my $select = $cgi->param('monster') ? encode_entities($cgi->param('monster'),'<>"') : undef;
-my $monsters = $select && $multi_monsters{$select} ? $multi_monsters{$select} : [$select];
 my $head = $select && grep(/\Q$select\E/, @selects) ? $select : undef;
 my $file_menu = file_menu('monster', \@selects , $select);
-page( heading => $head, 'file menu' => $file_menu, code => sub {
+page( 'heading' => $head, 'file menu' => $file_menu, code => sub {
   if ($select && grep(/\Q$select\E/, @selects)) {
-    print_monster($monsters) if $select ne 'Random monsters';
-    section(3, sub { list(5,'u', monster); }) if $select eq 'Random monsters';
+    print_monster($select) if $select ne 'Random monsters';
+    section(3, sub { list(5, 'u', random_monster_list) }) if $select eq 'Random monsters';
   }
   else {
     section (3, sub {

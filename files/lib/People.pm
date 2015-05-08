fce35e3dbf297d2ee4851f12fdf11f315ca8a20b
LadyAleena <LadyAleena@users.noreply.github.com>
2015-05-08 18:45:04 -0400

    Update People.pm
    condensed some of the code

--- files/lib/People.pm
+++ files/lib/People.pm
@@ -9,26 +9,18 @@ use Util::Sort qw(name_sort);
 use Util::Convert qw(filify searchify);
 use Util::ExternalLinks;
 
-sub split_line {
-  my ($raw_line) = @_;
-  my ($name,$wikipage) = split(/\|/,$raw_line);
-  return [$name,$wikipage];
-}
-
 sub get_people {
   my ($file) = @_;
 
   open(my $data_file,'<',data_file('People',$file));
-  my @data = <$data_file>;
-  chomp(@data);
-  my @base_people = map { split_line($_) } @data;
+  my @base_people = map { chomp $_; [ split(/\|/, $_) ] } <$data_file>;
   
   my @people;
-  for my $line (sort { name_sort($a->[0],$b->[0]) } @base_people) {
+  for my $line (sort { name_sort($a->[0], $b->[0]) } @base_people) {
     my $name = $line->[0];
        $name =~ s/^_//;
     my $article_name = $line->[1] ? $line->[1] : filify($name);
-    my $link = external_links([['Wikipedia', $article_name, $name],['Google', searchify($name), $name]]);
+    my $link = external_links([['Wikipedia', $article_name, $name], ['Google', searchify($name), $name]]);
     push @people, "$name $link";
   }
   

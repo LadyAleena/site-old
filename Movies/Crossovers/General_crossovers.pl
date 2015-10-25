6f6b612c99e1e231369871e2ebd95fc47736189b
LadyAleena <LadyAleena@users.noreply.github.com>
2015-10-25 05:39:27 -0400

    Create General_crossovers.pl

---
+++ Movies/Crossovers/General_crossovers.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../files/lib';
+use Base::Page qw(page story);
+
+page( 'code' => sub { story(*DATA) });
+
+__DATA__
+These crossovers are completely contained within themselves. They are not part of any bigger continuity.
+2 Babylon 5
+from TNT and Nickelodeon
+Before I<A<Babylon 5|href="Movies_by_series.pl?series=Babylon+5#Babylon_5">> spun off I<A<Crusade|href="Movies_by_series.pl?series=Babylon+5#Crusade">>, in the episode "There All Honor Lies", Captain John Sheridan is presented with a teddy bear which is supposed to be a representation of him. He disapproves and throws it out of an airlock. The teddy bear ends up on I<Space Cases> "Who Goes Where".
+2 Ghostbusters and Casper
+I<Casper> crossed with I<Ghostbusters>. I<Ghostbusters> spun off I<Ghostbusters II>.

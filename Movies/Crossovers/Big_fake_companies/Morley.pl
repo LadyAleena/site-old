8e318ec1c15e44f700565756951c1134455833f0
LadyAleena <LadyAleena@users.noreply.github.com>
2015-11-23 19:13:09 -0500

    Create Morley.pl

---
+++ Movies/Crossovers/Big_fake_companies/Morley.pl
@@ -0,0 +1,99 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../../files/lib';
+use Base::Page qw(page story);
+
+page( 'code' => sub { story(*DATA) });
+
+__DATA__
+B<Morley> became popular with its use in I<The X-Files> however the earliest known use of the brand name is in I<Psycho>. Not all series which mention Morley cross over with each other. One note of caution, the list of series was compiled on A<Wikipedia|href="https://en.wikipedia.org/wiki/Morley_(cigarette)">, so some of these may not even contain Morley cigarettes (or similarly designed cigarette pack).
+
+2 Etruscan horse
+The Etruscan horse Morley was the first package design introduced, possibly on I<Psycho>. Anything using this design could be considered a crossover with I<Psycho>.
+* I<Psycho> (16 June 1960)
+* I<Naked City> Q<Tombstone for a Derelict> (5 April 1961)
+* I<Mission: Impossible> Q<Operation 'Heart'> (22 October 1967)
+* I<Mannix> Q<All Around the Money Tree> (22 February 1969)
+* I<Seinfeld> Q<The Invitations> (16 May 1996)
+* I<Friends> Q<The One Where Rachel Smokes>  (8 April 1999)
+
+2 Red box Morley
+The red box Morley pack design first appeared on I<Beverly Hills 90210>. Since I<The X-Files> became part of popular culture, Morley is used to create crossovers by enthusiasts. 
+3 Television
+* I<Beverly Hills 90210> Q<The Back Story> (16 September 1992)
+* I<Space: Above and Beyond> Q<Ray Butts> (22 October 1995)
+* I<Buffy the Vampire Slayer> Q<School Hard> (29 September 1997)
+* I<Nash Bridges> Q<Power Play> (16 April 1999)
+* I<That 70s Show> Q<Eric Gets Suspended> (30 September 1999)
+* I<Becker> Q<Santa On Ice> (13 December 1999)
+* I<Malcolm in the Middle> Q<Stock Car Races> (2 April 2000)
+* I<Special Unit 2> Q<The Grain> (3 October 2001)
+* I<ER> Q<Beyond Repair> (10 January 2002)
+* I<Jake 2.0> Q<Arms and the Girl> (1 October 2003)
+* I<Cold Case>
+** Q<The Plan> (2 May 2004)
+** Q<The Sleepover> (7 November 2004)
+* I<Everybody Hates Chris> Q<Everybody Hates Playboy> (27 April 2006)
+* I<Huff> Q<Radio Silence> (21 May 2006)
+* I<Criminal Minds> Q<North Mammon> (1 November 2006)
+* I<Prison Break> Q<Bolshoi Booze> (11 November 2006)
+* I<Breaking Bad> pilot (20 January 2008)
+* I<Medium>
+** Q<To Have and to Hold> (21 January 2008)
+** Q<The First Bite is the Deepest> (18 May 2009)
+* I<New Amsterdam> Q<Love Hurts> (14 April 2008)
+* I<Reaper> Q<The Devil and Sam Oliver> (26 May 2008)
+* I<Saving Grace> Q<It's a Fierce, White-Hot, Mighty Love> (4 August 2008)
+* I<Sordid Lives: The Series> Q<Pills, Poison and Penises> (6 August 2008)
+* I<Burn Notice>
+** Q<Scatter Point> (7 August 2008)
+** Q<Fearless Leader> (25 June 2009)
+** Q<Blind Spot> (19 August 2010)
+** Q<Square One> (21 July 2011)
+** Q<Reckoning> (12 September 2013)
+* I<Heroes> Q<One of Us, One of Them> (29 September 2008)
+* I<Pushing Daisies> Q<Bad Habits> (15 October 2008)
+* I<Californication>
+** Q<The Raw & the Cooked> (19 October 2008)
+** Q<Waiting for the Miracle> (29 January 2012)
+* I<United States of Tara> pilot (18 January 2009)
+* I<24> Q<Day 8: 9:00 p.m. – 10:00 p.m.>  (25 January 2010)
+* I<American Horror Story: Murder House> Q<Birth> (25 January 2010)
+* I<Lost> Q<LA X> (2 February 2010)
+* I<Walking Dead>
+** Q<Save the Last One> (30 October 2011)
+** Q<Consumed> (16 November 2014)
+** Q<JSS> (18 October 2015)
+* I<The Middle> Q<The Map> (11 January 2012)
+* I<Shameless> Q<I'll Light a Candle for You Every Day> (22 January 2012)
+* I<Justified> Q<Riverbrook>  (23 March 2010)
+* I<Warehouse 13> Q<The Ones You Love> (24 September 2012)
+* I<Kingdom> Q<Please Refrain from Crying> (12 November 2014)
+3 Film
+* I<The World's End> (10 July 2013)
+
+2 Other pack designs
+The Etruscan horse and Marlboro look-alike designs are not the only ones out there.
+* I<Killer Instinct> Q<Game Over> (11 November 2005) has a pack which is red with a horizontal white stripe.
+* I<The L-Word> Q<Lap Dance> (27 February 2005) and Q<Lunar Cycle> (16 March 2008) has packs which look a lot like the red box Morley, but Morley is spelled in all capital letters where the red box is in title case.
+* I<The Strain> Q<The Third Rail> (21 September 2014) has a pack which is white with vertical gold stripes.
+
+2 Not Morley
+It could be there are those creatives who know about the popularity of the Morley brand and want to distance themselves from it, so use another name on the packaging. On I<Frasier> Q<Morning Becomes Entertainment> (6 Apirl 2000) the pack looks like Morley, but the company name is Adler.
+
+2 Seen but unclear
+Sometimes there is a pack which looks like Morley, but the angle or distance of the shot makes it impossible to tell if the pack says Morley or not.
+* I<Weeds> Q<The Brick Dance> (27 August 2007)
+* I<The Americans> pilot (30 January 2011)
+* I<Up All Night> Q<I Can't Quit You> (6 December 2012) has cigarettes but no sign of Morley.
+* I<Orange is the New Black> Q<Tall Men with Feelings> (11 July 2013)
+
+2 Heard but not seen
+Morley could be mentioned without ever showing a pack of cigarettes, making it impossible to tell where the series connects. On I<Judging Amy> Q<Can They Do That With Vegetables?> (5 February 2002) the company is mentioned, but no pack is shown.
+
+2 Not cigarettes
+No matter the pack design, I<The Dick Van Dyke Show> Q<Sally is a Girl> (19 December 1961) will not cross with any of the above groups. From the description of the appearance, the cigarettes are candy.

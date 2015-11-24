56324dd06328f115ffa34f63e51d3bc296d6dbde
LadyAleena <LadyAleena@users.noreply.github.com>
2015-11-23 19:13:31 -0500

    Create Oceanic_Airlines.pl

---
+++ Movies/Crossovers/Big_fake_companies/Oceanic_Airlines.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../../files/lib';
+use Base::Page qw(page story);
+
+page( 'code' => sub { story(*DATA) });
+
+__DATA__
+B<Oceanic Airlines> became popular when I<Lost> premiered, though the airline existed for at least a decade prior to I<Lost>. Not all series which mention Oceanic Airlines cross over with each other. One note of caution, the list of series was compiled on A<Wikipedia|href="https://en.wikipedia.org/wiki/Oceanic_Airlines">, so some of these may not even contain Oceanic Airlines (or similarly named airlines). You can also read A<Poobala's article|href="http://www.poobala.com/bfc.html" target="new">.
+
+2 Stock footage
+When money is in short supply, going out and getting fresh footage could break the bank, so reusing footage from other sources is the way to go. The use of stock footage does not constitute a crossover, unless the events in the original are referenced specifically. So while these all contain footage from I<Executive Decision>, they do not cross over with it or anything else which uses the same footage.
+* I<Executive Decision> released. (15 March 1996)
+* I<Diagnosis: Murder> Q<Murder in the Air> (24 April 1997)
+* I<JAG> Q<Vanished> (28 October 1997) and Q<The Bridge at Kang So Ri> (29 February 2000)
+* I<Category 6: Day of Destruction> (14 November 2004)
+* I<The War at Home> Q<The West Palm Beach Story> (16 April 2006)
+
+2 Before Lost
+These series mention Oceanic, but these appearances of Oceanic were prior to I<Lost>, so do not count as crossovers with it. How could there be any intention of crossing with I<Lost> when it was not even on television yet nor in popular culture?
+* I<Flipper> Q<The Ditching> (30 October 1965)
+* I<The Pretender> Q<Flyer> (19 October 1996)
+* I<The X-Files> Q<Synchrony> (13 April 1997)
+* I<Crossing Jordan> Q<Conspiracy> (17 March 2003): Just a mention, no logo seen.
+
+2 Lost's Oceanic Airlines
+Once I<Lost> premiered and became entrenched into popular culture, it was inevitable some would want to include it in their series whenever they needed an airline. Also, being on the same network means it is easier to get the rights to use the logo and name of I<Lost>'s Oceanic Airlines. When it comes to I<Chuck> from NBC, I read a good argument to merit its inclusion on this list. I<LAX>, also from NBC, had a similarly good argument.
+* I<Lost> premiered. (22 September 2004)
+* I<Alias> Q<A Clean Conscience> (27 April 2005)
+* I<LAX> Q<Senator's Daughter> (16 April 2006)
+* I<Chuck> Q<Chuck Versus the Helicopter> (1 October 2007)
+* I<FlashForward> Q<No More Good Days> (24 September 2009)
+* I<Once Upon A Time> Q<The Stranger> (29 April 2012)
+
+2 Oceanic Air
+The airline on these series is Oceanic Air not Oceanic Airlines. However, these could be considered a crossover since the font used in both is nearly identical to the naked eye.
+* I<Fringe> Q<The Dreamscape> (25 November 2008)
+* I<Castle> Q<In Plane Sight> (27 April 2015)
+
+2 Different Oceanic logo
+The logo for Oceanic Airlines is not the same as seen on I<Lost>.
+* I<The Goldbergs> Q<The President's Fitness Test> (1 April 2014)
+
+2 Animated
+Animated series do not form crossovers with live action series unless the animated series is considered canon with a live action series which already crossed over. It does not matter if the Oceanic Airlines logo and name are an exact match to any of the above, there is no crossover.
+* I<Futurama> Q<Möbius Dick> (4 August 2011)
+* I<Transformers: Cybertron> Q<Inferno> (15 January 2005)
+
+2 Unknown Oceanic Airlines connection
+* I<Pushing Daisies> Q<Pie-lette> (2 October 2007)
+* I<Up All Night> Q<Travel Day> (16 February 2012)
+* I<White Collar> Q<Whack-A-Mole> (11 December 2014): The logo was never shown on screen, so it is unclear where this series lies.
+* I<Zero Hour>

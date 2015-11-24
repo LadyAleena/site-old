9469002ae3f969596c4261b91e16aa8b8bd0c5bd
LadyAleena <LadyAleena@users.noreply.github.com>
2015-11-23 19:08:04 -0500

    Update index.pl

--- Movies/Crossovers/index.pl
+++ Movies/Crossovers/index.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+#This is the Crossovers index.
 use strict;
 use warnings FATAL => qw( all );
 
@@ -10,38 +11,34 @@ use Base::Page qw(page story);
 page( 'code' => sub { story(*DATA) });
 
 __DATA__
-I hope you enjoy these crossovers. I included official, unofficial, and disputed crossovers. There are two sites which helped me in putting this together: Thomas Holbrook's site A<Crossovers and Spin Offs|href="http://www.poobala.com/crossoverlistb.html" target="new">, Keith Gow's and Ash Crowe's site A<The Tommy Westphall Universe|href="https://thetommywestphall.wordpress.com" target="new">, and Robert E. Wronski Jr.'s A<Television Crossover Universe|href="http://www.televisioncrossoveruniverse.com" target="new">. Without those resources, I would not be as far along as I am now. I am very thankful for their work even thought there are a few places where I disagree with Holbrook, Gow, Crowe, or Wronski.
-There are some crossover continuities which are not covered here because they are well covered elsewhere such as Stephen King's and Quentin Tarantino's separate universes, Kevin Smith's View Askewniverse, and the Pixar Theory. If my research connects a continuity with the ones currently here, they will be included then. If Joss Whedon sneaks a mention, either written or verbal, to Weyland-Yutani on I<Agents of S.H.I.E.L.D.>, it would bring the Marvel Cinematic Universe into the Westphall universe.
+I hope you enjoy these crossovers. I included official, unofficial, and disputed crossovers. There are several sites which helped me in putting this together: Thomas Holbrook's site A<Crossovers and Spin Offs|href="http://www.poobala.com/crossoverlistb.html" target="new"> and Keith Gow's and Ash Crowe's site A<The Tommy Westphall Universe|href="https://thetommywestphall.wordpress.com" target="new">. Recently I came across Robert E. Wronski Jr.'s A<Television Crossover Universe|href="http://www.televisioncrossoveruniverse.com" target="new"> which has given me a few insights. Without those resources, I would not be as far along as I am now. I am very thankful for their work even thought there are a few places where I disagree with Holbrook, Gow, Crowe, or Wronski.
+I have been working on A<verifying and adding to|href="Westphall_crossovers.pl"> or A<severing|href="Westphall_non-crossovers.pl"> Westphall crossovers.
+There are some crossover continuities which are not covered here because they are well covered elsewhere such as Stephen King's and Quentin Tarantino's separate universes, Kevin Smith's View Askewniverse, and the Pixar Theory. If my research connects a continuity with the ones currently here, they will be included then.
+I will not mention all of the character crossovers between series in a franchise. So, I will not break down all of the characters who crossed between the various I<Stak Trek>, I<Law & Order>, or I<Doctor Who> franchises. I may mention characters who crossed over between the franchises within the Wolf universe.
 The crossovers mostly are in chronological order by the episodes' air dates.
 I have many personal rules I use to determine if two series crossed over or not. I am pretty strict with them, however I have made a few exceptions.
 2 TV series not considered
-I do not consider crossovers with the follow types of TV series and episodes...
-* animated series (with the exception of A<I<Star Trek> (1973)|href="Movies_by_series.pl?series=Star+Trek#Star_Trek_1973">)
-* anthology series such as I<The Twilight Zone>, I<The Outer Limits>, and I<Love, American Style>
-* reality series such as I<COPS>.
-* sketch comedy series such as I<Saturday Night Live>
-* variety series such as I<The Brady Bunch Variety Hour>
-* parody episodes
-* dream episodes SMALL<(Yes, I know about Tommy Westphall from I<St. Elsewhere> and Dr. Robert Hartley from I<The Bob Newhart Show> dreaming I<Newhart>.)>
+I do not consider crossovers with the following types of films, TV series, and episodes...
+* Animated series do not form crossovers with live action series unless the animated series is considered canon with a live action series which already crossed over.
+* Television series without a continuing story throughout the entire run of the series
+** Anthology series such as I<The Twilight Zone>, I<The Outer Limits>, and I<Love, American Style>
+** Sketch comedy series such as I<Saturday Night Live>
+** Variety series such as I<The Brady Bunch Variety Hour>
+* Reality series such as I<COPS>.
+* Parody episodes
+* Dream episodes SMALL<(Yes, I know about Tommy Westphall from I<St. Elsewhere> and Dr. Robert Hartley from I<The Bob Newhart Show> dreaming I<Newhart>.)>
+* Non-contiguous parent or child films and television series (however they may still be noted)
 2 People not considered
 People appearing as themselves and historical figures are not considered for crossovers. When Jon Cryer, Charlie Sheen, and Angus Jones appeared on I<CSI: Crime Scene Investigation> Q<Two and a Half Deaths> as themselves, not their characters from I<Two and a Half Men>, the series did not cross. Mark Twain visiting the Enterprise-D on I<Star Trek: The Next Generation> Q<Time's Arrow> will not create a crossover with any other series where Mark Twain is a character.
 2 Reference specificity
 The more specific a reference, the better chance I will consider it a crossover.
 When a reference is specific, a crossover did occur. When the titular I<Luther> mentions John Munch by name and specifically mentions where he works, the SVU; a crossover occurred with I<Law & Order: Special Victims Unit>.
 When a reference is vague, a crossover did not occur. On I<Haven> Q<The Trial of Audrey Parker>, Agent Audrey Parker mentions an agent Agent Byron Howard trained who was chasing aliens, a crossover did not occur with I<The X-Files>. Had Agents Fox Mulder, Dana Scully, or any other agent from I<The X-Files> been mentioned by name, this would make a cross, however, the reference is too vague to be legitimate. I would have allowed it if any of the writers or the director had been involved with I<The X-Files>.
-2 Crossovers between series in a franchise
-I will not mention all of the character crossovers between series in a franchise. So, I will not go through a break down all of the characters who crossed between the various I<Stak Trek>, I<Law & Order>, or I<Doctor Who> franchises. I may mention characters who crossed over between the franchises within the Wolf universe.
+Reference specificity also includes the use of names. If the names are not the same, then no crossover occured unless the name change is addressed directly on screen or by the creators of the crossover.
+A<Big (and little) fake companies|href="Big_fake_companies"> fall under this rule.
 2 Stock footage
-If two series only share stock footage, it does not mean they crossed over. However, if the events in the stock footage are mentioned, it just might mean they crossed over.
-2 Crossovers with big fake companies
-When creating a crossover between two series with a A<big fake company|href="http://www.poobala.com/bfc.html" target="new">, it might be good to apply a reality check. The biggest hint the two companies are not the same is the name of the company. Companies are sticklers when it comes to the use of its company name. So no matter how close the names are, they are not the same company. Oceanic Airlines from I<Lost> would not truncate their company name on their tickets, meaning Oceanic Air from I<A<Fringe|href="Movies_by_series.pl?series=Fringe">> is not the same company. Also, mentioning B<a> paper factory in Slough on I<Lost> does not mean it is A<Wernham Hogg from I<The Office> (U.K.)|href="http://www.poobala.com/lostandoffice.html">.
-The next biggest is the logos. If they are not the same, then there is no cross. Companies spend millions on their logos and signage. When a company changes its logo, it would cost millions, so companies do not change their logos often if ever. A real world example of where a logo is changed is the McDonald's in Sedona, Arizona. The town required McDonald's to change its logo's color because the golden arches would clash with the surrounding architecture, however, the shape of the arch is the same. In the rest of the world, the arches are gold.
-3 Companies to double check
-* B<Morley> has four different pack designs floating out there, and they appeared way before they appeared on I<The X-Files>. The earliest sighting was in the film I<Psycho>.
-* B<Oceanic Airlines> - see A<Poobala's article|href="http://www.poobala.com/bfc.html" target="new">.
-* B<Yoyodyne> - see A<Poobala's article|href="http://www.poobala.com/bfc.html" target="new">.
-* B<Trans Global Airlines> - pending
-* B<Lariat Rent-A-Car> has four different logos out there, so it is more than likely the companies are not the same.
-3 Companies which are safe
-* B<Doublemeat Palace> was first seen on I<A<Buffy the Vampire Slayer|href="Movies_by_series.pl?series=Buffy+the+Vampire+Slayer">> Q<Doublemeat Palace>. It was only mentioned in I<Dexter> Q<Truth Be Told>. It was used again in I<A<Andy Barker, P.I.|href="Movies_by_series.pl?series=Andy+Barker,+P.I.">> Q<Fairway, My Lovely>.
-* B<Weyland-Yutani> - see A<Poobala's article|href="http://www.poobala.com/bfc.html" target="new">. (I am still waiting to see if it appears in the Marvel Cinematic Universe.)
+If two series only share stock footage, it does not mean they crossed over. However, if the events in the stock footage are mentioned, it just might create a crossover.
+2 Good reason
+After all of my other rules are applied, there are some crossovers which would be considered too vague to count. Sometimes I come across a good reason to allow the crossover. So if the argument for the crossover is good enough, I will include it.
+2 I feel like it
+And after having a good reason, sometimes I just feel like saying two series crossed over with each other. The proof may be scant, but sometimes I just decide to add it.

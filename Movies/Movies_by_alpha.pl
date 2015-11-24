6acf307918dbbbb53ddbfb7284fb5c3620901be6
LadyAleena <LadyAleena@users.noreply.github.com>
2015-11-23 19:05:50 -0500

    Update Movies_by_alpha.pl
    changed "based on" to "source" and removed "format" from search

--- Movies/Movies_by_alpha.pl
+++ Movies/Movies_by_alpha.pl
@@ -17,28 +17,26 @@ use Util::GrammaticalJoin;
 use Movie qw(movie option display_movie display_option start_year end_year);
 
 my $cgi = CGI::Minimal->new;
-my $year   = $cgi->param('year')     ? encode_entities($cgi->param('year'),     '<>"') : '';
-my $media  = $cgi->param('media')    ? encode_entities($cgi->param('media'),    '<>"') : '';
-my $format = $cgi->param('format')   ? encode_entities($cgi->param('format'),   '<>"') : '';
-my $genre  = $cgi->param('genre')    ? encode_entities($cgi->param('genre'),    '<>"') : '';
-my $basis  = $cgi->param('based on') ? encode_entities($cgi->param('based on'), '<>"') : '';
-my $title  = $cgi->param('title')    ? encode_entities($cgi->param('title'),    '<>"') : '';
+my $year   = $cgi->param('year')   ? encode_entities($cgi->param('year'),   '<>"') : '';
+my $media  = $cgi->param('media')  ? encode_entities($cgi->param('media'),  '<>"') : '';
+my $genre  = $cgi->param('genre')  ? encode_entities($cgi->param('genre'),  '<>"') : '';
+my $source = $cgi->param('source') ? encode_entities($cgi->param('source'), '<>"') : '';
+my $title  = $cgi->param('title')  ? encode_entities($cgi->param('title'),  '<>"') : '';
 
-my @selects = (qw(year media format genre), 'based on');
+my @selects = (qw(year media genre), 'source');
 my %select_options;
 $select_options{$_} = option($_) for @selects;
 
 my $movies_data = movie('data');
 my $search;
 my @movies;
-if ( $year || $media || $genre || $format || $basis || $title ) {
+if ( $year || $media || $genre || $source || $title ) {
   my @selections;
   push @selections, qq($title)  if $title;
   push @selections, qq($year)   if $year;
   push @selections, qq($media)  if $media;
-  push @selections, qq($format) if $format;
   push @selections, qq($genre)  if $genre;
-  push @selections, qq($basis)  if $basis;
+  push @selections, qq($source) if $source;
   
   my $selection = @selections ? grammatical_join('and',map("<b>$_</b>",@selections)) : '';
   my $selection_text = $selection;
@@ -63,8 +61,7 @@ if ( $year || $media || $genre || $format || $basis || $title ) {
     }
     next if $media  && $movie->{'media'} ne $media;
     next if $genre  && !$movie->{'genre'}{$genre};
-    next if $format && (!$movie->{'format'}   || !$movie->{'format'}{$format});
-    next if $basis  && (!$movie->{'based on'} || $movie->{'based on'} !~ /$basis/i);
+    next if $source && (!$movie->{'source'} || $movie->{'source'} !~ /$source/i);
     next if $title  && lc($movie->{'title'}) !~ /\L$title\E/i;
 
     push @movies, $movie->{'title'};
@@ -77,7 +74,7 @@ else {
 
 my %alpha_movies = alpha_array(\@movies);
 for my $movies (keys %alpha_movies) {
-  my @movies = map(display_movie($movies_data->{$_}, { 'series' => 1, 'links' => 1 }), sort { article_sort(lc $a,lc $b) } @{$alpha_movies{$movies}});
+  my @movies = map(display_movie($movies_data->{$_}, { 'series' => 1, 'links' => 1, 'crossover' => 1 }), sort { article_sort(lc $a,lc $b) } @{$alpha_movies{$movies}});
   @{$alpha_movies{$movies}} = @movies;
 }
 my $alpha_menu = alpha_menu(\%alpha_movies, anchor('Search the list', { href => '#Search' }));

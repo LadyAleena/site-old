2a4b05752c8b619829167e44ded81c8c7df3ca4f
LadyAleena <LadyAleena@users.noreply.github.com>
2015-11-30 02:44:56 -0500

    Update Chess_variants.pl

--- Miscellany/Chess_variants.pl
+++ Miscellany/Chess_variants.pl
@@ -11,7 +11,7 @@ use HTML::Elements qw(table definition_list);
 
 # I want to add character entity pieces.
 
-my $definition_list = get_array( 'headings' => ['term','definition'] );
+my $definition_list = get_array( 'file' => ['Miscellany', 'Royal_chess.txt'], 'headings' => ['term','definition'] );
 
 my %chess_rows;
 $chess_rows{$_} = [qw(rook knight marshal bishop cardinal queen king cardinal bishop marshal knight rook)] for (1,12);
@@ -40,6 +40,7 @@ __DATA__
 At one time I played chess regularly, however, I have grown rusty from not playing. I do like to come up with new rules for chess, and here are some of them. Bughouse chess is my favorite variant to play.
 2 Pez chess
 If Pez ever comes out with a set of chess piece dispensers, one could play Pez chess. After setting up the board, it would be a good idea for each player to have a little dish for the Pez candies. Pez chess uses the same moves and rules from normal chess except whenever a piece is captured, one candy is removed from the dispenser. The dispenser is placed back on the board, and the game resumes. Only after a dispenser is empty of candy can it be permanently captured.
+This variant came from a conversation with A<Pez|href="https://twitter.com/PEZofficial" target="new">.
 2 Royal chess
 This is a game of capture, so there are no checks or checkmate. The standard pieces move the same. If the king is captured, the prince becomes the king. If the prince (king) is captured, the princess becomes king. If the princess (king) is captured, the queen becomes king. The captures do not have to be in that order. If the prince is captured before the king, the princess becomes the prince. If a pawn is on the outer edge on the opponent's side of the board, the pawn can be replaced by the lowest ranking member of the royal family (queen, princess, prince, king). Once the entire royal family is captured, the game is over.
 I have not tested this version. If anyone does test it, please A<let me know|href="mailto:fantasy@xecu.net"> how it turned out.

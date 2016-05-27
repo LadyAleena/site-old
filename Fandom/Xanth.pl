9cd58aaca2a4eca942dcf9014fcdeda7f0b400fe
LadyAleena <LadyAleena@users.noreply.github.com>
2016-05-27 16:17:51 -0400

    Update Xanth.pl

--- Fandom/Xanth.pl
+++ Fandom/Xanth.pl
@@ -20,7 +20,7 @@ for my $file (@files) {
   $doc_magic->{textify($file)} = sub {
     my $read = read_file(data_file($directory, $file));
     my $tree = make_tree($read);
-    pre(5, sub { print $tree })
+    pre(5, sub { print $tree }, {'style' => 'overflow-x:auto;'})
   }
 }
 
@@ -30,6 +30,7 @@ __DATA__
 This is a collection of my notes about the I<Xanth> series I have gathered for reasons. If some of this looks familiar, I started a lot of Xanth related articles on Wikipedia.
 2 People
 3 Kings of Xanth
+Ivy is the current King of Xanth. There are no ruling queens; so no matter gender, the ruler of Xanth is King. The power of Sorceresses is equal to the power of Magicians, she when a woman becomes King, she is referred to as Magician.
 # B<A<Merlin|href="#The_family_of_Merlin">>, magician of knowledge
 # B<A<Roogna|href="#The_family_of_Merlin">>, magician of adaptation
 # B<Rana>, mother of Rune, magician of creation, and the first female King
@@ -50,21 +51,31 @@ This is a collection of my notes about the I<Xanth> series I have gathered for r
 # B<Aeolus>, magician of storms
 # B<A<Trent|href="The_family_of_Ebnez">> is the magician of transformation. During the Next Wave in I<Night Mare>, several magicians acted as King pro tem.
 ## B<Dor> served as King pro tem previously in I<Centaur Aisle> and becomes King at the end of I<Night Mare>.
-## B<Jonathan> served as King pro tem previously in I<Centaur Aisle>.
+## B<Jonathan> was the sixth King of Xanth and served as King pro tem previously in I<Centaur Aisle>.
 ## B<Humfrey> served as King previously until B<Aeolus> replaced him.
-## B<Bink>
+## B<Bink> is a magician who can not be harmed by magic.
 ## B<Arnold>, a centaur, is the first non-human Magician to rule Xanth in any capacity.
-## B<Iris>
-## B<Irene> was classified a neo-Sorceress until I<Night Mare> when she was elevated to full Sorceres so she could take the throne.
-## B<Chameleon>
-## Imbri
+## B<Iris> is the sorceress of Illusion.
+## B<Irene> was classified a neo-Sorceress until I<Night Mare> when she was elevated to full Sorceress so she could take the throne.
+## B<Chameleon> is not a sorceress, however, she was the most intelligent person available to be king.
+## B<Imbri> is a night mare who became king as she was the only one who could break the chain.
 # B<A<Dor|href="The_family_of_Ebnez">> is the magician who can talk to inanimate objects; he called upon B<Murphy> to act as King pro tem in I<Isle of View>.
+# B<Ivy> is the magician of enhancement, daughter of B<Dor>.
+2 Families
+These are the known family tress of most of some of the major characters of Xanth, though not all of them.
+* All people with question marks by their names are from an alternate reality.
+* SPAN<&#9829;|style="font-size: larger;"> denotes marriage.
+* &#9825; denotes affair or accidental love spring union.
 3 The family of Merlin
 B<Merlin> was the magician of knowledge and the first King of Xanth.
 B<Tapis>, his wife, was the magician of tapestry.
 B<Jonathan>, his son, was the magician of zombies and the sixth King of Xanth. He would become King pro tem in I<Night Mare>.
 B<Roogna>, Taplin's husband and Merlin's son-in-law, was the magician of adaptation and the second King of Xanth.
 & Merlin
+3 The family of Ebnez
+B<Ebnez> was the sixteenth King of Xanth.
+& Ebnez
+The twins Ordinate and Abscissa, who can travel by geometry, were supposed to be Ivy's and Grey's children. They were dropped off at an orphanage when Ivy and Grey took too long to marry.
 3 The family of Humfrey
 B<Humfrey> is the magician of information and was the seventeenth King of Xanth. He has five and a half wives and family connections from all of them.
 & Humfrey
@@ -91,11 +102,6 @@ B<Melantha> was married to B<Mewrin>, who died. Together they had B<Marcie>. Mel
 B<Grossclout> is the father of Prince B<Vore>. Vore married Nada, and they had B<Demonica>.
 4 Humfrey and Mare Ann
 As of I<Pet Peeve> Humfrey and Mare Ann have not had children. Nothing is known of Mare Ann's family.
-3 The family of Ebnez
-B<Ebnez> was the sixteenth King of Xanth.
-& Ebnez
-The twins Ordinate and Abscissa, who can travel by geometry, were supposed to be Ivy's and Grey's children. They were dropped off at an orphanage when Ivy and Grey took too long to marry.
-All members of this family with an asterisk(*) by the name are possible children.
 3 The Baldwin Family
 Jim and Mary Baldwin are mundanes who enter Xanth in I<Yon Ill Wind> with their children. Sean is Jim's son by a previous marriage. David is Mary's son by a previous marriage. Karen is the daughter to both Jim and Mary. They are traveling with their pets Woofer, Sean's dog; Midrange, David's cat, and Tweeter, Karen's bird.
 & Baldwin
@@ -103,14 +109,17 @@ Jim and Mary Baldwin are mundanes who enter Xanth in I<Yon Ill Wind> with their 
 & centaur
 3 The goblin family
 & goblin
+4 Harpies
+B<Hardy> is a male harpy. B<Gloha> and B<Harglo> are winged goblins.
+4 Giants
+B<Graebo> was an invisible giant who was transformed by Magician Trent into a winged goblin. Graebo has two cousins, Greatbow and Girard.
 3 The zombie family
 & zombie
-3 Other families and people
+3 Other families
 * Nimby (the personification of the demon Xanth) and Chlorine had Nimbus.
-* The Simurgh had Sim.
-* Justin who was turned into a tree by Trent, became human again and married Breanna. They had Amber Dawn.
-* The clouds Cumulo Fracto Nimbus married and and Happy Bottom (Hurricane Gladys) had Fray Cloud and Wynde Tchill.
-* The dragons Stanley Steamer married and Stacy (Stella) had Steven.
+* Justin, who was turned into a tree by Trent, became human again and married Breanna. They had Amber Dawn.
+* The clouds Cumulo Fracto Nimbus and Happy Bottom (Hurricane Gladys) married and had Fray Cloud and Wynde Tchill.
+* The dragons Stanley Steamer Stacy (Stella) married and had Steven.
 * The fairies Joan (John) and John (Joan) married.
 * The gargoyles Gary Gar and Gayle Goyle married.
 * The harpies Harold and Heavenly Helen married.
@@ -118,9 +127,7 @@ Jim and Mary Baldwin are mundanes who enter Xanth in I<Yon Ill Wind> with their 
 * The mundanes Doug and Kim entered Xanth through the game interface in I<Demons Don't Dream>. They later married and had Knut.
 * The ogres Okra and Smithereen married and had Og and Not-Og.
 * The pookas Pook and Peek married and had Puck.
-* The skeletons Marrow Bones and Grac'l married and had Picka Bone and Joy'nt.
 * The sylphs Sylvester and Sylvia married and had Sylvinia.
-* Ichabod is a mundane archivist who comes to Xanth to do research with Arnold, the magician centaur. He is introduced in I<Centaur Aisle>.
 * Known twins in Twin City
 ** Mariana and Anairam (stone related talents)
 ** Amanda and Adnama (hair color related talents)
@@ -166,6 +173,7 @@ Jim and Mary Baldwin are mundanes who enter Xanth in I<Yon Ill Wind> with their 
 ** Region of Fire
 ** Region of Water
 ** The Void
-* Mount Parnassus
+* The Gourd is the realm of the Night Stallion and Night Mares.
+* Mount Parnassus is the home of the Muses.
 * Ida's Moons
 * Counter Xanth

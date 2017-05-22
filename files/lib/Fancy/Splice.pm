2416296b0b312a8591ef982dbf3e1541c4af9048
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 05:00:23 -0400

    Update and rename files/lib/Util/FancySplice.pm to files/lib/Fancy/Splice.pm

--- files/lib/Fancy/Splice.pm
+++ files/lib/Fancy/Splice.pm
@@ -1,28 +1,30 @@
-package Util::FancySplice;
+package Fancy::Splice;
 use strict;
 use warnings;
 use Exporter qw(import);
-our @EXPORT = qw(fancy_splice);
+
+our $VERSION = '1.0';
+our @EXPORT  = qw(fancy_splice);
 
 # written with the help of farang and wjw on PerlMonks
 sub fancy_splice { 
   my ($amount, @in_list) = @_;
   my @list;
   while (@in_list) {
-    push @list,[splice(@in_list,0,$amount)];
+    push @list, [splice(@in_list, 0, $amount)];
   }
   return \@list;
 }
 
 =head1 NAME
 
-Util::FancySplice - splice an array into groups.
+B<Fancy::Splice> splices an array into groups.
 
 =head1 SYNOPSIS
 
   my @colors = ('red', 'orange', 'yellow', 'spring', 'green', 'teal', 'cyan', 'azure',
                 'blue', 'violet', 'magenta', 'pink', 'white', 'black', 'gray');
-  my $list = fancy_splice(2,@colors);
+  my $list = fancy_splice(2, @colors);
 
   [
     ['red','orange'],
@@ -37,7 +39,7 @@ Util::FancySplice - splice an array into groups.
 
 =head1 DESCRIPTION
 
-The C<fancy_splice> function, exported by default, returns a referenced array of arrays spliced by a user specified amount. It takes two paraments: the size of the groups and the list. The size can be any whole number, and the list is an array.
+C<fancy_splice> is exported by default ans returns a referenced array of arrays spliced by a user specified amount. It takes two paraments: the size of the groups and the list. The size can be any whole number, and the list is an array.
 
 =head1 AUTHOR
 

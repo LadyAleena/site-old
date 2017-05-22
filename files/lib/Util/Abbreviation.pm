61b8836ebbe43e9a91618bc1db6be65b1dd41501
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 05:03:34 -0400

    Update Abbreviation.pm

--- files/lib/Util/Abbreviation.pm
+++ files/lib/Util/Abbreviation.pm
@@ -9,7 +9,7 @@ sub abbr {
   die("Sorry, I can't return an abbreviation if you don't give me a name.") if !$opt{'name'};
 
   my $name = $opt{'name'};
-     $name =~ s/^(?:The|An?) //i;
+     $name =~ s/^(?:The|An?) //i if $opt{'article'} && $opt{'article'} eq 'drop';
 
   if ($name !~ /[ _-]/) {
     return $opt{'name'};
@@ -35,5 +35,59 @@ sub abbr {
 sub initials {
   abbr(@_);
 }
+
+=head1 NAME
+
+B<Util::Abbreviation> returns an abbreviation for a string.
+
+=head1 SYNOPSIS
+
+  use Util::Abbreviation qw(abbr initial);
+  
+  my $abbr = abbr(
+    name    => 'The Lady Aleena',
+    article => 'drop',
+    periods => 'no',
+    ALLCAPS => 'yes',
+    HTML    => 'no'
+  );
+  
+  print $abbr;
+
+=head1 DESCRIPTION
+
+In B<Util::Abbreviation> C<abbr> and C<initial> can be exported. C<initial> is an alias for C<abbr>.
+
+=over
+
+=item name
+
+C<name> is the name to be abbreviated.
+
+=item article
+
+C<article> is the option to C<drop> the article from the abbreviation. The World Wide Web would drop the article "The".
+
+=item periods
+
+C<periods> is the option to add periods to the abbreviation with yes, true, or 1. With it on John Doe would be abbreviated J.D.
+
+=item ALLCAPS
+
+C<ALLCAPS> is the option to make the abbreviation all capital letters. The International House of Pancakes would be abbreviated IHOP.
+
+=item HTML
+
+C<HTML> is the option to add the HTML C<<abbr>> tag to the abbreviation. Plain Old Documentation would be returned as follows: 
+
+  <abbr title="Plain Old Documentation">POD</abbr>
+
+=back
+
+=head1 AUTHOR
+
+Lady Aleena
+
+=cut
 
 1;

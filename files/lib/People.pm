6c7ba36aa190c93d0493cfc182825580b97709c0
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 04:58:10 -0400

    Update People.pm

--- files/lib/People.pm
+++ files/lib/People.pm
@@ -2,29 +2,31 @@ package People;
 use strict;
 use warnings;
 use Exporter qw(import);
-our @EXPORT_OK = qw(get_people);
+our @EXPORT_OK = qw(people_list);
 
 use Base::Data qw(data_file);
 use Util::Sort qw(name_sort);
 use Util::Convert qw(filify searchify);
 use Util::ExternalLinks;
 
-sub get_people {
+sub people_list {
   my ($file) = @_;
 
-  open(my $data_file,'<',data_file('People',$file)) || return undef;
-  my @base_people = map { chomp $_; [ split(/\|/, $_) ] } <$data_file>;
-  
-  my @people;
-  for my $line (sort { name_sort($a->[0], $b->[0]) } @base_people) {
-    my $name = $line->[0];
-       $name =~ s/^_//;
-    my $article_name = $line->[1] ? $line->[1] : filify($name);
-    my $link = external_links([['Wikipedia', $article_name, $name], ['Google', searchify($name), $name]]);
-    push @people, "$name $link";
+  my $people_file = data_file('People', $file);
+  my $people = undef;
+  if (-f $people_file) {
+    open(my $fh, '<', $people_file) || die "Can not open $people_file $!";
+    my @people_lines = map { chomp $_; [ split(/\|/, $_) ] } <$fh>;
+    
+    for my $line (sort { name_sort($a->[0], $b->[0]) } @people_lines) {
+      my $name = $line->[0];
+         $name =~ s/^_//;
+      my $article_name = $line->[1] ? $line->[1] : filify($name);
+      my $link = external_links([['Wikipedia', $article_name, $name], ['Google', searchify($name), $name]]);
+      push @$people, "$name $link";
+    }
   }
-  
-  return \@people;
+  return $people;
 }
 
 1;

a33e1461899e979b4ab1d3f142e2e33a7f246b57
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 13:52:16 -0400

    Update Word_finds.pl

--- Role_playing/Miscellany/Word_finds.pl
+++ Role_playing/Miscellany/Word_finds.pl
@@ -10,6 +10,7 @@ use HTML::Entities qw(encode_entities);
 use lib '../../files/lib';
 use Base::Page qw(page);
 use Base::Menu qw(file_menu);
+use HTML::Elements qw(section paragraph list span pre);
 use RolePlaying::WordFind qw(print_word_find);
 
 my %finds = (
@@ -20,6 +21,26 @@ my %finds = (
 my $cgi = CGI::Minimal->new;
 my $select = encode_entities($cgi->param('word find'),'<>"');
 my $head = $select && $finds{$select} ? $select : undef;
-page( 'heading' => $head, 'file menu' => file_menu('word find', [sort keys %finds], $select), 'code' => sub {
-  print_word_find($select,$finds{$select}) if ($select && $finds{$select});
+my $file_menu = file_menu('word find', [sort keys %finds], $select);
+
+page( 'heading' => $head, 'file menu' => $file_menu, 'code' => sub {
+  if ($select && $finds{$select}) {
+    my $lonely = span($finds{$select}, { 'class' => 'word_find' });
+    my $lone = "There is a lone $lonely in there too.";
+    my $word_find = print_word_find($select, $finds{$select});
+
+    section(3, sub {
+      pre(3, sub { print $word_find->{'board'} }, { 'class' => 'word_find'});
+    });
+    section(3, sub {
+      list(4, 'u', $word_find->{'list'}, { class => 'four' });
+      paragraph(4, join(' ', ($lone, $word_find->{'lonely'})));
+    }, { 'heading' => [2, 'Find these monsters'] });
+  }
+  else {
+    section(3, sub {
+      paragraph(5, "Please select a puzzle:");
+      list(5, 'u', $file_menu);
+    });
+  }
 });

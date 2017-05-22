e3ffcd10df69f59f647678c3da940e0ffe97deb3
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 13:22:17 -0400

    Update Movies_by_series.pl

--- Movies/Movies_by_series.pl
+++ Movies/Movies_by_series.pl
@@ -10,21 +10,22 @@ use lib '../files/lib';
 use Base::Page qw(page);
 use Base::Menu qw(file_menu alpha_menu);
 use Base::Data qw(alpha_array);
-use HTML::Elements qw(section paragraph list);
+use HTML::Elements qw(section nav list);
 use Util::Sort qw(article_sort);
-use Util::Columns qw(get_columns);
-use Movie qw(option print_franchise print_series print_movie);
+use Util::Columns;
+use Movie qw(movie_option print_franchise print_series print_movie);
 
-my $cgi = CGI::Minimal->new;
-my $select = encode_entities($cgi->param('series'),'<>"');
-my $series_select = option('series');
-my @selects = sort {article_sort(lc $a, lc $b)} keys %$series_select;
-my $file_menu = file_menu('series', \@selects, $select);
-my %alpha_movies = alpha_array(\@selects);
-my $alpha_menu = alpha_menu(\%alpha_movies);
+my $cgi           = CGI::Minimal->new;
+my $select        = encode_entities($cgi->param('series'),'<>"');
+my $series_select = movie_option('series');
+my @selects       = sort {article_sort(lc $a, lc $b)} keys %$series_select;
+my $file_menu     = file_menu('series', \@selects, $select);
+my %alpha_movies  = alpha_array(\@selects);
+my $alpha_menu    = alpha_menu(\%alpha_movies, { 'join' => ' | ' });
 
 my $body_head  = $select && $series_select->{$select} ? $select : undef;
 my $body_class = $select && $series_select->{$select} ? 'movie_series' : undef;
+
 page( 'heading' => $body_head, 'file menu' => $file_menu, 'class' => $body_class, 'code' => sub {
   if ($select && $series_select->{$select}) {
     if ($series_select->{$select} eq 'series') {
@@ -36,10 +37,10 @@ page( 'heading' => $body_head, 'file menu' => $file_menu, 'class' => $body_class
   }
   else {
     for my $alpha (sort keys %alpha_movies) {
-      my $class = get_columns(3, scalar @{$alpha_movies{$alpha}});
+      my $class = number_of_columns(3, scalar @{$alpha_movies{$alpha}}, 1);
       section(3, sub {
         list(5, 'u', file_menu('series', $alpha_movies{$alpha}), { 'class' => $class, 'style' => 'font-size:smaller' });
-        paragraph(5, $alpha_menu, { 'class' => 'alpha_menu' });
+        nav(5, $alpha_menu, { 'class' => 'alpha_menu' });
       }, { 'heading' => [2, $alpha, { 'id' => "section_$alpha" }] });
     }
   }

a93331d76eff4bd9dd8bd4375a23d705013e4307
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 05:10:43 -0400

    Update Menu.pm

--- files/lib/Base/Menu.pm
+++ files/lib/Base/Menu.pm
@@ -2,14 +2,14 @@ package Base::Menu;
 use strict;
 use warnings FATAL => qw( all );
 use Exporter qw(import);
-our @EXPORT_OK = qw(main_menu alpha_menu file_menu link_color);
+our @EXPORT_OK = qw(base_menu alpha_menu file_menu link_color);
 
 use Cwd qw(cwd realpath);
 
-use Base::Data qw(get_directory);
+use Base::Data qw(file_list);
 use HTML::Elements qw(anchor);
 use Util::Sort qw(article_sort name_sort);
-use Util::Convert qw(linkify textify searchify);
+use Util::Convert qw(textify searchify);
 
 sub link_color {
   my ($file,$style) = @_;
@@ -40,12 +40,12 @@ sub link_color {
   return qq(color:#$color);
 }
 
-sub main_menu {
+sub base_menu {
   my %opt = @_;
   my $directory = $opt{'directory'};
   my $curr_cwd = cwd;
 
-  my @contents = get_directory($directory); 
+  my @contents = file_list($directory); 
   @contents = grep {/^\p{uppercase}/} @contents if (!$opt{'full'} || $opt{'full'} !~ /^[yt1]/); # Thank you [tye]!
 
   # Thank you [davido]!
@@ -55,29 +55,33 @@ sub main_menu {
   my (@files, @directories);
   for my $content (@contents) {
     my $long_content = "$directory/$content";
-    my $link = linkify($long_content);
+    my $link = File::Spec->abs2rel($long_content);
     my $text = $content !~ /^\./ ? textify($content) : $content;
 
     if (-f $long_content) {
       my $active = realpath($0) eq $long_content ? 'active' : 'inactive';
       my $color = $opt{'color'} == 1 ? link_color($content,1) : undef;
-      my $inlist = $active eq 'active' && $opt{'file menu'} ? ['u', $opt{'file menu'}] : undef;
-         $active .= $active eq 'active' && $opt{'file menu'} ? ' open' : '';
+      my $inlist = $active eq 'active' && $opt{'file menu'} ? ['u', $opt{'file menu'}, { 'class' => 'sub_menu' }] : undef;
+        $active .= $active eq 'active' && $opt{'file menu'} ? ' open' : '';
+      if (-M $long_content < 3) {
+        $active .= ' updated';
+      }
       push @files, [anchor($text, { 'href' => $link, 'title' => $text, 'style' => $color }), { 'class' => $active, 'inlist' => $inlist } ];
     }
-    
+
     if (-d $long_content) {
       my $active = $curr_cwd =~ /$long_content/ ? 'open active' : 'closed inactive';
       my $file_list;
+      my $index = "$long_content/index.pl";
 
-      if (-e "$long_content/index.pl") {
-        $link .= "/index.pl";
+      if (-e $index) {
+#        $link .= "/index.pl";
         my $color = $opt{'color'} == 1 ? link_color($link,1) : undef;
         $text = anchor($text, { 'href' => $link, 'title' => $text, 'style' => $color });
-        $file_list = $0 =~ /index/ && $active =~ / active$/ && $opt{'file menu'} ? $opt{'file menu'} : undef;
+        $file_list = "$curr_cwd/$0" =~ /$index/ && $active =~ / active$/ && $opt{'file menu'} ? $opt{'file menu'} : undef;
       }
 
-      my $next_list = main_menu( 'directory' => $long_content, 'color' => $opt{'color'}, 'full' => $opt{'full'}, 'file menu' => $opt{'file menu'} );
+      my $next_list = base_menu( 'directory' => $long_content, 'color' => $opt{'color'}, 'full' => $opt{'full'}, 'file menu' => $opt{'file menu'} );
       unshift @$next_list, @$file_list if $file_list;
       my $inlist = $next_list ? ['u', $next_list] : undef;
       $active =~ s/^(?:open|closed) // if !$inlist;
@@ -101,7 +105,7 @@ sub file_menu {
 
 # alpha_menu prints a line of links based on keys of a hash.
 sub alpha_menu {
-  my ($hash, $list_addition) = @_;
+  my ($hash, $opt) = @_;
 
   my @line;
   for my $letter (sort { article_sort($a,$b) } keys %{$hash}) {
@@ -109,8 +113,12 @@ sub alpha_menu {
     push @line, anchor($letter, { 'href' => "#section_$section_name" });
   }
 
-  push @line, $list_addition if $list_addition;
-  my $line = join(' - ',@line);
+  if ($opt->{addition}) {
+    push @line, ref($opt->{addition}) eq 'ARRAY' ? @$opt->{addition} : $opt->{addition};
+  }
+
+  my $join = $opt->{'join'} ? $opt->{'join'} : ', ';
+  my $line = $opt->{'join'} ? join($join, @line) : \@line;
 
   return $line;
 }

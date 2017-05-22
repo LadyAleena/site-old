6cda0a02aa00433066ac45433be83f2d2743a32e
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 13:17:43 -0400

    Update Learning_HTML.pl

--- Miscellany/Learning_HTML.pl
+++ Miscellany/Learning_HTML.pl
@@ -17,12 +17,12 @@ I strongly suggest you not use ACRONYM<WYSIWYG|title="what you see is what you g
 While you are learning HTML, learn CSS as well. ^PageResource^ is a wonderful place to start learning HTML and CSS. The ^World Wide Web Consortium^ (W3C) is depreciating formatting elements and attributes. A good understanding of CSS will also make writing your markup faster. Keeping up with the latest technology is key. It is a pain to have to remarkup a page. I am still agonizing over my pages with old markup.
 When you are finally ready to write your site, it is suggested that you first get some paper and a pencil and draw out what you want your site, and the pages therein, to generally look like. This way you will have a guide, and your markup will go a lot faster. I did it that way when rebuilding this site from the ground up. I was able to markup my site in less time with those drawings as a reference.
 When you stop having fun, take a break from learning markup or writing your site, as it might show on your site. The more fun you are having, the more fun your users will have.
-If you have questions, ^#HTML^ on freenode or ^WXCity Forum^ are great places to get them answered. This site was built with the help of a lot of people in there.
+If you have questions, ^#HTML^ on freenode is a great place to get them answered. This site was built with the help of a lot of people in there.
 After a while unchanging text may not be enough for you. There are many scripting languages out there that you can learn including but not limited to javascript, perl, and python.
 2 HTML links
+* ^World Wide Web Consortium^
 * ^HTML An Interactive Tutorial for Beginners^
 * ^HTML Code Tutorial^
 * ^PageResource^
 * ^PageTutor^
 * ^Web Design Group^
-* ^World Wide Web Consortium^

20190c6a5742d45180eea7ebe502a9eae1fa1026
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 13:16:29 -0400

    Update Geek_codes.pl

--- Miscellany/Geek_codes.pl
+++ Miscellany/Geek_codes.pl
@@ -3,37 +3,71 @@ use strict;
 use warnings FATAL => qw( all );
 
 use CGI::Carp qw(fatalsToBrowser);
-use File::Slurp qw(read_file);
 
 use lib '../files/lib';
 use Base::Page qw(page story);
-use Base::Data qw(data_directory get_directory data_file);
-use Base::LineMagic qw($line_magic);
-use HTML::Elements qw(pre);
-use Util::Convert qw(textify);
 
-my $directory = 'Miscellany/Geek_codes';
-my @files = get_directory(data_directory($directory));
-
-my $doc_magic;
-for my $file (@files) {
-  my $read = read_file(data_file($directory, $file));
-  $doc_magic->{textify($file)} = sub { pre(5, sub { print $read })}
-}
-
-page( 'code' => sub { story(*DATA, { 'doc magic' => $doc_magic, 'line magic' => $line_magic }) });
+page( 'code' => sub { story(*DATA) });
 
 __DATA__
-Along with the ^Geek Code^ written by Robert Hayden, here are a few additions, use the standard pluses or minuses in each group.
+Along with the Geek Code written by Robert Hayden, here are a few additions, use the standard pluses or minuses in each group.
 2 Babylon 5
-& Babylon 5
+:| wrap
+: 5   - The entire Babylon 5 franchise
+: 5:s - Babylon 5
+: 5:C - Crusade
+: 5:f - Babylon 5 film series
 2 Buffy the Vampire Slayer
-& Buffy the Vampire Slayer
+:| wrap
+: B   - The entire Buffy the Vampire Slayer franchise
+: B:f - Buffy the Vampire Slayer film
+: B:V - Buffy the Vampire Slayer
+: B:A - Angel
+: 
+: +++++ I am Joss Whedon.
+: ++++  I have written so much fan fiction about my favorite couple (bangle, spuffy, cangel, spordy, spangle) that they have become part of the family.
+: +++   I have watched it from the beginning on the WB, know the names of every character, know all the demons, and followed Buffy to UPN and still watched Angel on the WB.
+: ++    I love this show and have them all on DVD.
+: +     I like it all and try to watch the reruns as much as possible.
+:       It is a good show, but I won't go out of my way to watch it.
+: -     Who needs a brooding vampire?
+: --    It's Dark Shadows meets Saved by the Bell.
+: ---   They will burn in Hell for meddling in the dark arts.
 2 Star Trek
-& Star Trek
+:| wrap
+: t   - The entire Star Trek franchise
+: t:o - Star Trek (the original series)
+: t:a - Star Trek (the animated series)
+: t:n - Star Trek: The Next Generation
+: t:d - Star Trek: Deep Space Nine
+: t:v - Star Trek: Voyager
+: t:e - Enterprise
 2 Stargate
-& Stargate
+:| wrap
+: SG   - The entire Stargate franchise
+: SG:f - Stargate film
+: SG:1 - Stargate SG-1
+: SG:A - Stargate: Atlantis
+: SG:U - Stargate Universe
+: SG:I - Stargate: Infinity
+: 
+: +++++ I am the person who sued Emmerich and Devlin when they stole my idea.
+: ++++  I am either Emmerich, Devlin, or Richard Dean Anderson.
+: +++   I started watching the show on Showtime, know all of the binary designations of all the planets visited, complain bitterly when the Sci-Fi channel plays the episodes out of order, and want a Stargate of my own.
+: ++    I love this show, have them all on DVD, and was depressed when Michael Shanks left the show for a season.
+: +     I loved the movie, and the show does it some justice.
+:       It is just another show.
+: -     It will never work, Star Trek kicks its butt.
+: --    Do we really need another MacGyver?
+: ---   When will this show just die!
 2 The X-Files
-& The X-Files
+:| wrap
+: X   - The entire The X-Files franchise
+: X:X - The X-files
+: X:M - Millennium
+: X:L - The Lone Gunmen
+: H:R - Harsh Realm
 2 Relationships
-& relationships
+:| wrap
+: r## Add a number to indicate how long the relationship has been.
+:    r15 would mean a 15 year relationship.

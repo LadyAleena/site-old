d828a9ceca0a2202cad83f79b34d2054ac2ff3ef
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 13:18:46 -0400

    Update Random_orgy_generator.pl

--- Miscellany/Random_orgy_generator.pl
+++ Miscellany/Random_orgy_generator.pl
@@ -2,17 +2,17 @@
 use strict;
 use warnings;
 
+use CGI::Carp qw(fatalsToBrowser);
 use Lingua::EN::Inflect qw(A NUMWORDS);
 
 use lib "../files/lib";
 use Base::Page qw(page story);
-use HTML::Elements qw(paragraph);
-use RolePlaying::Random::Misc qw(sexual_orientation relationship);
+use RolePlaying::Random::Misc qw(random_sexual_orientation random_relationship);
 use RolePlaying::Random::Body::Modification qw(random_body_modification);
 use RolePlaying::Random::Color qw(random_color);
 use RolePlaying::Random::Descriptor qw(random_descriptor);
 
-my $participants = (1..10)[rand 10];
+my $participants = (3..10)[rand 8];
 
 sub age {
   my @ages = (18..75);
@@ -20,10 +20,10 @@ sub age {
 }
 
 sub participant {
-  my $relationship = relationship;
+  my $relationship = random_relationship;
   my $age          = NUMWORDS(age);
   my $gender       = qw(man woman)[rand 2];
-  my $orientation  = sexual_orientation;
+  my $orientation  = random_sexual_orientation;
   my $somatotype   = random_body_modification('somatotype');
   my $quality      = A(random_descriptor('quality'));
   my $hair_color   = random_color('hair');
@@ -33,13 +33,13 @@ sub participant {
 }
 
 my @participants;
-push @participants, participant for (1..$participants);
+push @participants, participant for (3..$participants);
 
-my $doc_magic = { 'participants' => sub { paragraph(5,$_, { class => 'sex' }) for @participants } };
+my $magic = { 'participants' => join("\n", @participants)."\n" };
 
-page( 'code' => sub { story(*DATA, { 'doc magic' => $doc_magic }) });
+page( 'code' => sub { story(*DATA, { 'line magic' => $magic }) });
 
 __DATA__
 If you are writing an orgy scene and need inspiration on who is there, here you go!
 2 Orgy participants
-& participants
+^participants^

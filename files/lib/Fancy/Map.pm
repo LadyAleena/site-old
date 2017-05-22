90aa10044df7c60fbb0b3d02b4c8818768d9209b
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 04:59:45 -0400

    Update and rename files/lib/Util/MapinMap.pm to files/lib/Fancy/Map.pm

--- files/lib/Fancy/Map.pm
+++ files/lib/Fancy/Map.pm
@@ -1,15 +1,17 @@
-package Util::MapinMap;
+package Fancy::Map;
 use strict;
 use warnings;
 use Exporter qw(import);
-our @EXPORT = qw(map_in_map);
+
+our $VERSION = '1.0';
+our @EXPORT  = qw(fancy_map);
 
 # Written with the help of GotToBTru, RonW, and toolic of Perl Monks.
-sub map_in_map {
-  my ($opt,$list) = @_;
+sub fancy_map {
+  my ($opt, $list) = @_;
   map { 
     if (ref($_)) {
-      map_in_map($opt,$_);
+      fancy_map($opt, $_);
     }
     else {
       my $before = $opt->{'before'} ? $opt->{'before'}.' ' : '';
@@ -21,13 +23,13 @@ sub map_in_map {
 
 =head1 NAME
 
-Util::MapinMap - map within a map.
+B<Fancy::Map> maps within a map.
 
 =head1 SYNOPSIS
 
-  my $colors = [map_in_map(
+  my $colors = [fancy_map(
                  { 'before => 'glass', 'after' => 'beads' },
-                 [map("sparkley $_",('red','yellow','green','cyan','blue','magenta')),'white','black','gray']
+                 [map("sparkley $_", ('red', 'yellow', 'green', 'cyan', 'blue', 'magenta')), 'white', 'black', 'gray']
                )];
   
   [
@@ -44,13 +46,13 @@ Util::MapinMap - map within a map.
 
 =head1 DESCRIPTION
 
-The C<map_in_map> function, exported by default, returns a list of mapped values from a list which already has a map in it. It takes three parameters. The first two are in a hash reference with the values to be mapped before and after the values of the list. The third is a reference to the list.
+C<fancy_map> is exported by default and returns a list of mapped values from a list which already has a map in it. It takes three parameters. The first two are in a hash reference with the values to be mapped before and after the values of the list. The third is a reference to the list.
 
-  map_in_map( { 'before' => $before_value, 'after' => $after_value }, $list_with_a_map_in_it );
+  fancy_map( { 'before' => $before_value, 'after' => $after_value }, $list_with_a_map_in_it );
  
 =head1 AUTHOR
 
-Lady_Aleena
+Lady Aleena
 
 =cut
 

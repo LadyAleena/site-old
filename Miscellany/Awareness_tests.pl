093cafc10579c69774c9c1e555cdcfba02481bc1
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 13:15:25 -0400

    Update Awareness_tests.pl

--- Miscellany/Awareness_tests.pl
+++ Miscellany/Awareness_tests.pl
@@ -18,15 +18,15 @@ The answers are hidden on this page.
 # How many birthdays does the average man have? (A<Answer|class="toggle">SPAN<: One, we are born only once.|^hide^>)
 # Take two apples from three apples and what do you have? (A<Answer|class="toggle">SPAN<: Two apples|^hide^>)
 # Do they have a 4th of July in England? (A<Answer|class="toggle">SPAN<: Yes|^hide^>)
-# Why can’t a man living in North Carolina be buried west of the Mississippi River? (A<Answer|class="toggle">SPAN<: He is still alive.|^hide^>)
+# Why can't a man living in North Carolina be buried west of the Mississippi River? (A<Answer|class="toggle">SPAN<: He is still alive.|^hide^>)
 # If you had only one match and entered a room, in which there was a kerosene lamp, an oil heater, and a wood burning stove, which would you light first? (A<Answer|class="toggle">SPAN<: The match|^hide^>)
 # Some months have 30 days some have more or less. How many months have 28 days? (A<Answer|class="toggle">SPAN<: 12, all months have 28 days in them, at least.|^hide^>)
 # I have in my hand only two U.S. coins, which total fifty-five cents. One is not a nickel. What are the two coins? (A<Answer|class="toggle">SPAN<: A half dollar and a nickel|^hide^>)
 # A farmer had seventeen sheep. All but nine died. How many did he have left? (A<Answer|class="toggle">SPAN<: 9|^hide^>)
 # Divide thirty by ½ and add ten. What is the answer? (A<Answer|class="toggle">SPAN<: 70, 30/½ = 60 + 10 = 70|^hide^>)
 # How many animals of each species did Moses take aboard the Ark with him? (A<Answer|class="toggle">SPAN<: None, Noah took the animals onto the Ark.|^hide^>)
-# Is it legal in California for a man to marry his widow’s sister?  Explain. (A<Answer|class="toggle">SPAN<: Yes, since there is no need for such a law against it, since the man is dead.|^hide^>)
-# A woman gives a beggar fifty cents. The woman is the sister of the beggar, but the beggar is not the woman’s brother. How come? (A<Answer|class="toggle">SPAN<: The beggar is the woman’s sister.|^hide^>)
+# Is it legal in California for a man to marry his widow's sister?  Explain. (A<Answer|class="toggle">SPAN<: Yes, since there is no need for such a law against it, since the man is dead.|^hide^>)
+# A woman gives a beggar fifty cents. The woman is the sister of the beggar, but the beggar is not the woman's brother. How come? (A<Answer|class="toggle">SPAN<: The beggar is the woman's sister.|^hide^>)
 2 Narrative
 The owner of the Adams Manufacturing Company entered the office of one of his foremen where he found three employees playing cards. One of them was Carl Young, brother-in-law of Foreman Henry Dilson. Dilson, incidentally, often worked late. Company rules did not specifically forbid gambling on the premises, but the president had spoken forcibly on the subject.
 3 Statements

62e6acb2e45d2f554a1ecb279aa00807b6a66c21
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 05:11:37 -0400

    Update and rename Root.pm to Path.pm

---
+++ files/lib/Base/Path.pm
@@ -0,0 +1,37 @@
+package Base::Path;
+use strict;
+use warnings FATAL => qw( all );
+use Exporter qw(import);
+our @EXPORT = qw(base_path);
+
+my $server = $ENV{SERVER_NAME} ? $ENV{SERVER_NAME} : 'localhost';
+my $http   = $ENV{HTTPS} && $ENV{HTTPS} eq 'on' ? 'https' : 'http';
+
+my %hosts = (
+  'localhost' => {
+    'path' => '/home/me/Documents/www',
+    'link' => "$http://localhost",
+  },
+  'office.home' => {
+    'path' => '/home/me/Documents/www',
+    'link' => "$http://office.home",
+  },
+  'fantasy.xecu.net' => {
+    'path' => '/www/fantasy/public_html',
+    'link' => "$http://fantasy.xecu.net",
+  }
+);
+
+my $root_path = $hosts{$server}{'path'};
+
+for my $host (values %hosts) {
+  $host->{'data'} = "$root_path/files/data";
+  $host->{$_}     = $host->{'link'}."/files/$_" for qw(audio css images);
+}
+
+sub base_path {
+  my ($host_key) = @_;
+  return $hosts{$server}{$host_key};
+}
+
+1;

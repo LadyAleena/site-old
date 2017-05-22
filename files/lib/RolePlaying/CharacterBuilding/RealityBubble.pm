36a7959eb3581bb1d0e948c491e4456868a785c5
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 04:43:15 -0400

    Update RealityBubble.pm

--- files/lib/RolePlaying/CharacterBuilding/RealityBubble.pm
+++ files/lib/RolePlaying/CharacterBuilding/RealityBubble.pm
@@ -1,17 +1,17 @@
 package RolePlaying::CharacterBuilding::RealityBubble;
 use strict;
-use warnings;
+use warnings FATAL => ( 'all' );
 use Exporter qw(import);
-our @EXPORT = qw(get_reality_bubble);
+our @EXPORT = qw(reality_bubble);
 
 use List::Util qw(sum);
 
-use RolePlaying::CharacterBuilding::Class qw(convert_class get_level);
+use RolePlaying::CharacterBuilding::Class qw(convert_class class_level);
 
-sub get_reality_bubble {
+sub reality_bubble {
   my ($class,$opt) = @_;
-  $class = convert_class($class,'RealityBubble');
-  my $level = $opt->{'level'} ? $opt->{'level'} : get_level('chaos warden', $opt->{'experience'});
+  $class = convert_class($class, 'RealityBubble');
+  my $level = $opt->{'level'} ? $opt->{'level'} : class_level('chaos warden', $opt->{'experience'});
 
   my $bubble = sum(1..$level);
   return $bubble;

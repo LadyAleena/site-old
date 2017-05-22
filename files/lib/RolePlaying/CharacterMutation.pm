185cc3e2e3b7d7ce3a954f867bbc975a19690542
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 04:15:34 -0400

    Update CharacterMutation.pm

--- files/lib/RolePlaying/CharacterMutation.pm
+++ files/lib/RolePlaying/CharacterMutation.pm
@@ -2,16 +2,16 @@ package RolePlaying::CharacterMutation;
 use strict;
 use warnings FATAL => qw( all );
 use Exporter qw(import);
-our @EXPORT_OK = qw(random_mutations);
+our @EXPORT_OK = qw(random_mutations parent_knows);
 
 use Games::Dice qw(roll);
 use Lingua::EN::Inflect qw(PL_N A ORD);
 use List::Util qw(sum max);
 
 use Util::Convert qw(textify);
-use RolePlaying::Random qw(tinyrand);
+use RolePlaying::Random qw(tinyrand instant_rand);
 
-use RolePlaying::Random::Misc           qw(dice divinity proficiency_type sign);
+use RolePlaying::Random::Misc           qw(random_dice random_divinity random_generation random_language_common random_parent random_proficiency_type random_sign);
 use RolePlaying::Random::Class          qw(random_class random_class_special);
 use RolePlaying::Random::Event          qw(random_event);
 use RolePlaying::Random::Insanity       qw(random_mental_condition);
@@ -30,16 +30,11 @@ use RolePlaying::Random::Body::Function     qw(random_body_functions);
 
 use RolePlaying::CharacterBuilding::AbilityScores qw(random_ability random_game_effect);
 
-# This script randomly generates enhancements, diminishments, and mutations of player characters.
-# Written by Lady Aleena
-# Acknowledgements
-# Perl Monks: Anno, bradenshep, Corion, ikegami, Limbic~Region, Petruchio, shmem, Sidhekin, wfsp, ysth and the rest of the community that has been so kind to me.
-
 sub game_rolls {
   my @game_rolls = (
     random_ability.' checks',
     'on non-weapon proficiency checks',
-    'to saving throws'.tinyrand('',' vs. '.random_saving_throw(tinyrand('keys','all'))),
+    'to saving throws'.tinyrand('',' vs. '.random_saving_throw(tinyrand('by keys', 'all'))),
     'Armor Class',
     'THAC0 modifier',
     'to Surprise'
@@ -48,18 +43,37 @@ sub game_rolls {
 }
 
 sub parent_knows {
-  return tinyrand('',' that '.tinyrand(qw(father mother)).' knows');
+  my $generation = random_generation;
+     $generation = "one $generation" if $generation =~ /^g/;
+     $generation = "an $generation" if $generation eq 'ancestral ';
+  my $parent     = random_parent;
+  return " $generation$parent knows";
 }
 
 sub learning {
   my @learning = (
-    PL_N(proficiency_type).parent_knows,
-    'any language other than racial language',
-    tinyrand(random_ability.' based',random_class('keys')).' non-weapon proficiencies',
+    PL_N(random_proficiency_type).parent_knows,
+    random_language_common.' or related languages',
+    tinyrand(random_ability.' based',random_class('by keys')).' non-weapon proficiencies',
     random_weapons('damage type'),
     'the '.random_spell_group('psionisist')
   );
   return $learning[rand @learning];
+}
+
+sub miscellaneous_magic {
+  my @miscellaneous = (
+    'magic dead',
+    'a magic attracter',
+    'addicted to magic energies',
+    tinyrand(qw(blessed cursed)),
+    'a '.random_range('simple', 'imperial').' radius '.tinyrand('wild magic', 'magic dead').' zone',
+    A(random_monster('lycanthrope')),
+    'fated to become '.A(random_monster('undead', 'by keys')).' upon death',
+    'a '.tinyrand(qw(major minor)).' '.tinyrand(qw(hero villain)).' in a prophesy',
+    '<b>'.A(random_divinity).'</b> and should be retired'
+  );
+  return $miscellaneous[rand @miscellaneous];
 }
 
 sub events {
@@ -72,28 +86,13 @@ sub events {
   return $event;
 }
 
-sub miscellaneous_magic {
-  my @miscellaneous = (
-    'magic dead',
-    'a magic attracter',
-    'addicted to magic energies',
-    tinyrand(qw(blessed cursed)),
-    'a '.random_range('simple','imperial').' radius '.tinyrand('wild magic','magic dead').' zone',
-    A(random_monster('lycanthrope')),
-    'fated to become '.A(random_monster('undead','keys')).' upon death',
-    'a '.tinyrand(qw(major minor)).' '.tinyrand(qw(hero villain)).' in a prophesy',
-    '<b>'.A(divinity).'</b> and should be retired'
-  );
-  return $miscellaneous[rand @miscellaneous];
-}
-
 sub effects {
   my @effects = (
     'wild magic surge after '.events,
-    random_body_color_change.tinyrand(' after '.events,' at will'),
-    random_body_functions.tinyrand(' after '.events,''),
-    random_mental_condition.tinyrand(' after '.events,''),
-    miscellaneous_magic.tinyrand(' after '.events,'')
+    random_body_color_change.tinyrand(' after '.events, ' at will'),
+    random_body_functions.tinyrand(' after '.events, ''),
+    random_mental_condition.tinyrand(' after '.events, ''),
+    miscellaneous_magic.tinyrand(' after '.events, '')
   );
   my $effect = $effects[rand @effects];
   return $effect;
@@ -110,16 +109,18 @@ sub random_mutation {
     sub { return random_spell_casting(random_frequency) },
     sub { return random_magic_item_special },
     'wild psionic talent',
-    sub { return sign.roll('1d10').' '.game_rolls },
+    sub { return random_sign.roll('1d10').' '.game_rolls },
     sub { return random_spell_resistance },
     sub { return random_weapon_damage.tinyrand('',' from '.random_magic_weapon) },
     'birth level',
     'max level',
-    sub { return 'can '.tinyrand('only','not').' learn '.learning },
-    sub { return 'can '.tinyrand('only','not').' use '.random_weapons('material') },
+    sub { return 'can '. tinyrand('only','not') . instant_rand(
+      ' learn '.learning,
+      ' use '.tinyrand(random_weapons('material'), random_weapons('damage type'))
+    )},
     sub { return 'can '.tinyrand('only','not').' make '.PL_N(random_event) },
-    sub { return 'knows one '.proficiency_type.parent_knows },
-    sub { return 'knows all '.PL_N(proficiency_type).parent_knows },
+    sub { return 'knows one '.random_proficiency_type.parent_knows },
+    sub { return 'knows all '.PL_N(random_proficiency_type).parent_knows },
     sub { return 'attracts '.random_class.' followers' },
     sub { return tinyrand(qw(attracts repels)).' all '.random_thing('animals').' '.random_radius('touch','imperial') },
     sub { return tinyrand('communicates with','knows history of').' '.random_thing.' '.random_radius('touch','imperial') },
@@ -135,7 +136,7 @@ sub random_mutation {
 
 sub random_mutations {
   my ($user_rolls) = @_;
-  my $rolls = $user_rolls ? $user_rolls : roll(dice);
+  my $rolls = $user_rolls ? $user_rolls : roll(random_dice);
 
   my @effects;
   for (1..$rolls) {
@@ -154,7 +155,7 @@ sub random_mutations {
       $abilities{random_ability()}++ for (1..$value);
       for my $ability (keys %abilities) {
         my $rolls = $abilities{$ability};
-        my $modifier = sum(map(sign.roll(dice),(1..$rolls)));
+        my $modifier = sum(map(random_sign.roll(random_dice),(1..$rolls)));
         $abilities{$ability} = $modifier > 0 ? "+$modifier" : $modifier;
       }
 
@@ -169,7 +170,7 @@ sub random_mutations {
       $game_effects{random_game_effect()}++ for (1..$value);
       for my $game_effect (keys %game_effects) {
         my $rolls = $game_effects{$game_effect};
-        my $modifier = sum(map(sign.roll(dice),(1..$rolls)));
+        my $modifier = sum(map(random_sign.roll(random_dice),(1..$rolls)));
         $game_effects{$game_effect} = $modifier > 0 ? "+$modifier" : $modifier;
       }
       
@@ -178,7 +179,7 @@ sub random_mutations {
       }
     }
     elsif ($effect eq 'birth level') {
-      my @levels = map(roll(dice),(1..$value));
+      my @levels = map(roll(random_dice),(1..$value));
       my $birth_level = ORD(max(@levels));
       push @mutations, "born already at $birth_level level";
     }
@@ -199,5 +200,21 @@ sub random_mutations {
   @mutations = sort { textify($a) cmp textify($b) } @mutations;
   return \@mutations;
 }
+
+=head1 NAME
+
+B<RolePlaying::CharacterMutation> allows a DM to mutate characters by generating enhancements, diminishments, and mutations.
+
+=head1 AUTHOR
+
+Lady Aleena
+
+=head1 ACKNOWLEDGEMENTS
+
+Perl Monks: Anno, bradenshep, Corion, ikegami, Limbic~Region, Petruchio, shmem, Sidhekin, wfsp, ysth and the rest of the community that has been so kind to me.
+
+This generator is why I began to learn perl seriously. All my other work in perl stems from this randomness.
+
+=cut
 
 1;

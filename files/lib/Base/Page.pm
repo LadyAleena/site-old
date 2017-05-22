c7a28149fa6bceea3d9d961c0601893cbd6a098a
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 05:11:08 -0400

    Update Page.pm

--- files/lib/Base/Page.pm
+++ files/lib/Base/Page.pm
@@ -7,59 +7,32 @@ our @EXPORT_OK = qw(page story passage);
 use CGI::Carp qw(fatalsToBrowser);
 use Cwd qw(cwd realpath);
 use File::Basename;
+use File::Spec;
 use HTML::Entities qw(encode_entities);
 use List::Util qw(max);
 
-use Base::Root qw(get_root);
+use Base::Menu  qw(base_menu);
+use Base::Path  qw(base_path);
+use Base::Style qw(base_stylesheets);
 use Base::Data qw(get_hash);
 use Base::Inline qw(inline);
 use Base::Line qw(line);
-use Base::Menu qw(main_menu);
-use HTML::Elements qw(html style nav article section heading paragraph blockquote list table anchor img div);
+use HTML::Elements qw(html style noscript nav main article section heading paragraph blockquote list table div pre anchor img);
 use Util::Convert qw(idify textify filify searchify);
 use Util::Columns;
 use Util::ExternalLinks;
 
 my $full_path = realpath($0);
-my $root_path = get_root('path');
-my $root_link = get_root('link');
-my $root_name = get_root('name');
-my $root_user = get_root('user');
-my $root_mail = get_root('mail');
-
-my %link = (
-  'mail' => anchor($root_user, { 'href' => "mailto:$root_mail" }),
-  'name' => anchor($root_name, { 'href' => $root_link }),
-);
+my $root_path = base_path('path');
 
 my $relative_path = $full_path;
    $relative_path =~ s/$root_path\///;
    $relative_path =~ s/\.\w+$//;
 
 my @relative_path_split = split(/\//,$relative_path);
-my @styles = (get_root('css').'/main.css');
+my $styles = base_stylesheets("$root_path/files/css", \@relative_path_split);
 
-sub get_styles {
-  my ($style_dir) = @_;
-
-  while (@relative_path_split) {
-    my $var = shift @relative_path_split;
-       $var =~ s/(\.\w{2,4})$//;
-    next if $var eq 'working';
-
-    if (-f ("$style_dir/$var.css")) {
-      my $new_style = "$style_dir/$var.css";
-         $new_style =~ s/$root_path/$root_link/;
-      push @styles, $new_style;
-    }
-
-    get_styles("$style_dir/$var");
-  }
-
-  return @styles;
-}
-
-sub get_contacts {
+sub contact_links {
   my $contacts = get_hash( 'file' => ['Base', 'other_sites.txt'] );
 
   my @links = ( anchor('Email', { 'href' => 'mailto:fantasy@xecu.net' }) );
@@ -76,46 +49,71 @@ sub get_contacts {
 
 sub page {
   my (%opt) = @_;
-  my $heading = textify(basename($0)) !~ /index/ ? textify(basename($0)) : 'My '.lc((split(/\//,cwd))[-1]);
+  my $heading = basename($0) !~ /index/ ? textify(basename($0)) : (split(/\//, cwd))[-1];
   if ($opt{'heading'}) {
     $relative_path =~ s/\/.+$/\/$opt{'heading'}/;
   }
-  my $title = textify(join(' - ', ($root_name, map(ucfirst, grep(!/index/, split(/\//, $relative_path))))));
+  my $title = textify(join(' - ', ('Lady Aleena', map(ucfirst, grep(!/index/, split(/\//, $relative_path))))));
   my $page_heading = $opt{'heading'} ? textify($opt{'heading'}) : textify($heading);
-  my $article_id = idify($page_heading);
-  my $menu = main_menu( 'directory' => $root_path, 'tab' => 2, 'color' => 0, 'full' => 0, 'file menu' => $opt{'file menu'} ? $opt{'file menu'} : undef );
+  my $main_id = idify($page_heading);
+  my $menu = base_menu( 'directory' => $root_path, 'tab' => 2, 'color' => 0, 'full' => 0, 'file menu' => $opt{'file menu'} ? $opt{'file menu'} : undef );
+  my $js = File::Spec->abs2rel("$root_path/files/lib/myjs.js");
 
   html(0, {
     'head' => {
       'title'    => $title,
-      'links'    => [map {{ 'rel' => 'stylesheet', 'type' => 'text/css', 'href' => $_ }} get_styles($root_path.'/files/css')],
+      'links'    => $styles,
       'scripts'  => [
         { 'type' => 'text/javascript', 'src' => 'https://code.jquery.com/jquery-1.11.1.min.js' },
-        { 'type' => 'text/javascript', 'src' => "$root_link/files/lib/myjs.js" }
+        { 'type' => 'text/javascript', 'src' => $js }
       ],
-      'meta'     => [{'http-equiv' => 'Content-Type', 'content' => 'text/html; charset=windows-1252'}],
-      'noscript' => sub { style(3, 'li.closed ol,li.closed ul,li.closed dl {display:block;}') },
+      'meta'     => [
+        {'http-equiv' => 'Content-Type', 'content' => 'text/html; charset=windows-1252'},
+        {'http-equiv' => 'Cache-Control', 'content' => 'max-age=300'},
+        {'name' => 'viewport', 'content' => 'width=device-width, initial-scale=1'}
+      ],
+      'noscript' => sub { style(3, 'li.closed ol, li.closed ul, li.closed dl {display:block;}') },
     },
     'body' => [
       sub {
         nav(2, sub {
           section(3, sub {
-            list(4, 'u', $menu, { 'id' => 'site_menu', 'onclick' => 'list_onclick(event)' }
-            );
+            div(4, sub {
+              line(5, join(' ', contact_links())) 
+            }, { 'id' => 'contacts_main' });
+            list(4, 'u', $menu, { 'id' => 'site_menu', 'onclick' => 'list_onclick(event)' } );
           });
         }, { 'id' => 'main' });
-        article(2, sub {
+        main(2, sub {
           &{$opt{'code'}};
-        }, { 'id' => $article_id, 'heading' => [1, $page_heading, { 'style' => $page_heading =~ /Lady Aleena$/ ? 'display: none' : undef }] });
+          section(3, sub {
+          div(4, '', { 'id' => 'disqus_thread'} );
+          line(4, qq(<script type="text/javascript">
+            /* * * CONFIGURATION VARIABLES * * */
+            var disqus_shortname = 'ladyaleena';
+            var disqus_identifier = '$page_heading';
+            
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        </script>));
+          noscript(4, q(Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus</a>.));
+          }, { 'heading' => [2, 'Comments', { 'class' => 'disqus' }], 'class' => 'disqus' } );
+        }, { 'id' => $main_id, 'heading' => [1, $page_heading, { 'style' => $page_heading =~ /Lady Aleena$/ ? 'display: none' : undef }] });
       },
       {
         'header' => [
           sub {
-            div(3, sub { print join(' ', get_contacts()) }, { 'id' => 'contacts', 'class' => 'no_indent' });
-            div(3, sub { print
-              img({ 'src' => "$root_link/files/images/avatar.jpg", 'alt' => "Lady Aleena's avatar", 'style' => 'height:100%; float:left;' }).
-              anchor('Lady Aleena', { 'href' => $root_link, 'title' => 'Home', 'style' => 'padding-left:.1em;'})
-            }, { 'style' => 'height:100%; vertical-align:middle; font-size:250%' });
+            div(3, sub {
+              line(4, join(' ', contact_links())) 
+            }, { 'id' => 'contacts' });
+            div(3, sub { line(4,
+              img({ 'src' => File::Spec->abs2rel("$root_path/files/images/avatar.jpg"), 'alt' => "Lady Aleena's avatar", 'class' => 'avatar' }).
+              anchor('Lady Aleena', { 'href' => File::Spec->abs2rel($root_path), 'title' => 'Home', 'class' => 'home'})
+            )}, { 'style' => 'height:100%;' });
           }
         ],
         'class' => $opt{'class'} ? $opt{'class'} : undef
@@ -155,7 +153,8 @@ sub dissect_source {
     
     push @{$sections[$inc]}, $line;
   }
-
+  push @toc, [anchor('Comments', { 'href' => '#Comments' })];
+  
   return { 'cols' => $cols, 'toc' => \@toc, 'sections' => \@sections };
 }
 
@@ -172,21 +171,32 @@ sub row_line {
   for my $cell (@row) {
     push @{$row_data}, $cell =~ /^r(\d)\s(.+)$/ ? [inline(convert_string($2, $opt->{'line magic'})), { 'rowspan' => $1 }] :
                        $cell =~ /^c(\d)\s(.+)$/ ? [inline(convert_string($2, $opt->{'line magic'})), { 'colspan' => $1 }] :
-                       inline(convert_string($cell, $opt->{'line magic'}));
+                       $cell && length $cell    ? inline(convert_string($cell, $opt->{'line magic'})) : '&nbsp';
   }
   return $row_data;
 }
+
+# |# is id
+# || is class(es)
+# |! is caption
+# |* is head row
+# |+ is row starting with head
+# |- is a normal row
 
 sub table_opts {
   my ($lines, $opt) = @_;
   my $table_opts;
   
-  my $match = '|!*+-';
+  my $match = '#|!*+-';
   for (my $lineno = 0; $lineno < @$lines; $lineno++) {
     my $line = $lines->[$lineno];
     $match = substr($line, 0, 1);
 
-    if ($match eq '|') {
+    if ($match eq '#') {
+      $line =~ s/^# (.+)$/$1/;
+      $table_opts->{'id'} = $line;
+    }
+    elsif ($match eq '|') {
       $line =~ s/^\| (.+)$/$1/;
       $table_opts->{'class'} = $line;
     }
@@ -215,21 +225,23 @@ sub table_opts {
   return $table_opts;
 }
 
-# list_type and get_list are written by [hdb].
+# list_type and list_opts are written by [hdb].
 # [ig] wrote a version too.
 sub list_type {
   my %type = ( '*' => 'u', '#' => 'o' );
   $type{ substr shift, -1 }
 }
 
-sub get_list {
+sub list_opts {
   my ($list, $opt) = @_;
   $list     = [ map (inline(convert_string($_, $opt->{'line magic'})), @$list) ];
+
   my @lines = map {
     /([*#]*)(\d*)\s+(.*)/;
     $2 ? [$1, [$3, {value => $2}]] : [$1, $3]
   } @$list;
   my $maxlevel = max map { length $_->[0] } @lines;
+
   while( $maxlevel ) {
     my @indices = grep { $maxlevel == length $lines[$_]->[0] } 0..@lines - 1;
     while( @indices ) {
@@ -240,7 +252,7 @@ sub get_list {
         list_type($lines[$start]->[0]),
         [ map { $_->[1] } splice @lines, $start, $end-$start + 1 ]
       ];
-      $lines[$start-1]->[1] = [ $lines[$start-1]->[1], { 'inlist' => $sublist} ] if $maxlevel > 1;
+      $lines[$start-1]->[1] = [ $lines[$start-1]->[1], { 'inlist' => $sublist } ] if $maxlevel > 1;
       splice @lines, $start, 0, $sublist if $maxlevel == 1;
     }
     $maxlevel--;
@@ -261,7 +273,7 @@ sub heading_w_links {
 
 sub passage {
   my ($tab, $section, $opt) = @_;
-  my $match = '\*#|';
+  my $match = ':\*#|';
 
   for (my $lineno = 0; $lineno < @$section; $lineno++) {
     my $line = $section->[$lineno];
@@ -274,42 +286,49 @@ sub passage {
 
       my @list_lines = @{$section}[$start..$end];
 
-      if ($match eq '|') {
+      $tab++;
+      if ($match eq ':') {
+        my $class = $list_lines[0] =~ s/^:\| (.+)$/$1/ ? shift @list_lines : undef;
+        pre($tab, sub { print join("\n", map(substr($_, 2), @list_lines)) }, { 'class' => $class });
+      }
+      elsif ($match eq '|') {
         my @list = map { $_ =~ s/^\|(.+)/$1/; $_; } @list_lines;
         my $opts = table_opts(\@list, $opt);
-        table($tab + 1, $opts);
+        table($tab, $opts);
       }
       else {
         my $class = $list_lines[0] =~ s/^[\*#]\| (.+)$/$1/ ? shift @list_lines : undef;
-        my @list = get_list(\@list_lines, $opt);
+        my @list = list_opts(\@list_lines, $opt);
         $list[0][2]->{'class'} = $class ? $class : undef;
-        list($tab + 1, @{$list[0]});
+        list($tab, @{$list[0]});
       }
+      $tab--;
 
       $lineno = $end;
-      $match = '\*#|';
+      $match = ':\*#|';
     }
     else {
       heading_w_links($tab, $1, $2), next if $line =~ /^([1-6])\s+(.+) \+$/;
       heading($tab, $1, textify($2), { 'id' => idify($2) }), next if $line =~ /^([1-6])\s+(.+)/;
-      div($tab, $2, { 'class' => "h$1" }), next if $line =~ /^([7-8])\s+(.+)/;
 
+      $tab++;
       $opt->{'doc magic'}->{$1}->(), next if $line =~ /^&\s+(.+)/;
-      line($tab + 1, $line),     next if $line =~ /^<.+>/;
-      line($tab + 1, "<$line>"), next if $line =~ /^[bh]r$/;
-      blockquote($tab + 1, inline(convert_string($1, $opt->{'line magic'}))),  next if $line =~ /^bq\s(.+)/;
+      line($tab, $line),     next if $line =~ /^<.+>/;
+      line($tab, "<$line>"), next if $line =~ /^[bh]r$/;
+      blockquote($tab, inline(convert_string($1, $opt->{'line magic'}))),  next if $line =~ /^bq\s(.+)/;
       # paragraphs
-      paragraph($tab + 1, inline(convert_string($1, $opt->{'line magic'})),    { 'class' => 'stanza', 'break' => '\|' }), next if $line =~ /^stanza (.+)$/;
-      paragraph($tab + 1, inline(convert_string($2, $opt->{'line magic'})),    { 'class' => "indent$1"}),   next if $line =~ /^\>(\d+) (.+)$/;
-      paragraph($tab + 1, inline(convert_string($line, $opt->{'line magic'})), { 'class' => 'author' }), next if $line =~ /^(?:by|with|from|as) /;
-      paragraph($tab + 1, inline(convert_string($line, $opt->{'line magic'})));
+      paragraph($tab, inline(convert_string($1, $opt->{'line magic'})),    { 'class' => 'stanza', 'break' => '\|' }), next if $line =~ /^stanza (.+)$/;
+      paragraph($tab, inline(convert_string($2, $opt->{'line magic'})),    { 'class' => sprintf "indent_%02s", $1}), next if $line =~ /^\>(\d+) (.+)$/;
+      paragraph($tab, inline(convert_string($line, $opt->{'line magic'})), { 'class' => 'author' }),  next if $line =~ /^(?:by|with|from|as) /;
+      paragraph($tab, inline(convert_string($line, $opt->{'line magic'})));
+      $tab--;
     }
   }
 }
 
 sub story {
   my ($source, $opt) = @_;
-  
+
   my $d_source = dissect_source($source);
   my $cols     = $d_source->{'cols'};
   my $toc      = $d_source->{'toc'};
@@ -318,11 +337,11 @@ sub story {
   my $tab = 2;
   my $inc = 0;
   for my $section (@$sections) {
-    section($tab, sub { passage($tab + 1, $section, $opt) }) if $section;
-    
+    section($tab + 1, sub { passage($tab + 2, $section, $opt) }, { 'class' => 'content' }) if $section;
+
     if ($inc == 0 && $cols > 3) {
-      nav($tab, sub {
-        my $class = get_columns(4, $cols);
+      nav($tab + 1, sub {
+        my $class = number_of_columns(4, $cols, 1);
         list($tab + 2, 'u', $toc, { 'class' => $class });
       }, { 'heading' => [2, 'Table of contents'], 'class' => 'contents'} );
     }

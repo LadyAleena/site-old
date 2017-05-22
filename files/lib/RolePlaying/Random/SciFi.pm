59a85b07e94a31987a55f7f767356640d0679c07
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 04:22:06 -0400

    Update SciFi.pm

--- files/lib/RolePlaying/Random/SciFi.pm
+++ files/lib/RolePlaying/Random/SciFi.pm
@@ -4,31 +4,26 @@ use warnings FATAL => qw(all);
 use Exporter qw(import);
 our @EXPORT_OK = qw(random_HHGTTG_sector random_MIB_agent);
 
-my @letters = ('A'..'Z');
-my @Greeks = qw(Alpha Beta Gamma Delta Epsilon Digamma Zeta Eta Theta Iota Kappa Lambda Mu Nu Xi Omicron Pi San Qoppa Rho Sigma Tau Upsilon Phi Chi Psi Omega Sampi);
-my @types = qw(Active Heavy Light Over Plural Passive Single Under);
-
-sub random_letter {
-  return $letters[rand @letters];
-}
+use RolePlaying::Random::Misc qw(random_alpha random_Greek_letter);
 
 sub random_letters {
   my ($number) = @_;
-  my $letter = sub { random_letter };
+  my $letter = sub { random_alpha };
   my $letters;
   $letters .= &$letter for (1..$number);
   return $letters;
 }
 
 sub random_HHGTTG_sector {
-  my $Greek = $Greeks[rand @Greeks];
-  my $type = $types[rand @types];
+  my @types = qw(Active Heavy Light Over Plural Passive Single Under);
+  my $Greek = sub { random_Greek_letter };
+  my $type  = sub { $types[rand @types] };
   return random_letters(2)." ".$type." ".random_letter." ".$Greek;
 }
 
 sub random_MIB_agent {
   my @letters = (1,2);
-  my $letter  = $letters[rand @letters];
+  my $letter  = sub { random_alpha };
   return random_letters($letter);
 }
 

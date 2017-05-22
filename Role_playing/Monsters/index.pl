e9b609baf29218006aefa3c0354e5d6b28a6c860
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 13:53:03 -0400

    Update index.pl

--- Role_playing/Monsters/index.pl
+++ Role_playing/Monsters/index.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings FATAL => qw( all );
 
 use CGI::Minimal;
+use CGI::PathInfo;
 use CGI::Carp qw(fatalsToBrowser);
 use HTML::Entities qw(encode_entities);
 use Lingua::EN::Inflect qw(A);
@@ -11,7 +12,7 @@ use Lingua::EN::Inflect qw(A);
 use lib '../../files/lib';
 use Base::Page qw(page);
 use Base::Menu qw(file_menu);
-use HTML::Elements qw(nav section paragraph list anchor);
+use HTML::Elements qw(section paragraph list);
 use Util::Convert qw(textify searchify);
 use RolePlaying::Monster qw(print_monster);
 use RolePlaying::Random::Monster qw(random_monster random_monster_list);
@@ -21,8 +22,10 @@ my @selects = ('Chaos elemental-kin', 'Daemar', 'Dark centaur', 'Demi-lycanthrop
 
 my $cgi = CGI::Minimal->new;
 my $select = $cgi->param('monster') ? encode_entities($cgi->param('monster'),'<>"') : undef;
+
 my $head = $select && grep(/\Q$select\E/, @selects) ? $select : undef;
 my $file_menu = file_menu('monster', \@selects , $select);
+
 page( 'heading' => $head, 'file menu' => $file_menu, code => sub {
   if ($select && grep(/\Q$select\E/, @selects)) {
     print_monster($select) if $select ne 'Random monsters';
@@ -30,7 +33,7 @@ page( 'heading' => $head, 'file menu' => $file_menu, code => sub {
   }
   else {
     section (3, sub {
-      paragraph(3, "You can read about several new monsters or generate a list of random monsters from the Monstrous Manual and its many appendixes. Look there's $random_monster!");
+      paragraph(3, "You can read about several new monsters or generate a list of random monsters from the <i>Monstrous Manual</i> and its many appendixes. Look there's $random_monster!");
       list(5, 'u', $file_menu, { 'class' => 'two' });
     });
   }

932955c6ec787dc22e890360447d48b738eb29de
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-22 04:42:08 -0400

    Update HitDice.pm

--- files/lib/RolePlaying/CharacterBuilding/HitDice.pm
+++ files/lib/RolePlaying/CharacterBuilding/HitDice.pm
@@ -2,12 +2,12 @@ package RolePlaying::CharacterBuilding::HitDice;
 use strict;
 use warnings FATAL => qw( all );
 use Exporter qw(import);
-our @EXPORT_OK = qw(get_hit_dice roll_hit_points);
+our @EXPORT_OK = qw(hit_dice roll_hit_points);
 
 use Games::Dice qw(roll);
 use POSIX qw(ceil);
 
-use RolePlaying::CharacterBuilding::Class qw(convert_class get_level);
+use RolePlaying::CharacterBuilding::Class qw(convert_class class_level);
 
 my %classes;
 while (my $line = <DATA>) {
@@ -17,17 +17,17 @@ while (my $line = <DATA>) {
   $classes{$class}{plus}      = $plus;
 }
 
-sub get_hit_dice {
+sub hit_dice {
   my ($class,$opt) = @_;
   
   my $die;
   if (ref($class) eq 'ARRAY') {
-    my @die_array = map(get_hit_dice($_,$opt),@{$class});
+    my @die_array = map(hit_dice($_,$opt),@{$class});
     $die = [@die_array];
   }
   else {
     $class = convert_class($class,'HitDice');
-    my $level = $opt->{'level'} ? $opt->{'level'} : get_level($class, $opt->{'experience'});
+    my $level = $opt->{'level'} ? $opt->{'level'} : class_level($class, $opt->{'experience'});
 
     my $max_level = $classes{$class}{max_level};
 
@@ -48,13 +48,13 @@ sub roll_hit_points {
   
   my $roll;
   if (ref($class) eq 'ARRAY') {
-    $roll += roll(get_hit_dice($_,$opt)) for @{$class};
+    $roll += roll(hit_dice($_,$opt)) for @{$class};
     $roll = ceil($roll / scalar(@{$class}));
   }
   else {
-    my $level = $opt->{level} ? $opt->{level} : get_level($class,$opt->{experience});
+    my $level = $opt->{level} ? $opt->{level} : class_level($class,$opt->{experience});
 
-    my $dice = get_hit_dice($class, {level => $level });
+    my $dice = hit_dice($class, {level => $level });
     $roll = roll($dice);
   }
   return $roll;
@@ -64,24 +64,24 @@ sub roll_hit_points {
 
 =head1 HitDice
 
-B<HitDice> returns the die type and amount of dice needed to roll the hit points for your character with C<get_hit_dice> or just rolls your character's hit points with C<roll_hit_points>.
+B<HitDice> returns the die type and amount of dice needed to roll the hit points for your character with C<hit_dice> or just rolls your character's hit points with C<roll_hit_points>.
 
 =head2 Usage
 
-For both C<get_hit_dice> and C<roll_hit_points>, you will need your chracter's class or classes and level or experience points. Neither of the two functions are exported by default, so you will have to call them.
+For both C<hit_dice> and C<roll_hit_points>, you will need your chracter's class or classes and level or experience points. Neither of the two functions are exported by default, so you will have to call them.
 
-  use RolePlaying::CharacterBuilding::HitDice qw(get_hit_dice roll_hit_points);
+  use RolePlaying::CharacterBuilding::HitDice qw(hit_dice roll_hit_points);
 
 The classes are C<warrior>, C<rogue>, C<priest>, C<wizard>, C<psionisist>, C<chaos warden>, or C<theopsyelementalist>. The latter two classes are my creations.
 
-=head3 C<get_hit_dice>
+=head3 C<hit_dice>
 
-  get_hit_dice($class, { level => $level });
+  hit_dice($class, { level => $level });
 
 or
 
-  get_hit_dice($class, { experience => $xp });
-  get_hit_dice([$class1,$class2], { experience => $xp});
+  hit_dice($class, { experience => $xp });
+  hit_dice([$class1,$class2], { experience => $xp});
   
 =head3 C<roll_hit_points>
 
@@ -92,7 +92,7 @@ or
   roll_hit_points($class, { experience => $xp });
   roll_hit_points([$class1,$class2], { experience => $xp});
 
-=head2 Author
+=head1 AUTHOR
 
 Lady Aleena
 

44e95f043e011a12290014ab93be3a29a2bd76f2
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-26 00:56:26 -0400

    Create index.pl

---
+++ Role_playing/Locations/Afma/index.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+# This is the Afma index.
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../../files/lib';
+use Base::Page qw(page story);
+
+page( 'code' => sub { story(*DATA) });
+
+__DATA__
+2 The History of Afma
+At one time the land was peaceful and there was land for all. The people began to drift from the gods, and the gods came together to remind the people that they are supreme. The rains came, the heat started to rise all over the planet, the polar caps started to melt, sinkholes formed creating new lakes, and the land became a bog. Even the desert sands became hard to travel on because of the increased water underneath the ground.
+At the same time, volcanoes started erupting. Mountains rose higher and higher. New volcanoes formed where none had been.
+The world became one of steam. In some places there would be whole months of constant fog. Other areas suffered from flash floods where the water would not recede. Out in the oceans the currents became fiercer.
+People began dying such as the infirm who could not handle the increased water. Pestilence ravaged areas of the world because of the dampness. The floods took the lives of all who were in the paths of the water.
+The water still rose.
+For years this happened. Lives were lost. People migrated to the hills and then the mountains. Families were torn apart. Not a single race was spared.
+After fifty years of rain and heat, the rains subsided and the heat returned to normal, but nothing of what was was left.  Those trapped on mountaintops came together to form new governments. Old enemies may become friends. Some places had conflicts to see who would rule. In the end the people remembered the gods. Some marveled at how much water fell from the sky. There was no way for there to be that much water unless there was godlike interference.
+Since they did not remember the names of the gods, the peoples created names for them. All the gods rejoiced in the worship of the people and accepted the new names. All but one celebrated.
+The A<Aleena|href="Aleena.pl">, the Overpower of Afma, looked on to what her children had done. When she saw the full damage, she smote her children in such a way that they would never have that power again. She took most of their power away, and some gods were lost in her wrath. Some mortals were elevated to godhood for their various feats of wisdom, courage, strength, kindness, and other goals set forth by the overpower. She took residence on the largest of the islands and created a palace for herself there.
+Slowly the water is receding, but it will be ages before what was will be again.
+2 Afma Now
+Afma is a world full of islands. Some of the islands are so close to each other that there are bridges between them. Some of these bridges are floating bridges that are are just floating right on top of the water. Those bridges are sturdy enough for horses and their riders to cross without any problems, but there is always the possibility that they could break from weather, age, animal or monster attack, and other random events.
+Some islands are so small that there are only small fishing villages protected by their Lords. Some are large enough to have cities on them and maybe even small forests. Others may be actively volcanic. There may be a cluster of islands that seem to be one big island at low tide because of sandbars, or islands in the polar regions that are locked together by ice. There can be islands, floating on the water, which do not have any connection to the ocean floor or they could be constructed. There could be islands that are not even in the water just floating in air.
+Each island has a place on it that is magically linked to A<Aleenia|href="Aleenaia.pl">, the Isle of the Goddess. No matter how far away from the Isle the island is, a magical bridge will appear to transport those who are worthy to the Isle. There are no other bridges to the Isle and the currents are such that ships could not dock there without the blessing of the Goddess.
+Afma has three bands of ice that bisect the planet creating large arctic regions. In the center of these triangular arctic areas are the eight tropical regions.
+One of the problems with this world is finding fresh water. Fresh water wells are tightly guarded. Some islands have the magic or technology to distill the ocean water. Decanters of endless water and other items that create drinkable water are highly prized. Another problem is travel between islands. Unless there is enough material to build ships, some are trapped on the island where they were born. Teleportation magic and technology is on the rise making travel between islands easier.
+2 Create an Island on Afma
+The best thing about this is that since islands don't have to be near each other, every island or island cluster becomes a mini-world. One island cluster could have magic developed to such a degree that it's almost like a modern city where it uses magic instead of technology to accomplish the luxuries of life. The next island cluster could be like feudal Japan or has highly evolved ogres where the humans are the monsters. Bouncing from island cluster to island cluster could create an unlimited source of adventure for an unlimited number of styles. Island clusters are nearly the same in technology, what creatures are good and evil, that sort of thing. Each island cluster can have its own religions, pantheons, etc.
+There is more that can be done with islands than with one major land mass. Pick a region of the world and plunk your island there. It would be harder to vie for space on a continent. Don't worry about running out of space on this world, it will grow as more islands and island clusters are added.
+Please use the A<comments section|href="#disqus"> below to add a new island or island cluster.
+2 North - West One
+3 Tianna
+by A<Jill|href="mailto:Lilnutta@aol.com">
+A small village called Kiarba is in the heart of Tienna where Mortals gather and take solace. Around the village are gnomes, pixies, elves, unicorns and dragons in the darkest forest where Enchanted trees have been known to walk around. There is a small path that leads through the forest, where you must travel by horse and through the clearing is a castle where the Lord Alexander lives. He is a good lord and his army are loyal men, but he is battling to overcome the black dragon in the hills of Tienna, the black dragon terrorizes people in the village.
+2 Note
+Afma and its subpages are cobbled together from various places I have been a part of on the internet. There may be inconsistancies. If you see one, please point it out to me.

6ae22f6669f45c2b764f47fd4959343f7a92f3f1
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-26 00:45:28 -0400

    Update and rename files/lib/RolePlaying/CharacterBuilding/RogueSkills.pm to files/lib/RolePlaying/Character/GameTable/RogueSkills.pm

--- files/lib/RolePlaying/Character/GameTable/RogueSkills.pm
+++ files/lib/RolePlaying/Character/GameTable/RogueSkills.pm
@@ -1,10 +1,10 @@
-package RolePlaying::CharacterBuilding::RogueSkills;
+package RolePlaying::Character::GameTable::RogueSkills;
 use strict;
 use warnings FATAL => ( 'all' );
 use Exporter qw(import);
 our @EXPORT_OK = qw(thief_skills bard_skills rogue_skills rogue_skills_table_rows);
 
-use RolePlaying::CharacterBuilding::Class qw(class_level);
+use RolePlaying::Character::Class qw(class_level);
 
 # part of the Character Building table suite.
 

da76c4940b9482ff87362002a7351f542c6c9f04
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-27 17:04:34 -0400

    Update Alignment.pm

--- files/lib/RolePlaying/Random/Alignment.pm
+++ files/lib/RolePlaying/Random/Alignment.pm
@@ -49,23 +49,6 @@ sub random_alignment {
   return $alignment;
 }
 
-sub expand_alignment {
-  my ($var) = @_;
-  $var = uc $var;
-  $var =~ s/^(-\w|\w\+|\w\(\w\)|\w)(-\w|\w\+|\w\(\w\)|\w)$/$1 and $2/;
-  $var =~ s/\((\w)\)/ with $1 tendencies/g;
-  $var =~ s/(\w)\+/true $1/g;
-  $var =~ s/-/non-/g;
-  $var =~ s/T N/true neutral/;
-  $var =~ s/N/neutral/g;
-  $var =~ s/L/lawful/;
-  $var =~ s/C/chaotic/;
-  $var =~ s/G/good/;
-  $var =~ s/E/evil/;
-  $var =~ s/^(\w+) and (\w+)$/$1 $2/;
-  return $var;
-}
-
 =head1 NAME
 
 B<RolePlaying::Random::Alignment> generates random and expands alignments for AD&D 2nd Edition.

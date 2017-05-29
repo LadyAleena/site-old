b53c05e7c81602e2019e9b7528d123f56ea362da
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-28 23:25:26 -0400

    Update Monster.pm
    Moved all printing to the accompanying script to make this module more useful for other things.

--- files/lib/RolePlaying/Monster.pm
+++ files/lib/RolePlaying/Monster.pm
@@ -1,104 +1,61 @@
 package RolePlaying::Monster;
 use strict;
-use warnings FATAL => qw(all);
+use warnings; # FATAL => qw( all );
 use Exporter qw(import);
-our @EXPORT_OK = qw(print_monster);
+our @EXPORT_OK = qw(monster_info);
 
-use CGI::Carp qw(fatalsToBrowser);
-use File::Basename;
-use IO::All;
+use Base::Data qw(data_file make_hash);
+use Util::Convert qw(filify);
 
-use Base::Page qw(passage);
-use Base::Data qw(get_hash get_array data_file);
-use HTML::Elements qw(section list table);
-use Util::Convert qw(filify textify);
+my @selects = ('Chaos elemental-kin', 'Daemar', 'Dark centaur', 'Demi-lycanthrope', 'Dragod', 'Eldent', 'Gobpry', 'Phase elemental', 'Rainbow dragonette', 'Throglin', 'Tralg', 'Trobold', 'Twarg', 'Zarden');
+my @monster_headings = (
+  'Monster', 'Climate/Terrain', 'Frequency', 'Organization', 'Activity cycle', 'Diet', 'Intelligence', 'Treasure', 'Alignment', 'No. Appearing', 'Armor Class',
+  'Movement', 'Hit Dice', 'THAC0', 'No. of Attacks', 'Damage/Attack', 'Special Attacks', 'Special Defenses', 'Magic Resistance', 'Size', 'Morale', 'XP Value'
+);
+my @long_values = ('Appearance', 'Combat', 'Habitat/Society', 'Ecology', 'Variants', 'Note');
 
-my @monster_headings = ('Monster','Climate/Terrain','Frequency','Organization','Activity cycle','Diet','Intelligence','Treasure','Alignment','No. Appearing','Armor Class','Movement','Hit Dice','THAC0','No. of Attacks','Damage/Attack','Special Attacks','Special Defenses','Magic Resistance','Size','Morale','XP Value');
-my $monsters = get_hash(
+my $monsters = make_hash(
   'file' => ['Role_playing','Monsters.txt'],
   'headings' => \@monster_headings,
 );
-my @long_values = ('Appearance','Combat','Habitat/Society','Ecology','Variants','Note');
+$monsters->{'Wild hairs'}{'Monster'} = 'Wild hairs';
 
-my %multi_monsters = (
-  'Throglin' => ['Throglin','Freshwater throglin','Saltwater throglin'],
-  'Tralg'    => ['Arctic tralg','Desert tralg','Two-headed tralg'],
-  'Twarg'    => ['Hill twarg','Mountain twarg','Jungle twarg'],
-);
+sub monster_attributes {
+  my ($heading_level, $in_monster) = @_;
+  my $monster_data = $in_monster;
+  my $attributes;
 
-my %rainbow_dragonette;
-my %rainbow_dragonette_headings = (
-  'odf' => ['Color','Breath Weapon','Range','Immunities'],
-  'age' => ['Age','Body Lgt. (")','Tail Lgt. (")','AC','Breath Weapon','Spells Wizard','MR','Treas. Type','XP Value'],
-);
+  for my $attribute (@monster_headings) {
+    push @$attributes, qq(<strong class="caps">$attribute:</strong> $monster_data->{$attribute}) if $monster_data->{$attribute};
+  }
 
-for my $param ('odf','age') {
-  $rainbow_dragonette{$param} = get_array(
-    'file'     => ['Role_playing/Monsters',"Rainbow_dragonette_$param.txt"],
-    'headings' => \@{$rainbow_dragonette_headings{$param}},
-  );
+  return $attributes;
 }
 
-sub dragonette_table {
-  my ($var) = @_;
+sub monster_info {
+  my ($heading_level, $in_monster, $opt) = @_;
+  my $monster = $monsters->{$in_monster};
+     $monster->{attributes} = monster_attributes($heading_level, $monster);
 
-  my @rows;
-  for my $row (@{$rainbow_dragonette{$var}}) {
-    push @rows, [map($row->{$_}, @{$rainbow_dragonette_headings{$var}})];
-  }
-  table(4, { class => 'rainbow_dragonette centered', rows => [['header', [$rainbow_dragonette_headings{$var}]], ['data', \@rows]] });
-}
+  my $file_name = filify($in_monster);
+  my $file = data_file('Role_playing/Monsters', "$file_name.txt");
+  open(my $fh, '<', $file) || warn "$file: $!";
 
-my $doc_magic = {
-  'odf_table' => sub { dragonette_table('odf') },
-  'age_table' => sub { dragonette_table('age') },
-};
+  my $heading = 'description';
+  my $inc = 0;
+  while (my $line = <$fh>) {
+    chomp $line;
+    next if !$line;
 
-sub print_monster {
-  my ($in_monster) = @_;
-  my $in_monsters = $multi_monsters{$in_monster} ? $multi_monsters{$in_monster} : [$in_monster];
-  my @monsters = @$in_monsters;
-  my $columns = @monsters > 1 ? 'multi' : 'two';
-  my @lists;
-  my @passages;
-  for my $monster (@monsters) {
-    my $heading_level = 2;
-    my $heading = @monsters > 1 ? [ $heading_level, $monster ] : undef;
-    $heading_level++ if @monsters > 1;
-    
-    my @items;
-    for my $key (@monster_headings) {
-      push @items, $monsters->{$monster}{$key} ? [qq(<strong class="caps">$key:</strong> $monsters->{$monster}{$key}), { class => 'monster_attribute' }] : '';
+    if ($line =~ /^\! /) {
+      my ($marker, $text) = split(/ /, $line, 2);
+      $heading = $text if ($opt->{slurp} !~ /^[yt1]/);
+      $line =~ s/^\!/$heading_level/ if $heading_level;
+      next if !$heading_level;
     }
-    push @lists, [\@items, $columns];
-    
-    my $file = filify($monster).'.txt';
-    my @file_lines = io(data_file('Role_playing/Monsters',$file))->slurp;
-    chomp @file_lines;
-
-    my %long_fields;
-    @long_fields{@long_values} = @file_lines;
-
-    my @lines;
-    for my $value (@long_values) {
-      next if !$long_fields{$value};
-      my @in_lines = split(/\\/, $long_fields{$value});
-      unshift @in_lines, "$heading_level $value";
-      push @lines, @in_lines;
-    }
-    push @passages, [$heading, \@lines];
+    push @{$monster->{$heading}}, $line;
   }
-
-  section(3, sub {
-    for my $list (@lists) {
-      list(4, 'u', $list->[0], { class => "monster $list->[1]" });
-    }
-  });
-  for my $passage (@passages) {
-    section(3, sub {
-      passage(4, $passage->[1], { 'doc magic' => $doc_magic });
-    }, { heading => $passage->[0] });
-  }
+  return $monster;
 }
 
 1;

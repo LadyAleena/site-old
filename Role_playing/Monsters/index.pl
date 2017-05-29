1fc4b980a761b8c11901fbd1f0f45fffdf32b844
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-28 23:24:25 -0400

    Update index.pl
    Expanded to include more printing to make the accompanying module non-printing.

--- Role_playing/Monsters/index.pl
+++ Role_playing/Monsters/index.pl
@@ -4,18 +4,22 @@ use strict;
 use warnings FATAL => qw( all );
 
 use CGI::Minimal;
-use CGI::PathInfo;
 use CGI::Carp qw(fatalsToBrowser);
 use HTML::Entities qw(encode_entities);
 use Lingua::EN::Inflect qw(A);
 
 use lib '../../files/lib';
-use Base::Page qw(page);
+use Base::Page qw(page passage);
 use Base::Menu qw(file_menu);
 use HTML::Elements qw(section paragraph list);
-use Util::Convert qw(textify searchify);
-use RolePlaying::Monster qw(print_monster);
+use RolePlaying::Monster qw(monster_info);
 use RolePlaying::Random::Monster qw(random_monster random_monster_list);
+
+my $multi_monsters = {
+  'Throglin' => ['Throglin', 'Freshwater throglin', 'Saltwater throglin'],
+  'Tralg'    => ['Arctic tralg', 'Desert tralg', 'Two-headed tralg'],
+  'Twarg'    => ['Hill twarg', 'Mountain twarg', 'Jungle twarg'],
+};
 
 my $random_monster = A(random_monster);
 my @selects = ('Chaos elemental-kin', 'Daemar', 'Dark centaur', 'Demi-lycanthrope', 'Dragod', 'Eldent', 'Gobpry', 'Phase elemental', 'Rainbow dragonette', 'Throglin', 'Tralg', 'Trobold', 'Twarg', 'Zarden', 'Random monsters');
@@ -28,8 +32,44 @@ my $file_menu = file_menu('monster', \@selects , $select);
 
 page( 'heading' => $head, 'file menu' => $file_menu, code => sub {
   if ($select && grep(/\Q$select\E/, @selects)) {
-    print_monster($select) if $select ne 'Random monsters';
-    section(3, sub { list(5, 'u', random_monster_list) }) if $select eq 'Random monsters';
+    if ($select ne 'Random monsters') {
+      if ($multi_monsters->{$select}) {
+        my $monster_attributes;
+        my $monster_descriptions;
+        for my $sub_monster (@{$multi_monsters->{$select}}) {
+          my $new_monster = monster_info(3, $sub_monster, { slurp => 'yes' });
+          my $attributes = $new_monster->{attributes};
+          my $description = $new_monster->{description} ? $new_monster->{description} : undef;
+
+          push @$monster_attributes, $attributes;
+          push @$monster_descriptions, { heading => $sub_monster, description => $description};
+        }
+        section(3, sub {
+          for my $list (@$monster_attributes) {
+            list(4, 'u', $list, { class => 'monster multi' }); # the lists for all the monsters are printed before descriptions
+          }
+        });
+        for my $monster_description (@$monster_descriptions) {
+          # the descriptions for all the monsters are printed after the lists
+          section(3, sub {
+            print passage(5, $monster_description->{description}) if $monster_description->{description};
+          }, { heading => [2, $monster_description->{heading}] });
+        }
+      }
+      else {
+        my $new_monster = monster_info(2, $select, { slurp => 'yes' });
+        my $attributes  = $new_monster->{attributes};
+        section(3, sub {
+          list(4, 'u', $attributes, { class => 'monster two' });
+        });
+        section(3, sub {
+          print passage(4, $new_monster->{description}) if $new_monster->{description};
+        });
+      }
+    }
+    else {
+      section(3, sub { list(5, 'u', random_monster_list) });
+    }
   }
   else {
     section (3, sub {

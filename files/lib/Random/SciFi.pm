59d2d58f9589ef460a2eee7b76c32c12ab15d6cb
LadyAleena <LadyAleena@users.noreply.github.com>
2017-05-29 23:11:34 -0400

    Update SciFi.pm

--- files/lib/Random/SciFi.pm
+++ files/lib/Random/SciFi.pm
@@ -4,12 +4,11 @@ use warnings FATAL => qw(all);
 use Exporter qw(import);
 our @EXPORT_OK = qw(random_HHGTTG_sector random_MIB_agent);
 
-# sub-part of the 'random' suite from RolePlaying::Random
-use RolePlaying::Random::Misc qw(random_alpha random_Greek_letter);
+use Random::Alpha qw(random_alpha);
 
 sub random_letters {
   my ($number) = @_;
-  my $letter = sub { random_alpha };
+  my $letter = sub { random_alpha('upper') };
   my $letters;
   $letters .= &$letter for (1..$number);
   return $letters;
@@ -17,14 +16,14 @@ sub random_letters {
 
 sub random_HHGTTG_sector {
   my @types = qw(Active Heavy Light Over Plural Passive Single Under);
-  my $Greek = sub { random_Greek_letter };
+  my $Greek = sub { random_alpha('Greek') };
   my $type  = sub { $types[rand @types] };
   return random_letters(2)." ".$type." ".random_letter." ".$Greek;
 }
 
 sub random_MIB_agent {
-  my @letters = (1,2);
-  my $letter  = sub { random_alpha };
+  my @letters = (1, 2);
+  my $letter  = sub { random_alpha('upper') };
   return random_letters($letter);
 }
 

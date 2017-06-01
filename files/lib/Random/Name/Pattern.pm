12c34be36473767606f8770e2d23e3c891b62810
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-01 04:36:51 -0400

    Update and rename files/lib/RolePlaying/Random/Name/Pattern.pm to files/lib/Random/Name/Pattern.pm

--- files/lib/Random/Name/Pattern.pm
+++ files/lib/Random/Name/Pattern.pm
@@ -1,75 +1,8 @@
-package RolePlaying::Random::Name::Pattern;
+package Random::Name::Pattern;
 use strict;
 use warnings;
 use Exporter qw(import);
 our @EXPORT_OK = qw(random_name);
-
-=head1 Pattern
-
-This module generates random names by a specified pattern, based on Random Name by Jason Seeley.
-
-=head2 Use
-
-To use this module, please enter the following.
-
-	use Games::Random::Name::Pattern qw(random_name);
-
-When you want to generate a random name with this module, see the table below to build your pattern.
-
-=head3 Patterns
-
-  L   any single letter
-  Z   any single letter or a combination of letters
-  c   any single consonant
-  v   any single vowel
-  C   any double consonant bb dd gg kk ll mm nn pp rr ss tt zz
-  V   any double vowel     aa ee oo uu
-  B   any combination of consonants
-  A   any combination of vowels
-  D   any single or combination of consonants
-  E   any single or combination of vowels
-  e   a or e # not yet implemented
-  u   o or u # not yet implemented
-
-  a   ea ia oa ua
-  b   mb rb
-  h   ch gh ph sh th wh
-  i   ai ei oi ui
-  k   ck lk nk sk wk
-  l   bl cl fl gl pl sl
-  n   gn kn mn sn
-  p   mp rp sp
-  r   br cr dr fr gr mr pr sr tr wr
-  t   nt rt st
-  w   dw gw sw tw
-  y   ay ey oy uy
-  x   ng qu rth
-  o   ae ao au eo eu ie io ou ue
-  
-  #   any single digit
-  ^   capitalize next character
-
-The simplest sample pattern is 
-
-  random_name(cv);  #returns a random name with a single consonent and a single vowel.
-
-  random_name(vc);  #returns the opposite by creating a name starting with a single vowel followed by a single consonent.
-
-=head3 Repetition
-
-If you want to repeat the next character in the pattern, put the amount of repeats directly in front of the character.
-
-  random_name('L2v') #will return any letter first followed by any two single vowels.
-  
-If you want to repeat a group in the pattern, put the amount of repeats directly in front of the character, however you must surround the repeat in parentheses.
-
-  random_name('L2(vc)') #will return any letter first follow by two repeated syllables each comprised of a single vowel and a single consonent.
-
-=head1 AUTHOR
-
-Lady Aleena with help of simcop2387 of #perlcafe and #buubot on freenode and from the PerlMonks and others in #perlcafe.
-
-=cut
 
 my %alpha = (
   letters => ["a" .. "z"],
@@ -89,6 +22,8 @@ my %alpha = (
   },
   vowels => {
     single => [qw(a e i o u)],
+    a_or_e => [qw(a e)],
+    o_or_u => [qw(o u)],
     combo_a => [qw(ea ia oa ua)],
     combo_i => [qw(ai ei oi ui)],
     combo_y => [qw(ay ey oy uy)],
@@ -140,16 +75,18 @@ sub random_letter {
   my $key;
   my $subkey;
 
-  $key = 'letters'    if ($letter eq 'L');
-  $key = 'all'        if ($letter eq 'Z');
-  $key = 'consonants' if ($letter =~ m!(c|b|h|k|l|n|p|r|t|w|x|C|B|D)!);
-  $key = 'vowels'     if ($letter =~ m!(v|a|i|y|o|V|A|E)!);
-  $key = 'integers'   if ($letter eq '#');
+  $key = 'letters'    if $letter eq 'L';
+  $key = 'all'        if $letter eq 'Z';
+  $key = 'consonants' if $letter =~ /(c|b|h|k|l|n|p|r|t|w|x|C|B|D)/;
+  $key = 'vowels'     if $letter =~ /(v|a|e|i|o|u|y|V|A|E)/;
+  $key = 'integers'   if $letter eq '#';
   
-  $subkey = 'single'     if ($letter =~ m!(c|v)!);
-  $subkey = 'double'     if ($letter =~ m!(C|V)!);
-  $subkey = 'all_combos' if ($letter =~ m!(B|A)!);
-  $subkey = 'all'        if ($letter =~ m!(D|E)!);
+  $subkey = 'single'     if $letter =~ /(c|v)/;
+  $subkey = 'double'     if $letter =~ /(C|V)/;
+  $subkey = 'all_combos' if $letter =~ /(B|A)/;
+  $subkey = 'all'        if $letter =~ /(D|E)/;
+  $subkey = 'a_or_e'     if $letter =~ /e/;
+  $subkey = 'o_or_u'     if $letter =~ /u/;
   
   $subkey = "combo_$letter" if $letter =~ m/[bhklnprtwaiyo]/;
   $subkey = 'combo_other'   if ($letter eq "x");
@@ -178,5 +115,67 @@ sub random_name {
   $result =~ s!\^(.)!\u$1!g;
   return $result;
 }
+
+=head1 NAME
+
+B<Random::Name::Pattern> generates random names by a specified pattern, based on Random Name by Jason Seeley.
+
+=head1 SYNOPSIS
+
+	use Random::Name::Pattern qw(random_name);
+
+  random_name('cv');  # returns a random name with a single consonant and a single vowel.
+  random_name('vc');  # returns a random name with a single vowel and a single consonant.
+
+=head1 DESCRIPTION
+
+To generate a random name with this module, see the table below to build your pattern.
+
+  L   any single letter
+  Z   any single letter or a combination of letters
+  c   any single consonant
+  v   any single vowel
+  C   any double consonant bb dd gg kk ll mm nn pp rr ss tt zz
+  V   any double vowel     aa ee oo uu
+  B   any combination of consonants
+  A   any combination of vowels
+  D   any single or combination of consonants
+  E   any single or combination of vowels
+  e   a or e
+  u   o or u
+
+  a   ea ia oa ua
+  b   mb rb
+  h   ch gh ph sh th wh
+  i   ai ei oi ui
+  k   ck lk nk sk wk
+  l   bl cl fl gl pl sl
+  n   gn kn mn sn
+  p   mp rp sp
+  r   br cr dr fr gr mr pr sr tr wr
+  t   nt rt st
+  w   dw gw sw tw
+  y   ay ey oy uy
+  x   ng qu rth
+  o   ae ao au eo eu ie io ou ue
+  
+  #   any single digit
+  ^   capitalize next character
+
+=head2 Repetition
+
+If you want to repeat the next character in the pattern, put the amount of repeats directly in front of the character.
+
+  random_name('L2v') # will return any letter followed by any two single vowels.
+  
+If you want to repeat a group in the pattern, put the amount of repeats directly in front of the character, however you must surround the repeat in parentheses.
+
+  random_name('L2(vc)') # will return any letter followed by two repeated syllables each comprised of a single vowel and a single consonant.
+
+=head1 AUTHOR
+
+Lady Aleena with help of simcop2387 of #perlcafe and #buubot on freenode and from the PerlMonks and others in #perlcafe.
+
+=cut
 
 1;

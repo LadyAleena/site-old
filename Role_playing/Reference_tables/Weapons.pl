985aa3483f17a40aa94afc8d52bb80185d124340
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-05 14:41:55 -0400

    Update Weapons.pl

--- Role_playing/Reference_tables/Weapons.pl
+++ Role_playing/Reference_tables/Weapons.pl
@@ -7,10 +7,11 @@ use CGI::Carp qw(fatalsToBrowser);
 use HTML::Entities qw(encode_entities);
 
 use lib '../../files/lib';
-use Base::Data qw(get_hash);
 use Base::Page qw(page);
+use Base::Data qw(make_hash);
 use HTML::Elements qw(section table);
 use HTML::Forms qw(tiny_select);
+use RolePlaying::WeaponName qw(display_weapon);
 
 my $cgi = CGI::Minimal->new;
 my $broad = $cgi->param('broad') ? encode_entities($cgi->param('broad'),'<>"') : '';
@@ -18,15 +19,16 @@ my $tight = $cgi->param('tight') ? encode_entities($cgi->param('tight'),'<>"') :
 
 my @headings = ('Weapon','#AT','Dmg(S/M)','Dmg(L)','Range','Weight','Size','Type','Speed','KO');
 
-my $weapons_list = get_hash( 
+my $weapons_list = make_hash( 
   'file' => ['Role_playing/Reference_tables','Weapons.txt'],
-  'headings' => [@headings,'broad group','tight group','value']
+  'headings' => [@headings, 'broad group', 'tight group', 'value']
 );
 
 my @rows;
 for my $weapon (sort { $a->{'Weapon'} cmp $b->{'Weapon'} } values %$weapons_list) {
   next if $broad && (!$weapon->{'broad group'} || $weapon->{'broad group'} ne $broad);
   next if $tight && (!$weapon->{'tight group'} || $weapon->{'tight group'} ne $tight);
+  $weapon->{'Weapon'} = display_weapon($weapon->{'Weapon'});
   push @rows, [map($weapon->{$_} ? $weapon->{$_} : '', @headings)];
 }
 

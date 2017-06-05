8cb03ae68a4787f1b99e1f73a3142a083721b9e9
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-05 15:04:51 -0400

    Update Thing.pm

--- files/lib/Random/Thing.pm
+++ files/lib/Random/Thing.pm
@@ -2,14 +2,15 @@ package Random::Thing;
 use strict;
 use warnings FATAL => qw(all);
 use Exporter qw(import);
-our @EXPORT_OK = qw(random_thing random_animals random_musical_instruments random_plants random_utensils);
-
-use RolePlaying::Random qw(random);
-use RolePlaying::Random::MagicItem qw(random_magic_item);
-use RolePlaying::Random::Monster qw(random_monster);
-use RolePlaying::Random::Weapon qw(random_weapons);
-
 use Lingua::EN::Inflect qw(PL_N);
+
+use Fancy::Rand qw(fancy_rand);
+use Random::RPG::MagicItem qw(random_magic_items);
+use Random::RPG::Monster   qw(random_monster);
+use Random::RPG::Weapon    qw(random_weapons);
+
+our @EXPORT_OK = qw(random_things random_animals random_armor random_musical_instruments random_plants random_utensils
+                    random_magic_items random_monster random_weapons);
 
 my %things = (
   'animals'             => [qw(animals amphibians arthropods birds fish mammals reptiles insects spiders)],
@@ -17,20 +18,32 @@ my %things = (
   'armor'               => [qw(armor helmets shields)],
   'musical instruments' => ['musical instruments', map("$_ instruments", qw(brass percussion string woodwind))],
   'utensils'            => ['utensils', map("$_ utensils", qw(cooking eating writing))],
-  'magic items'         => ['magic items', 'magical '.random_magic_item],
-  'monsters'            => ['monsters', PL_N(random_monster,2)],
-  'weapons'             => ['weapons', random_weapons],
+  'magic items'         => ['magical '.random_magic_items('all', ['items'])],
+  'monsters'            => ['monsters', PL_N(random_monster, 2)],
+  'weapons'             => [random_weapons('all', ['weapons'])],
   'other'               => ['inanimate objects', 'religious symbols', qw(bedding boats books clothing dishes furniture jewelry rocks rugs tools wagons)],
 );
 
-sub random_thing {
-  my $thing = random(\%things, @_);
+sub random_things {
+  my ($user_thing, $user_additions) = @_;
+  my $thing = fancy_rand(\%things, $user_thing, { caller => 'random_thing', additions => $user_additions ? $user_additions : undef });
   return $thing;
 }
 
-sub random_animals             { random_thing('animals'            , { 'caller' => (caller(0))[3] }) }
-sub random_musical_instruments { random_thing('musical instruments', { 'caller' => (caller(0))[3] }) }
-sub random_plants              { random_thing('plants'             , { 'caller' => (caller(0))[3] }) }
-sub random_utensils            { random_thing('utensils'           , { 'caller' => (caller(0))[3] }) }
+sub random_animals             { my $user_addition = shift; random_things('animals' , $user_addition) }
+sub random_armor               { my $user_addition = shift; random_things('armor'   , $user_addition) }
+sub random_plants              { my $user_addition = shift; random_things('plants'  , $user_addition) }
+sub random_utensils            { my $user_addition = shift; random_things('utensils', $user_addition) }
+sub random_musical_instruments { my $user_addition = shift; random_things('musical instruments', $user_addition) }
+
+=head1 NAME
+
+B<Random::Thing> selects random things.
+
+=head1 AUTHOR
+
+Lady Aleena
+
+=cut
 
 1;

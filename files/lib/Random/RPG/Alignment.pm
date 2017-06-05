c1c991970a0f85b39b5eb5a5c2d171c3d16ebb05
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-05 14:52:40 -0400

    Update and rename files/lib/RolePlaying/Random/Alignment.pm to files/lib/Random/RPG/Alignment.pm

--- files/lib/Random/RPG/Alignment.pm
+++ files/lib/Random/RPG/Alignment.pm
@@ -1,8 +1,10 @@
-package RolePlaying::Random::Alignment;
+package Random::RPG::Alignment;
 use strict;
 use warnings FATAL => ( 'all' );
 use Exporter qw(import);
 our @EXPORT_OK = qw(random_alignment);
+
+use Fancy::Rand qw(fancy_rand tiny_rand);
 
 my %alignments = (
   'parts'             => [qw(lawful chaotic good evil neutral)],
@@ -17,11 +19,23 @@ $alignments{'lawful'}  = [map( "lawful $_",  @{$alignments{'good vs evil'}})];
 $alignments{'neutral lvc'} = [map( "$_ neutral", grep {$_ ne "neutral"} @{$alignments{'lawful vs chaotic'}} ), "true neutral"];
 $alignments{'neutral gve'} = [map( "neutral $_", grep {$_ ne "neutral"} @{$alignments{'good vs evil'}} ),      "true neutral"];
 
+sub random_alignment {
+  my ($user_alignment, $user_additions) = @_;
+  my $alignment;
+  if ($user_alignment && $user_alignment eq 'full') {
+    $alignment = full();
+  }
+  else{
+    $alignment = fancy_rand(\%alignments, $user_alignment, { caller => 'random_alignment', additions => $user_additions ? $user_additions : undef });
+  }
+  return $alignment;
+}
+
 sub full {
   my @prefixes = @{$alignments{'lawful vs chaotic'}};
   my @suffixes = @{$alignments{'good vs evil'}};
-  my $prefix = $prefixes[rand @prefixes];
-  my $suffix = $suffixes[rand @suffixes];
+  my $prefix = tiny_rand(@prefixes);
+  my $suffix = tiny_rand(@suffixes);
 
   my $alignment;
   if ($prefix eq $suffix) {
@@ -34,33 +48,19 @@ sub full {
   return $alignment;
 }
 
-sub random_alignment {
-  my $type = shift;
-  my $axis = $type ? $type : 'any';
-
-  my $alignment;
-  if (exists $alignments{$axis}) {
-    $alignment = $alignments{$axis}[rand @{$alignments{$axis}}];
-  }
-  else {
-    $alignment = full;
-  }
-
-  return $alignment;
-}
-
 =head1 NAME
 
-B<RolePlaying::Random::Alignment> generates random and expands alignments for AD&D 2nd Edition.
+B<Random::RPG::Alignment> selects random alignments for AD&D 2nd Edition from I<Advanced Dungeons & Dragons, Second Edition>.
 
 =head1 SYNOPSIS
 
 To use this module to return a random alignment, use the following.
 
-	use RolePlaying::Random::Alignment qw(random_alignment);
+	use Random::RPG::Alignment qw(random_alignment);
 
 When you want to generate a random alignment with this module, you can choose from any of the alignment axes.
 
+	random_alignment('all');                # returns any two part alignment and is the default setting.
 	random_alignment('parts');              # returns lawful, chaotic, good, evil, or neutral.
 	random_alignment('good vs evil');       # returns good, neutral, or evil.
 	random_alignment('lawful vs chaotic');  # returns lawful, neutral, or chaotic.
@@ -70,9 +70,8 @@ When you want to generate a random alignment with this module, you can choose fr
 	random_alignment('lawful');             # returns lawful good, lawful neutral, or lawful evil.
 	random_alignment('neutral lvc');        # returns lawful neutral, true neutral, or chaotic neutral.
 	random_alignment('neutral gve');        # returns neutral good, true neutral, or neutral evil.
-	random_alignment('any');                # returns any two part alignment and is the default setting.
 
-The default random result is C<random_alignment('any')>.
+The default random result is C<random_alignment('all')>.
 
 =head1 AUTHOR
 

70ae02adedd99a98a285cbfcb4998a2324562b1a
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-06 02:33:57 -0400

    Update Fictional_family_trees.pl

--- Fandom/Fictional_family_trees.pl
+++ Fandom/Fictional_family_trees.pl
@@ -7,13 +7,13 @@ use IO::All;
 
 use lib '../files/lib';
 use Base::Page qw(page story);
-use Base::Data qw(file_list data_directory data_file);
 use HTML::Elements qw(pre);
 use Util::Convert qw(textify);
+use Util::Data qw(file_list file_directory data_file);
 use Util::FamilyTree qw(make_tree);
 
 my $directory = 'Fandom/Fictional_family_trees';
-my @files = file_list(data_directory($directory));
+my @files = file_list(file_directory($directory));
 
 my $doc_magic;
 for my $file (@files) {
@@ -29,8 +29,11 @@ page( 'code' => sub { story(*DATA, { 'doc magic' => $doc_magic }) });
 __DATA__
 These are miscellaneous family trees I have collected over time.
 2 Journey to the Center of the Earth
+The Anderson family created by the I<A<Journey to the Center of the Earth|href="../Movies/Movies_by_series.pl?series=Journey">> (2008) series
 & Journey
 2 The Mummy
+The Carnahan/O'Connell family created by I<A<The Mummy|href="../Movies_by_series.pl?series=The+Mummy+(1999)">> (1999) series
 & Mummy
 2 Buffy the Vampire Slayer vampires
+The family of vampires created in the I<A<Buffy the Vampire Slayer|href="../Movies_by_series.pl?series=Buffy+the+Vampire+Slayer">> series
 & Buffy vampires

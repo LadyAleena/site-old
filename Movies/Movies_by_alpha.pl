581b6738db89ccbadd899a3c9a2643543a838c1f
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-06 02:43:49 -0400

    Update Movies_by_alpha.pl

--- Movies/Movies_by_alpha.pl
+++ Movies/Movies_by_alpha.pl
@@ -9,12 +9,12 @@ use HTML::Entities qw(encode_entities);
 use lib '../files/lib';
 use Base::Page qw(page);
 use Base::Menu qw(alpha_menu);
-use Base::Data qw(alpha_array);
+use Fancy::Join::Grammatical qw(grammatical_join);
 use HTML::Elements qw(section nav paragraph list form fieldset selection input anchor);
-use Util::Sort qw(article_sort);
+use Util::Movie qw(movie movie_option display_movie display_option start_year end_year);
 use Util::Convert qw(idify);
-use Fancy::Join::Grammatical;
-use Movie qw(movie movie_option display_movie display_option start_year end_year);
+use Util::Data qw(alpha_hash alpha_array first_alpha);
+use Util::Sort qw(article_sort);
 
 my $cgi = CGI::Minimal->new;
 my $title  = $cgi->param('title')  ? encode_entities($cgi->param('title'),  '<>"') : '';
@@ -30,7 +30,7 @@ my %select_options = map { $_ => movie_option($_) } @selects;
 my $movies_data = movie('data');
 my $search;
 my @movies;
-if ( $year || $media || $genre || $source || $title ) {
+if ( $title || $year || $media || $genre || $source || $alpha ) {
   my @selections;
   push @selections, $title  if $title;
   push @selections, $year   if $year;
@@ -63,6 +63,7 @@ if ( $year || $media || $genre || $source || $title ) {
     next if $genre  &&   !$movie->{'genre'}{$genre};
     next if $source &&  (!$movie->{'source'} || $movie->{'source'} !~ /$source/i);
     next if $title  && lc($movie->{'title'}) !~ /\L$title\E/i;
+    next if $alpha  && first_alpha($movie->{'title'}) ne $alpha;
 
     push @movies, $movie->{'title'};
   }
@@ -73,14 +74,17 @@ else {
   @movies = keys %$movies_data;
 }
 
-my %alpha_movies = alpha_array(\@movies);
-for my $movies (keys %alpha_movies) {
-  my @movies = map(display_movie($movies_data->{$_}, { 'series' => 1, 'links' => 1, 'crossover' => 1 }), sort { article_sort(lc $a, lc $b) } @{$alpha_movies{$movies}});
-  @{$alpha_movies{$movies}} = @movies;
+my $alpha_movies = alpha_array(\@movies);
+for my $movies (keys %$alpha_movies) {
+  my @movies = map(display_movie($movies_data->{$_}, { 'series' => 1, 'links' => 1, 'crossover' => 1 }), sort { article_sort(lc $a, lc $b) } @{$alpha_movies->{$movies}});
+  @{$alpha_movies->{$movies}} = @movies;
 }
 my $alpha_menu = scalar(@movies) > 10 ? 
-                  alpha_menu(\%alpha_movies, { 'addition' => anchor('Search the list', { href => '#Search' }), 'join' => ' | ' } ) :
+                  alpha_menu($alpha_movies, { 'addition' => anchor('Search the list', { href => '#Search' }), 'join' => ' | ' } ) :
                   undef;
+
+my $browse_alpha = alpha_hash($movies_data);
+my $browse_alpha_menu = alpha_menu($browse_alpha, { 'param' => 'alpha', 'join' => ' | '  });
 
 page( 'code' => sub {
   form(3, sub {
@@ -93,16 +97,23 @@ page( 'code' => sub {
       }
       input(5, { 'type' => 'submit', 'value' => 'List movies' });
       input(5, { 'type' => 'button', 'value' => 'Start over', 'onclick' => "location='Movies_by_alpha.pl'" });
+      nav(5, "Browse: $browse_alpha_menu", { 'class' => 'alpha_menu' });
     }, { 'legend' => 'Search...' });
   }, { 'action' => 'Movies_by_alpha.pl', 'method' => 'get', 'id' => 'Search' });
   section(3, sub {
-    paragraph(4, 'This is a list of films and television series I have collected that either interest my fiancé or me or helps populate other pages.');
-    paragraph(4, $search) if $search;
+    if ( $title || $year || $media || $genre || $source ) {
+      paragraph(4, $search) if $search;
+    }
+    else {
+      paragraph(4, 'This is a list of films and television series I have collected that either interest my fiancé or me or helps populate other pages. You can browse the list or search it.');
+    }
   });
-  for my $alpha_movie (sort keys %alpha_movies) {
-    section(3, sub {
-      list(5, 'u', $alpha_movies{$alpha_movie}, { 'class' => 'movie_list' });
-      nav(5, $alpha_menu, { 'class' => 'alpha_menu' }) if $alpha_menu;
-    }, { 'heading' => [2, $alpha_movie, { id => "section_$alpha" }] });
-  };
+  if ( $search || $alpha ) {
+    for my $alpha_movie (sort keys %$alpha_movies) {
+      section(3, sub {
+        list(5, 'u', $alpha_movies->{$alpha_movie}, { 'class' => 'movie_list' });
+        nav(5, $alpha_menu, { 'class' => 'alpha_menu' }) if $alpha_menu;
+      }, { 'heading' => [2, $alpha_movie, { id => "section_$alpha_movie" }] });
+    };
+  }
 });

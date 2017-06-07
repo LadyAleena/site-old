a70c9e6f790fc89b75c967dd867bf0ec1d943d07
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-06 23:41:08 -0400

    Update Alpha.pm

--- files/lib/Random/Alpha.pm
+++ files/lib/Random/Alpha.pm
@@ -4,7 +4,7 @@ use warnings FATAL => qw(all);
 use Exporter qw(import);
 our @EXPORT_OK = qw(random_alpha);
 
-use RolePlaying::Random qw(random);
+use Fancy::Rand qw(fancy_rand);
 
 my @vowels = (0,4,8,14,20);
 
@@ -21,28 +21,28 @@ $alphabet{'crosses and dots'} = ['f', 'i', 'j', 't'];
 $alphabet{'Greek'} = [qw(Alpha Beta Gamma Delta Epsilon Digamma Zeta Eta Theta Iota Kappa Lambda Mu Nu Xi Omicron Pi San Qoppa Rho Sigma Tau Upsilon Phi Chi Psi Omega Sampi)];
 
 sub random_alpha {
-  my ($user_alpha) = @_;
-  my $random_alpha = random(\%alphabet, $user_alpha);
+  my ($user_alpha, $user_additions) = @_;
+  my $random_alpha = fancy_rand(\%alphabet, $user_alpha, { caller => 'random_alpha', additions => $user_additions ? $user_additions : undef });
   return $random_alpha;
 }
 
 =head1 NAME
 
-B<Random::Alpha> returns random letters of the English alphabet.
+B<Random::Alpha> selects random letters of the English alphabet.
 
 =head1 SYNOPSIS
 
   use Random::Alpha qw(random_alpha);
   
-  my $upper_case   = random_alpha('upper');            # returns an upper case letter
-  my $lower_case   = random_alpha('lower');            # returns a lower case letter
-  my $upper_vowel  = random_alpha('upper vowels');     # returns an A, E, I, O, or U
-  my $lower_vowel  = random_alpha('lower vowels');     # returns an a, e, i, o, or u
-  my $ascender     = random_alpha('ascenders');        # returns a b, d, f, h, k, l, or t
-  my $descender    = random_alpha('descenders');       # returns a g, j, p, q, or y
-  my $cross_or_dot = random_alpha('crosses and dots'); # returns an f, i, j, or t
+  my $upper_case   = random_alpha('upper');            # selects an upper case letter
+  my $lower_case   = random_alpha('lower');            # selects a lower case letter
+  my $upper_vowel  = random_alpha('upper vowels');     # selects an A, E, I, O, or U
+  my $lower_vowel  = random_alpha('lower vowels');     # selects an a, e, i, o, or u
+  my $ascender     = random_alpha('ascenders');        # selects a b, d, f, h, k, l, or t
+  my $descender    = random_alpha('descenders');       # selects a g, j, p, q, or y
+  my $cross_or_dot = random_alpha('crosses and dots'); # selects an f, i, j, or t
 
-  my $Greek        = random_alpha('Greek'); # returns a Greek letter spelled out
+  my $Greek        = random_alpha('Greek'); # selects a Greek letter spelled out
   
 =head1 AUTHOR
 

a8547566da2e8c6d738de45dc3009e435b4b75a6
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-06 23:44:24 -0400

    Update Misc.pm

--- files/lib/Random/Misc.pm
+++ files/lib/Random/Misc.pm
@@ -5,7 +5,7 @@ use Exporter qw(import);
 our @EXPORT_OK = qw(random_divinity random_emotion random_generation random_group random_henchmen random_language_common random_non random_parent
                     random_proficiency_type random_relationship random_sexual_orientation random_shadow random_sign random_zstuff);
 
-use RolePlaying::Random qw(random);
+use Fancy::Rand qw(fancy_rand);
 
 my %misc = (
   'emotion'            => [qw(joy sorrow trust fear love hate indifference)],
@@ -22,25 +22,34 @@ my %misc = (
 
   'divinity'           => [map("${_}power",('demi-', 'lesser ', 'intermediate ', 'greater '))],
   'henchmen'           => [qw(friends servants minions slaves mercenaries)],
-  'language common'    => [map("$_ common",qw(beholder dragon dwarven elven faerie giant gnome goblinoid halfling human planar))],
+  'language common'    => [map("$_ common", qw(beholder dragon dwarven elven faerie giant gnome goblinoid halfling human planar))],
   'proficiency type'   => ['weapon', 'non-weapon proficiency', 'language'],
 );
 
-sub random_divinity           { random(\%misc, 'divinity'          , { 'caller' => (caller(0))[3] }) }
-sub random_emotion            { random(\%misc, 'emotion'           , { 'caller' => (caller(0))[3] }) }
-sub random_game               { random(\%misc, 'game'              , { 'caller' => (caller(0))[3] }) }
-sub random_generation         { random(\%misc, 'generation'        , { 'caller' => (caller(0))[3] }) }
-sub random_group              { random(\%misc, 'group'             , { 'caller' => (caller(0))[3] }) }
-sub random_henchmen           { random(\%misc, 'henchmen'          , { 'caller' => (caller(0))[3] }) }
-sub random_language_common    { random(\%misc, 'language common'   , { 'caller' => (caller(0))[3] }) }
-sub random_non                { random(\%misc, 'non'               , { 'caller' => (caller(0))[3] }) }
-sub random_parent             { random(\%misc, 'parent'            , { 'caller' => (caller(0))[3] }) }
-sub random_proficiency_type   { random(\%misc, 'proficiency type'  , { 'caller' => (caller(0))[3] }) }
-sub random_relationship       { random(\%misc, 'relationship'      , { 'caller' => (caller(0))[3] }) }
-sub random_sexual_orientation { random(\%misc, 'sexual orientation', { 'caller' => (caller(0))[3] }) }
-sub random_shadow             { random(\%misc, 'shadow'            , { 'caller' => (caller(0))[3] }) }
-sub random_sign               { random(\%misc, 'sign'              , { 'caller' => (caller(0))[3] }) }
-sub random_zstuff             { random(\%misc, 'zstuff'            , { 'caller' => (caller(0))[3] }) }
+sub random_divinity           { fancy_rand(\%misc, 'divinity'          , { 'caller' => (caller(0))[3] }) }
+sub random_emotion            { fancy_rand(\%misc, 'emotion'           , { 'caller' => (caller(0))[3] }) }
+sub random_game               { fancy_rand(\%misc, 'game'              , { 'caller' => (caller(0))[3] }) }
+sub random_generation         { fancy_rand(\%misc, 'generation'        , { 'caller' => (caller(0))[3] }) }
+sub random_group              { fancy_rand(\%misc, 'group'             , { 'caller' => (caller(0))[3] }) }
+sub random_henchmen           { fancy_rand(\%misc, 'henchmen'          , { 'caller' => (caller(0))[3] }) }
+sub random_language_common    { fancy_rand(\%misc, 'language common'   , { 'caller' => (caller(0))[3] }) }
+sub random_non                { fancy_rand(\%misc, 'non'               , { 'caller' => (caller(0))[3] }) }
+sub random_parent             { fancy_rand(\%misc, 'parent'            , { 'caller' => (caller(0))[3] }) }
+sub random_proficiency_type   { fancy_rand(\%misc, 'proficiency type'  , { 'caller' => (caller(0))[3] }) }
+sub random_relationship       { fancy_rand(\%misc, 'relationship'      , { 'caller' => (caller(0))[3] }) }
+sub random_sexual_orientation { fancy_rand(\%misc, 'sexual orientation', { 'caller' => (caller(0))[3] }) }
+sub random_shadow             { fancy_rand(\%misc, 'shadow'            , { 'caller' => (caller(0))[3] }) }
+sub random_sign               { fancy_rand(\%misc, 'sign'              , { 'caller' => (caller(0))[3] }) }
+sub random_zstuff             { fancy_rand(\%misc, 'zstuff'            , { 'caller' => (caller(0))[3] }) }
 
+=head1 NAME
+
+B<Random::Misc> selects random miscellaneous things.
+
+=head1 AUTHOR
+
+Lady Aleena
+
+=cut
 
 1;

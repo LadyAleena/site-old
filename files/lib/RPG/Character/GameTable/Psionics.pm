6da58dc7f5ebd3feed95e82d4035650701bbde4b
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-07 18:27:31 -0400

    Update and rename files/lib/RolePlaying/Character/GameTable/Psionics.pm to files/lib/RPG/Character/GameTable/Psionics.pm

--- files/lib/RPG/Character/GameTable/Psionics.pm
+++ files/lib/RPG/Character/GameTable/Psionics.pm
@@ -1,11 +1,11 @@
-package RolePlaying::Character::GameTable::Psionics;
+package RPG::Character::GameTable::Psionics;
 use strict;
 use warnings FATAL => ( 'all' );
 use Exporter qw(import);
 our @EXPORT_OK = qw(psionics_by_level psionics_table_rows);
 
 use Util::Data qw(make_hash);
-use RolePlaying::Character::Class qw(class_level);
+use RPG::Character::Class qw(class_level);
 
 # part of the Character Building table suite.
 

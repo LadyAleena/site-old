31646235c2ee45daa156bbb3a05d4d452fe299e0
LadyAleena <LadyAleena@users.noreply.github.com>
2017-06-06 23:47:47 -0400

    Update Time.pm

--- files/lib/Random/Time.pm
+++ files/lib/Random/Time.pm
@@ -4,11 +4,10 @@ use warnings FATAL => qw(all);
 use Exporter qw(import);
 our @EXPORT_OK = qw(random_time_unit random_day_part random_time random_frequency);
 
-# part of the 'random' suite from RolePlaying::Random
-use RolePlaying::Random qw(random);
-use RolePlaying::Random::SpecialDice qw(random_die);
-
 use Lingua::EN::Inflect qw(PL_N A);
+
+use Fancy::Rand qw(fancy_rand tiny_rand);
+use Random::SpecialDice qw(random_die);
 
 my %times = (
   'general' => [qw(second minute hour day week month year decade century millennium)],
@@ -20,24 +19,25 @@ $time_units{'second'}     = 1;
 $time_units{'minute'}     = $time_units{'second'}  * 60;
 $time_units{'hour'}       = $time_units{'minute'}  * 60;
 $time_units{'day'}        = $time_units{'hour'}    * 24;
-$time_units{'week'}       = $time_units{'day'}     * 7;
-$time_units{'month'}      = $time_units{'week'}    * 4;
+$time_units{'week'}       = $time_units{'day'}     *  7;
+$time_units{'month'}      = $time_units{'week'}    *  4;
 $time_units{'year'}       = $time_units{'week'}    * 52;
 $time_units{'decade'}     = $time_units{'year'}    * 10;
 $time_units{'century'}    = $time_units{'decade'}  * 10;
 $time_units{'millennium'} = $time_units{'century'} * 10;
 $time_units{'round'}      = $time_units{'minute'};
-$time_units{'segment'}    = $time_units{'round'} / 10;
-$time_units{'turn'}       = $time_units{'round'} * 10;
+$time_units{'segment'}    = $time_units{'round'}   / 10;
+$time_units{'turn'}       = $time_units{'round'}   * 10;
 
 sub random_time_unit {
-  my $time = random(\%times, @_);
+  my ($user_time, $user_additions) = @_;
+  my $time = fancy_rand(\%times, $user_time, { caller => 'random_time_unit', additions => $user_additions ? $user_additions : undef });
   return $time;
 }
 
 sub random_day_part {
   my @day_part = qw(dawn morning midmorning noon midday afternoon evening dusk night midnight);
-  my $day_part = $day_part[rand @day_part];
+  my $day_part = tiny_rand(@day_part);
   if ($day_part =~ /^(morning|afternoon|evening)$/) {
     return "in the $day_part";
   }
@@ -67,23 +67,23 @@ sub random_frequency {
 
 =head1 NAME
 
-B<Random::Time> returns a random time unit, random day part, random time, or random frequency.
+B<Random::Time> selects a random time unit, random day part, random time, or random frequency.
 
 =head1 SYNOPSIS
 
   use Random::Time qw(random_time_unit random_day_part random_time random_frequency);
-  
-  my $time_unit          = random_time_unit;            # returns any time unit
-  my $standard_time_unit = random_time_unit('general'); # returns a general time unit from second to millennium
-  my $game_time_unit     = random_time_init('game');    # returns a game time unit from segment to turn
-  
-  my $day_part = random_day_part; # returns part of the day such as dawn, noon, or dusk and more.
-  
-  my $time          = random_time;            # returns a random time unit, if the time unit is greater than a day, then adds a day part to it.
-  my $standard_time = random_time('general'); # returns a general random time unit
-  my $game_time     = random_time('game');    # returns a game random time unit
-  
-  my $frequency = random_frequency; # returns a frequency by time unit
+
+  my $time_unit          = random_time_unit;            # selects any time unit
+  my $standard_time_unit = random_time_unit('general'); # selects a general time unit from second to millennium
+  my $game_time_unit     = random_time_init('game');    # selects a game time unit from segment to turn
+
+  my $day_part = random_day_part; # selects part of the day such as dawn, noon, or dusk and more.
+
+  my $time          = random_time;            # selects a random time unit, if the time unit is greater than a day, then adds a day part to it.
+  my $standard_time = random_time('general'); # selects a general random time unit
+  my $game_time     = random_time('game');    # selects a game random time unit
+
+  my $frequency = random_frequency; # selects a frequency by time unit
 
 =head1 AUTHOR
 

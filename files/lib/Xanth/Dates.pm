8bef81b891c0380dfda6d3688e10e0cbfc51726b
LadyAleena <LadyAleena@users.noreply.github.com>
2019-05-08 17:27:05 -0400

    Update Dates.pm

--- files/lib/Xanth/Dates.pm
+++ files/lib/Xanth/Dates.pm
@@ -2,13 +2,16 @@ package Xanth::Dates;
 use strict;
 use warnings;
 use Exporter qw(import);
-our @EXPORT_OK = qw(get_birth get_death get_dates);
+our @EXPORT_OK = qw(get_birth get_death get_dates get_age);
 
 use Fancy::Join qw(join_defined grammatical_join);
+use Xanth::Novel     qw(current_year);
 use Xanth::PageLinks qw(timeline_link);
 use Xanth::Util      qw(character_link);
 
 # Begin getting dates
+
+my $current_year = current_year();
 
 # Begin getting birth date and method
 # People in Xanth are delivered by storks, except centaurs and derivatives thereof, because live births are "painful and messy".
@@ -46,12 +49,39 @@ sub get_dates {
 
   my $birth_text = $dates->{birth} ? get_birth($dates->{birth}, $species, $places) : undef;
   my $death_text = $dates->{death} ? get_death($dates) : undef;
-  my $dates_text = $dates->{birth} || $dates->{death} ? grammatical_join('and', ($birth_text, $death_text)): undef;
+  my $dates_text = $dates->{birth} || $dates->{death} ? join_defined(' and ', ($birth_text, $death_text)): undef;
 
   return $dates_text;
 }
 
 # End getting dates together
 # End getting dates
+# Start getting age
+
+sub get_age {
+  my $dates = shift;
+  my $age;
+  my $current_year = current_year();
+  my $birth = $dates->{birth};
+  my $last  = $dates->{death} ? $dates->{death} : $current_year;
+  
+  $age = $last - $birth;
+  
+  if ($dates->{suspension}) {
+    my $suspension = $dates->{suspension};
+    my $end = $suspension->{end} ? $suspension->{end} : $current_year;
+    my $suspension_modifier = $end - $suspension->{begin};
+    $age = $age - $suspension_modifier;
+  }
+  
+  if ($dates->{reage}) {
+    my $reage = $dates->{reage};
+    $age = $reage->{age}  + ($last - $reage->{year});
+  }
+  
+  return $age;
+}
+
+# End getting age
 
 1;

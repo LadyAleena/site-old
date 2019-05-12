122c6f8da689887e5c8865095a67a378d44e1942
LadyAleena <LadyAleena@users.noreply.github.com>
2019-05-12 18:02:20 -0400

    Update Dates.pm

--- files/lib/Xanth/Dates.pm
+++ files/lib/Xanth/Dates.pm
@@ -2,14 +2,16 @@ package Xanth::Dates;
 use strict;
 use warnings;
 use Exporter qw(import);
-our @EXPORT_OK = qw(get_birth get_death get_dates get_age);
+our @EXPORT_OK = qw(get_age get_age_suspension get_dates_family);
 
+use Util::Convert  qw(textify idify searchify);
 use Fancy::Join qw(join_defined grammatical_join);
+use Xanth::Family    qw(get_family);
 use Xanth::Novel     qw(current_year);
 use Xanth::PageLinks qw(timeline_link);
 use Xanth::Util      qw(character_link);
 
-# Begin getting dates
+# Begin getting dates and age
 
 my $current_year = current_year();
 
@@ -55,7 +57,6 @@ sub get_dates {
 }
 
 # End getting dates together
-# End getting dates
 # Start getting age
 
 sub get_age {
@@ -68,10 +69,12 @@ sub get_age {
   $age = $last - $birth;
   
   if ($dates->{suspension}) {
-    my $suspension = $dates->{suspension};
-    my $end = $suspension->{end} ? $suspension->{end} : $current_year;
-    my $suspension_modifier = $end - $suspension->{begin};
-    $age = $age - $suspension_modifier;
+    my @suspensions = @{$dates->{suspension}};
+    for my $suspension (@suspensions) {
+      my $end = $suspension->{end} ? $suspension->{end} : $current_year;
+      my $suspension_modifier = $end - $suspension->{begin};
+      $age = $age - $suspension_modifier;
+    }
   }
   
   if ($dates->{reage}) {
@@ -83,5 +86,162 @@ sub get_age {
 }
 
 # End getting age
+# Start getting text for when a character's age was suspended
+
+sub get_age_suspension {
+  my ($dates, $pronoun) = @_;
+  my @suspensions = @{$dates->{suspension}};
+
+  my $birth_year  = $dates->{birth};
+
+  my @suspensions_text;
+  my $begin_age;
+  for (my $supend_no = 0; $supend_no < @suspensions; $supend_no++) {
+    my $suspension   = $suspensions[$supend_no];
+    my $prev_suspend = $supend_no - 1 >= 0 ? $suspensions[$supend_no - 1] : undef; 
+
+    my $begin_year  = $suspension->{begin};
+    my $begin_event = $suspension->{'begin event'};
+       $begin_event =~ s/Brain Coral/character_link('Brain Coral')/e;
+
+    if ($prev_suspend) {
+      my $prev_end_year = $prev_suspend->{end};
+      $begin_age += ($begin_year - $prev_end_year);
+    }
+    else {
+      $begin_age += $begin_year - $birth_year;
+    }
+
+    my $end_year  = $suspension->{end} ? $suspension->{end} : undef;
+    my $end_event = $suspension->{'end event'} ? $suspension->{'end event'} : undef;
+
+    my $begin_link = timeline_link($begin_year);
+    my $end_link   = timeline_link($end_year) if $end_year;
+    
+    my $prep = $begin_event =~ /past|future/ ? 'from' : 'in';
+    
+    my $age_text;
+    if ($birth_year) {
+      $age_text = "was $begin_age years old when $pronoun";
+    }
+    
+    my $event_text = "$begin_event $prep $begin_link";
+    if ($end_year && $end_event) {
+      $event_text .= " and began aging again when $pronoun $end_event in $end_link";
+    }
+    elsif ($end_year) {
+      if ($begin_event =~ /past|future/) {
+        $event_text .= " to $end_link";
+      }
+      else {
+        $event_text .= " and began aging again in $end_link";
+      }
+    }
+    else {
+      $event_text .= " and stopped aging";
+    }
+
+    my $suspension_text = join_defined(' ', ($age_text, $event_text));
+    push @suspensions_text, $suspension_text;
+  }
+  
+  return @suspensions_text;
+}
+
+# End getting text for when a character's age was suspended
+# Begin getting text for when a character was reaged.
+
+sub get_reage {
+  my ($dates, $pronoun) = @_;
+
+  my $birth_year  = $dates->{birth};
+
+  my $reage = $dates->{reage};
+  my $reage_age   = $reage->{age};
+  my $reage_year  = $reage->{year};
+  my $reage_event = $reage->{event};
+  my $year_link   = timeline_link($reage_year);
+
+  my $age_reage;
+  if ($dates->{suspension} && $dates->{suspension}->[-1]->{end}) {
+    $age_reage = ($reage_year - $dates->{suspension}->[-1]->{end}) + ($dates->{suspension}->[-1]->{begin} - $birth_year);
+  }
+  else {
+    $age_reage = $reage_year - $birth_year;
+  }
+
+  my $start_verb = $reage_event && $reage_event =~ /compressed/ ? 'would have been' : 'was';
+  my $start_text = "$start_verb $age_reage in $year_link" if $age_reage > 0;
+  
+  my $pre_event;
+  if (($reage_event && $reage_event =~ /takes/) && $age_reage > 0) {
+    $pre_event = "but $pronoun";
+  }
+  elsif ($reage_event && $age_reage > 0) {
+    $pre_event = "when $pronoun";
+  }
+
+  my $post_event;
+  if ($reage_event && $reage_event =~ /maintain/) {
+    $post_event = "the age of";
+  }
+  elsif ($reage_event && $reage_event =~ /switched/) {
+    $post_event = "who was";
+  }
+  elsif ($reage_event && $reage_event =~ /youth|aged/) {
+    $post_event = "and became";
+  }
+  elsif ($reage_event) {
+    $post_event = "at the age of";
+  }
+  else {
+    $post_event = "but unclear how $pronoun became"
+  }
+
+  my $reage_text = join_defined(' ', ($start_text, $pre_event, $reage_event, $post_event, $reage_age));
+
+  return $reage_text;
+}
+
+# End getting text for when a character was reaged.
+# End getting dates and age
+# Being dates and family paragraph
+
+sub get_dates_family {
+  my ($character, $gendering) = @_;
+  
+  my $name    = $character->{Name};
+  my $text    = textify($name);
+  my $dates   = $character->{dates};
+  my $family  = $character->{family};
+  my $species = $character->{species};
+  my $places  = $character->{places};
+  my $pronoun = $gendering->{pronoun};
+
+  my $dates_family = undef;
+  if ($dates || $family) {
+    my $verb = $dates && $dates->{death} ? 'was' : 'is';
+    
+    my @dates_and_family;
+    push @dates_and_family, get_dates($dates, $species, $places)          if ($dates && !$family);
+    push @dates_and_family, get_birth($dates->{birth}, $species, $places) if ($dates->{birth} && $family);
+    push @dates_and_family, "$verb ".get_family($family, $gendering)      if $family;
+    push @dates_and_family, get_age_suspension($dates, $pronoun)          if $dates->{suspension};
+    push @dates_and_family, get_reage($dates, $pronoun)                   if $dates->{reage};
+    push @dates_and_family, get_death($dates)                             if ($dates->{death} && $family);
+    push @dates_and_family, "$verb ".get_age($dates).' years old'         if $dates->{birth};
+    
+    my $first = shift @dates_and_family;
+    @dates_and_family = map { ucfirst "$pronoun $_." } @dates_and_family;
+    unshift @dates_and_family, "$first.";
+
+    $dates_family = join(' ', @dates_and_family);
+  }
+  my $full_text = $dates_family ? "$text $dates_family" : undef;
+  
+  return $full_text;
+}
+
+# End dates and family paragraph
 
 1;

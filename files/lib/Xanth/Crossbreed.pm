37b0979ff51855ea485e2dbce9ba2bd988abe214
LadyAleena <LadyAleena@users.noreply.github.com>
2019-05-12 18:01:51 -0400

    Update Crossbreed.pm

--- files/lib/Xanth/Crossbreed.pm
+++ files/lib/Xanth/Crossbreed.pm
@@ -7,19 +7,31 @@ our @EXPORT_OK = qw(get_crossbreed);
 use Fancy::Join::Defined qw(join_defined);
 
 sub get_crossbreed {
-  my ($raw_species, $mother, $father) = @_;
+  my ($character) = @_;
   
-  my $species = $raw_species;
-
-  if ($raw_species eq 'crossbreed') {
-    my $mother_species = $mother ? Xanth_species($mother) : 'unknown';
-    my $father_species = $father ? Xanth_species($father) : 'unknown';
-    my $cross_species = join_defined('/', ($mother_species, $father_species));
-    $species = "$cross_species";
+  my $species = $character->{species}->[-1];
+  
+  if ($species eq 'crossbreed') {
+    my @species_list;
+    for my $parent_type ( qw(mother father) ) {
+      my $parent_name = $character->{family}->{$parent_type}->[0] ? $character->{family}->{$parent_type}->[0] : undef;
+      
+      my $parent_species = undef;
+      if ($parent_name) {
+        if ( $parent_name =~ /^[a-z]/ && $parent_name ne 'unknown' ) {
+          $parent_species = $parent_name;
+        }
+        else {
+          my $parent = $characters->{$parent_name};
+          $parent_species = get_crossbreed($parent);
+        }
+      }
+      push @species_list, $parent_species;
+    }
+    $species = join_defined('/', @species_list);
   }
 
-  #return $species;
-  return "get_crossbreed does not work.";
+  return $species;
 }
 
 1;

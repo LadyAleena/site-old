8682235b33f4e5ac1355ccde55f40f03eb4fd7fd
LadyAleena <LadyAleena@users.noreply.github.com>
2019-05-14 11:37:40 -0400

    Update Species.pl

--- Fandom/Xanth/Species.pl
+++ Fandom/Xanth/Species.pl
@@ -6,7 +6,7 @@ use CGI::Carp qw(fatalsToBrowser);
 
 use lib '../../files/lib';
 use Base::Page qw(page story);
-use Util::Xanth::LineMagic qw(Xanth_line_magic);
+use Xanth::LineMagic qw(Xanth_line_magic);
 
 page( 'heading' => 'Species in Xanth', 'code' => sub { story(*DATA, { 'line magic' => Xanth_line_magic('page') }) });
 

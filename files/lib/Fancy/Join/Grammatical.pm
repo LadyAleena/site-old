1fc64a621d2bc0fca199fdccb9f15b1c1ce89c90
LadyAleena <LadyAleena@users.noreply.github.com>
2020-04-27 07:39:46 -0400

    Update Grammatical.pm

--- files/lib/Fancy/Join/Grammatical.pm
+++ files/lib/Fancy/Join/Grammatical.pm
@@ -3,8 +3,8 @@ use strict;
 use warnings FATAL => qw( all );
 use Exporter qw(import);
 
-our $VERSION = '1.0';
-our @EXPORT = qw(grammatical_join);
+our $VERSION   = '1.0';
+our @EXPORT_OK = qw(grammatical_join);
 
 # Written with the help of DrForr in #perlcafe on freenode.
 sub grammatical_join {
@@ -22,17 +22,19 @@ B<Fancy::Join::Grammatical> joins a list with a final conjunction.
 
 =head1 SYNOPSIS
 
+  Use Fancy::Join::Grammatical qw(grammatical_join);
+
   my @color_array = ('red', 'yellow', 'green', 'cyan', 'blue', 'magenta');
   my $colors = grammatical_join('and', @color_array);
   # red, yellow, green, cyan, blue, and magenta
 
 =head1 DESCRIPTION
 
-C<grammatical_join> is exported by default and returns a string of joined array values with commas and a comma with a conjunction between the last two values. It takes two parameters: the conjunction and the array. If any values within the array already have commas, semicolons will be used in place of commas.
+C<grammatical_join> can be exported and returns a string of joined array values with commas and a comma with a conjunction between the last two values. It takes two parameters: the conjunction and the array. If any values within the array already have commas, semicolons will be used in place of commas.
 
 =head1 SEE ALSO
 
-There is a function in L<Lingua::EN::Inflect> called C<WORDLIST> which does the same as C<grammatical_join>.
+L<Lingua::EN::Inflect> has C<WORDLIST> and L<Lingua::EN::Inflexion> has C<wordlist> which does a little more than C<grammatical_join>.
 
 =head1 AUTHOR
 

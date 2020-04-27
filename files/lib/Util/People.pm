4f93033aacaba1e83e32d82656a6b8659236bd4c
LadyAleena <LadyAleena@users.noreply.github.com>
2020-04-27 07:38:03 -0400

    Update People.pm

--- files/lib/Util/People.pm
+++ files/lib/Util/People.pm
@@ -4,6 +4,7 @@ use warnings;
 use Exporter qw(import);
 our @EXPORT_OK = qw(people_list);
 
+use HTML::Elements qw(span);
 use Util::Convert qw(filify searchify);
 use Util::Data qw(data_file);
 use Util::ExternalLinks;
@@ -17,12 +18,14 @@ sub people_list {
   if (-f $people_file) {
     open(my $fh, '<', $people_file) || die "Can not open $people_file $!";
     my @people_lines = map { chomp $_; [ split(/\|/, $_) ] } <$fh>;
-    
+
     for my $line (sort { name_sort($a->[0], $b->[0]) } @people_lines) {
       my $name = $line->[0];
          $name =~ s/^_//;
       my $article_name = $line->[1] ? $line->[1] : filify($name);
-      my $link = external_links([['Wikipedia', $article_name, $name], ['Google', searchify($name), $name]]);
+      my $links = external_links([['Wikipedia', $article_name, $name], ['Google', searchify($name), $name]]);
+      my $links_text = join(', ', @$links);
+      my $link = span($links_text, { class => 'links'});
       push @$people, "$name $link";
     }
   }

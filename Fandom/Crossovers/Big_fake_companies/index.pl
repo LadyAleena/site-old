dd2548712cd53e1490eb5c1a45e5713db6c120a2
LadyAleena <LadyAleena@users.noreply.github.com>
2020-04-27 07:48:42 -0400

    Update and rename Movies/Crossovers/Big_fake_companies/index.pl to Fandom/Crossovers/Big_fake_companies/index.pl

--- Fandom/Crossovers/Big_fake_companies/index.pl
+++ Fandom/Crossovers/Big_fake_companies/index.pl
@@ -8,7 +8,10 @@ use CGI::Carp qw(fatalsToBrowser);
 use lib '../../../files/lib';
 use Base::Page qw(page story);
 
-page( 'code' => sub { story(*DATA) });
+my $magic;
+$magic->{'series base'} = '../../../Movies/Movies_by_series.pl';
+
+page( 'code' => sub { story(*DATA, { 'line magic' => $magic }) });
 
 __DATA__
 When creating a crossover between two series with a A<big (or little) fake company|href="http://www.poobala.com/bfc.html" target="new">, it might be good to apply a reality check. The biggest hint the two companies are not the same is the name of the company. Companies are sticklers when it comes to the use of its company name. So no matter how close the names are, they are not the same company. Oceanic Airlines from I<Lost> would not truncate their company name on their tickets, meaning Oceanic Air from I<A<Fringe|href="Movies_by_series.pl?series=Fringe">> is not the same company. Also, mentioning B<a> paper factory in Slough on I<Lost> does not mean it is A<Wernham Hogg from I<The Office> (U.K.)|href="http://www.poobala.com/lostandoffice.html">.
@@ -19,7 +22,7 @@ B<A<Morley|href="Morley.pl">> and B<A<Oceanic Airlines|href="Oceanic_Airlines.pl
 B<Yoyodyne> is covered well in A<Thom Holbrooks's article|href="http://www.poobala.com/bfc.html" target="new">.
 Finder-Spyder, MacCutchoen Whisky, and Trans Global Airlines need further investigation before crossover determinations can be made.
 3 Doublemeat Palace
-B<Doublemeat Palace> is a fake fast food restaraunt appearing first on I<A<Buffy the Vampire Slayer|href="../../Movies_by_series.pl?series=Buffy+the+Vampire+Slayer">> Q<Doublemeat Palace>. It was mentioned in I<Dexter> Q<Truth Be Told> and used again in I<A<Andy Barker, P.I.|href="../../Movies_by_series.pl?series=Andy+Barker,+P.I.">> Q<Fairway, My Lovely>.
+B<Doublemeat Palace> is a fake fast food restaraunt appearing first on I<A<Buffy the Vampire Slayer|href="^series base^?series=Buffy+the+Vampire+Slayer">> Q<Doublemeat Palace>. It was mentioned in I<Dexter> Q<Truth Be Told> and used again in I<A<Andy Barker, P.I.|href="^series base^?series=Andy+Barker,+P.I.">> Q<Fairway, My Lovely>.
 3 Gannon Car Rentals
 B<Gannon Car Rentals> is a fake car rental company. The same name and logo are used throughout its appearances.
 * I<Lost> Q<Raised by Another>
@@ -27,18 +30,18 @@ B<Gannon Car Rentals> is a fake car rental company. The same name and logo are u
 * I<Nip/Tuck> Q<Conor McMamara, 2026>
 * I<Glee> Q<The New Rachel>
 3 Lariat
-B<Lariat> is several fake car rental companies which can be used when a real company can not be. The use of Lariat does not constitute a crossover because of name and logo mismatches. 
-* A<I<The X-Files>|href="../../Movies_by_series.pl?series=The+X-Files#The_X-Files_1993">
+B<Lariat> is several fake car rental companies which can be used when a real company can not be. The use of Lariat does not constitute a crossover because of name and logo mismatches.
+* A<I<The X-Files>|href="^series base^?series=The+X-Files#The_X-Files_1993">
 * I<Veronica Mars>, called Lariat Rental Car
 * I<Supernatural>, called Lariat Car Rentals
 * I<Breaking Bad>, called Lariat Rent-A-Car
 3 The Montecito Resort and Casino
-B<The Montecito Resort and Casino> is a fake hotel and casino featured in I<Las Vegas>. Other NBC series used The Montecito.
-* I<A<Las Vegas|href="../../Movies_by_series.pl?series=Las+Vegas">>
+B<The Montecito Resort and Casino> is a fake hotel and casino featured in I<A<Las Vegas|href="^series base^?series=Las+Vegas">>. Other NBC series used The Montecito.
+* I<Monk> Q<Mr. Monk Goes to Vegas>
 * I<Passions> (20 and 23 May 2005)
-* I<A<Medium|href="../../Movies_by_series.pl?series=Medium">> Q<Method to His Madness>
-* I<A<Heroes|href="../../Movies_by_series.pl?series=Heroes">> Q<Collision>
-* I<A<Knight Rider|href="../../Movies_by_series.pl?series=Knight+Rider#Knight_Rider_2008_film">> (2008 film)
+* I<A<Medium|href="^series base^?series=Medium">> Q<Method to His Madness>
+* I<A<Heroes|href="^series base^?series=Heroes">> Q<Collision>
+* I<A<Knight Rider|href="^series base^?series=Knight+Rider#Knight_Rider_2008_film">> (2008 film)
 3 Nozz-a-la Cola
 B<Nozz-a-la Cola> is a fake cola company first appearing in Stephen King's Dark Tower world. From all the descriptions I have found, it appears Nozz-a-la Cola's design is like the design of Coca-Cola. The advertisement on the balloon on I<Lost> looks nothing like it. The Dark Tower world will remain safely discrete for now.
 3 Sergio Georgini
@@ -46,21 +49,21 @@ B<Sergio Georgini> is a fake fashon company appearing first on I<The Office> (U.
 3 Slusho!
 B<Slusho!> is a fake frozen drink appearing first on and popularized by I<Alias> Q<It Begins>. The logo seen on I<Alias> is different from the logos in all other appearances, however, the logos in future appearances are the same. The logo rule is being suspended for this company since all appearances except I<Heroes> were produced by J.J. Abrams.
 * I<Alias> Q<It Begins>
-* I<A<Heroes|href="../../Movies_by_series.pl?series=Heroes">> Q<Cautionary Tales> and Q<Truth or Consequences>
+* I<A<Heroes|href="^series base^?series=Heroes">> Q<Cautionary Tales> and Q<Truth or Consequences>
 * I<Cloverfield>
-* I<A<Fringe|href="../../Movies_by_series.pl?series=Fringe">> Q<The Road Not Taken> and Q<A Better Human Being>
-* I<A<Star Trek|href="../../Movies_by_series.pl?series=Star+Trek#Star_Trek_2009">> (2009)
+* I<A<Fringe|href="^series base^?series=Fringe">> Q<The Road Not Taken> and Q<A Better Human Being>
+* I<A<Star Trek|href="^series base^?series=Star+Trek#Star_Trek_2009">> (2009)
 * I<Super 8>
 3 Trans Global Airlines
 B<Trans Global Airlines> is a fictional airline first appearing in I<Airport>. As of now, I do not have episode information or screen shots so can not confirm any of these as crossovers.
-* I<Airport>
+* I<Airport> spun off I<Airport 1975>, I<Airport '77>, and I<The Concorde... Airport '79>. All of these films may have used Trans Global Airlines.
+* I<Emergency> Q<The Stewardess> used stock footage from I<Airport 1975> so does not cross over with anything. SMALL<(A<Drag a big net|href="Westphall_non-crossovers.pl#Drag_a_big_net">)>
+I am still working on the other connections.
 * I<All My Children>
-* I<Emergency> Q<The Girl on the Balance Beam> SMALL<(A<Drag a big net|href="Westphall_non-crossovers.pl#Drag_a_big_net">)>
 * I<Quincey M.E.>
-* I<Matlock> SMALL<(A<Lucy's Diagnosis|href="Westphall_crossovers.pl#Lucys_Diagnosis">)>
+* I<Matlock> SMALL<(A<Lucy's Diagnosis|href="../Westphall_crossovers.pl#Lucys_Diagnosis">)>
 * I<The Young and the Resless>
 * I<Miami Vice>
-I am still working on the other connections.
 3 Weyland-Yutani
 B<Weyland-Yutani> is a fake mulinational conglomerate appearing first in I<Aliens>. Joss Whedon included this company in his other works.  (I am still waiting to see if Weyland-Yutani appears in the Marvel Cinematic Universe.)
 * I<A<Alien|href="../Movies_by_series.pl?series=Alien">>

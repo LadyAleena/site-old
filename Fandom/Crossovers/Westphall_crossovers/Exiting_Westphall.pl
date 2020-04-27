aa6087a8017f1ce5cf10631d5f16f5ea6da7c8ec
LadyAleena <LadyAleena@users.noreply.github.com>
2020-04-27 07:47:42 -0400

    Update and rename Movies/Crossovers/Westphall_non-crossovers.pl to Fandom/Crossovers/Westphall_crossovers/Exiting_Westphall.pl

---
+++ Fandom/Crossovers/Westphall_crossovers/Exiting_Westphall.pl
@@ -0,0 +1,120 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => qw( all );
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use lib '../../../files/lib';
+use Base::Page qw(page story);
+use Util::Crossover::LineMagic qw(crossover_magic);
+
+my $magic = crossover_magic( big => ['McHale UNCLE Smart'], dots => '../../..' );
+$magic->{'series base'} = '../../../Movies/Movies_by_series.pl';
+
+page( 'code' => sub { story(*DATA, { 'doc magic' => $magic, 'line magic' => $magic }) });
+
+__DATA__
+All of these crossovers are part of the Tommy Westphall Universe. Some crossover enthusiasts have created crossovers where it does not make much sense. Here are those I consider non-crosses, however there may still be crossovers in these non-crosses.
+>0 STRONG<Notes:>
+*| note two
+* A &#9660; goes to another section on this page.
+* A &#9650; goes to the top page and a specific rule.
+* A &#9664; goes to another crossover page.
+* A &#9654; goes to a page off of this site.
+2 Back to Westphall
+I<Blossom> in the A<All in the Families|href="Westphall_crossovers.pl#All_in_the_Families"> continuity had a A<small crossover|href="Westphall_crossovers.pl#Father_Guido_Sarducci"> that put it back into A<Westphall|href="Westphall_crossovers.pl">.
+I<NCIS> in the A<JAG and NCIS|href="Departing_from_Westphall.pl#JAG_and_NCIS"> continuity had a A<small crossover|href="Departing_from_Westphall.pl#Red_box_Morley"> that put it back into A<Westphall|href="Westphall_crossovers.pl">.
+2 Westerns in Crisis
+A<B<The Westerns> and the B<I<Arrowverse>>|href="Westerns_in_Crisis.pl"> came together through a surprising link. Both are still disconnected from Westphall, but who knows for how much longer?
+2 Drag a big net
+& Dragnet
+by NBC and ABC
+I<Dragnet> spun off a film of the same name in 1954, I<Dragnet 1967>, I<Dragnet 1966>, I<Dragnet> (1987), I<The New Dragnet>, and I<Dragnet> (2003) that was renamed I<L.A. Dragnet>.
+The character Jim Reed from I<Dragnet 1967> Q<The Phony Police Racket> and Q<The Big Search> went on to appear as a regular on I<Adam-12>. Once on I<Adam-12>, he and Pete Malloy appeared on I<Dragnet 1967> Q<Internal Affairs: DR-20> and I<Emergency!> Q<Emergency!>. I<Adam-12> Q<The Radical> concludes on I<The D.A.> Q<People vs. Saydo>. I<Adam-12> spun off I<The New Adam-12>.
+I<The D.A.> is a spin off of two television films: I<The D.A.: Murder One> and I<The D.A.: Conspiracy to Kill>.
+John Gage and Roy DeSoto from I<Emergency!> appeared on I<Adam-12> Q<Lost and Found> and I<Sierra> Q<The Urban Rangers>. I<Emergency!> spun off six television films. I<Emergency!> Q<The Stewardess> used stock footage of A<Trans Global Airlines|href="Big_fake_companies#Trans_Global_Airlines"> from I<Airport 1975>.
+Either station or squad 51 from I<Emergency!> appeared on I<CHiPs> Q<Cry Wolf>, Q<MAIT Team>, and Q<Hot Wheels>. I<CHiPs> spun off the contiguous I<CHiPs '99> and non-contiguous I<CHiPs> (2017).
+This continuity is ^TH-11^ with a few additions.
+2 McHale's U.N.C.L.E. Gets Smart
+& McHale UNCLE Smart
+from ABC, NBC, CBS, and FOX
+Guido Panzini first appeared on I<McHale's Navy> Q<McHale's Country Club Caper>. He reappeared on I<^The Man from U.N.C.L.E. (1964)^> Q<The Bow-Wow Affair>, then on I<One Day at a Time> Q<Panzini>.
+I<McHale's Navy> spun off I<Broadside> and had two theatrical films with the main cast and one theatrical film with Quinton McHale's son Quinton McHale, Jr.
+I<The Man from U.N.C.L.E.> spun off I<^The Girl from U.N.C.L.E.^> There were eight theatrical films released that were made using episodes from the series. The last theatrical film I<^The Return of the Man from U.N.C.L.E.^> was new.
+I<The Man from U.N.C.L.E.> characters Napoleon Solo and Illya Kuryakin appear on I<Please Don't Eat the Daisies> Q<Say UNCLE>. I<The Girl from U.N.C.L.E.> character April Dancer appeared on I<Please Don't Eat the Daisies> Q<Remember Lake Serene?>
+I<^Get Smart, Again!^>, the second film sequel to I<^Get Smart (1965)^>, mentioned T.H.R.U.S.H., the evil organization from I<The Man from U.N.C.L.E.> I<The Nude Bomb> is the first film sequel to I<Get Smart>. I<Get Smart> also spun off another television series with the same name in 1995. A<&#9660;|href="#Im_not_home" title="I&#700;m Not Home!">
+I<Get Smart> spun off the non-contiguous 2008 I<^Get Smart (2008)^> film. The film and its spin off are not within the same continuity.
+I<The Man from U.N.C.L.E.> spun off the non-contiguous 2015 I<The Man from U.N.C.L.E.> film.
+I<One Day at a Time> spun off the non-contiguous 2017 I<One Day at a Time> television series.
+This continuity is ^TH-28^ with a few additions.
+2 No News for the Detectives
+A A<crossover|href="http://tommywestphall.livejournal.com/25863.html"> was attempted between I<Columbo>, I<^Wonder Woman (1975)^>, I<^Matt Houston^>, I<^Remington Steele^>, I<^Moonlighting^>, I<City of Angels>, I<Burke's Law> (1994), and I<24> A<&#9664;|href="Departing_from_Westphall.pl#twenty-four"> for allegedly using the I<Los Angeles Tribune>, the fictional newspaper from I<Lou Grant>. With the amount of networks involved and no discernible connections, I do not see this as a crossover.
+3 Mary Tyler Moore
+& Mary Tyler Moore
+from CBS
+I<The Mary Tyler Moore Show> spun off four series: I<Rhoda>, I<Phyllis>, I<Lou Grant>, and I<Mary and Rhoda>.A<&#9660;|href="#Im_not_home" title="I&#700;m Not Home!">
+This continuity is ^TH-13^ with one addition.
+3 Columbo
+& Columbo
+from NBC and ABC
+I<Columbo> spun off the non-contiguous I<Mrs. Columbo>. I<Mrs. Columbo> went through three name changes: I<Kate Columbo>, I<Kate the Detective>, and I<Kate Loves a Mystery>.
+3 Burke's Law
+& Burkes Law
+from ABC and CBS
+I<Burke's Law> (1963) was renamed I<Amos Burke, Secret Agent> in season three. It spun off I<Burke's Law> (1994).
+The titular character of I<Honey West> appeared on I<Burke's Law> (1963) Q<Who Killed the Jackpot?> and I<Burke's Law> (1994) Q<Who Killed Nick Hazard?>.
+This continuity is ^TH-7^.
+3 Moonlighting
+from ABC
+Max from I<^Hart to Hart^> appeared on I<^Moonlighting^> Q<It's a Wonderful Job>. In the next I<Moonlighting> episode, Q<The Straight Poop>, Pierce Brosnan from I<Remington Steele> appeared as A<himself|href="index.pl#People_not_considered">.
+2 Hudson University
+Some series have been linked from a mention of the fictional A<Hudson University|href="../Big_fake_companies#Hudson_University">. Hudson University is too generic to be considered for a crossover. However, if any of these series have the same logo for Hudson University on something like stationary, I would consider it a crossover. The following series do not cross because of Hudson University.
+* I<Murder, She Wrote> A<&#9664;|href="Departing_from_Westphall.pl#Original_series_crossovers" title"Magnum P.I.">
+* I<Degrassi: The Next Generation> Q<Broken Wings>
+* I<Tru Calling>
+* I<The Secret Life of the American Teenager>
+* I<Blue Bloods>
+* I<Unforgettable>
+* I<Beauty &amp; the Beast>
+* I<Arrow> A<&#9664;|href="../Westerns_in_Crisis.pl#Arrowverse" title="Arrowverse">
+* I<The Mysteries of Laura>
+* I<Blindspot>
+* All the following, while not contiguous with the above, are contiguous with each other in A<Westphall|href="Westphall_crossovers.pl"> through other connections.
+** I<The Cosby Show> A<&#9664;|href="Arriving_to_Westphall.pl#Happy_Days" title="Happy Days">
+** I<Law &amp; Order> A<&#9664;|href="Departing_from_Westphall.pl#Law_and_Order" title="Law &amp; Order">
+** I<Without a Trace> A<&#9664;|href="Departing_from_Westphall.pl#CSI" title="CSI">
+** I<Castle> A<&#9664;|href="Departing_from_Westphall.pl#Fringe" title="Fringe">
+2 I'm not home!
+Many series have been linked because of the series I<Hi Honey, I'm Home!>. I<Hi Honey, I'm Home!> takes the characters out of their original continuities and places them into another. Since they are outside their original continuity, they are not the same characters. So the following do I<not> cross with each other or anything above.
+* I<The Donna Reed Show> or I<Dennis The Menace> though they did cross.
+* I<The Many Loves of Dobie Gillis>
+* I<Hazel>
+* I<The Munsters> and its spin-offs
+* I<My Mother the Car>
+* I<The Flying Nun>
+* I<Get Smart> A<&#9660;|href="#McHales_U.N.C.L.E._Gets_Smart" title="McHale's U.N.C.L.E. Gets Smart">
+* I<The Mary Tyler Moore Show> A<&#9660;|href="#Mary_Tyler_Moore" title="Mary Tyler Moore">
+* All the following, while not contiguous with the above, are contiguous with each other in A<Westphall|href="Westphall_crossovers.pl"> through other connections.
+** I<The Dick Van Dyke Show> and I<Gomer Pyle, U.S.M.C.> A<&#9664;|href="Westphall_crossovers.pl#Lucy_Makes_Room_for_Daddy" title="Lucy Makes Room for Daddy">
+** I<The Honeymooners> and I<The Lucy Show> A<&#9664;|href="Westphall_crossovers.pl#Lucys_Diagnosis" title="Lucy&#700;s Diagnosis">
+** I<Leave it to Beaver> A<&#9664;|href="Westphall_crossovers.pl#Leave_It_to_Beaver_to_cross_over" title="Leave It to Beaver to cross over">
+** I<Mister Ed> and I<The Brady Bunch> A<&#9664;|href="Westphall_crossovers.pl#Hooterville" title="Hooterville">
+** I<Hello, Larry> A<&#9664;|href="Westphall_crossovers.pl#All_in_the_Families" title="All in the Families">
+** I<The Cosby Show> A<&#9664;|href="Westphall_crossovers.pl#Happy_Days" title="Happy Days">
+2 Real people
+I<^Adventures of Superman^> did not cross with I<I Love Lucy>  Q<Lucy and Superman> when George Reeves appeared. He was playing himself, though for the children in the audience, it was made to appear as if he were playing Superman.
+3 Two and a Half, Bang!
+I<Two and a Half Men> Q<Fish in a Drawer> did not crossover with I<CSI: Crime Scene Investigation>.A<&#9664;|href="Departing_from_Westphall.pl#CSI" title="CSI"> I<CSI: Crime Scene Investigation> Q<Two and a Half Deaths> did not crossover with I<Two and a Half Men> because Jon Cryer, Charlie Sheen, and Angus Jones appeared as themselves.A<&#9650;|href="../index.pl#People_not_considered" title="people not considered">
+& Two Half Bang
+Dharma and Gregory Montgomery from I<Dharma & Greg> appeared in I<Two and a Half Men> Q<Nice to Meet You, Walden Schmidt>.
+In I<Two and a Half Men> Q<The Salmon Under My Sweater>, Charlie and Jake collaborate on the theme song to the fictional anime I<Oshikuru: Demon Samuri>. Leonard, Sheldon, and Penny from I<^The Big Bang Theory^> watch the show in Q<The Gothowitz Deviation>.
+I<The Big Bang Theory> spun off I<Young Sheldon>.
+2 Other
+A crossover between I<The Dick Van Dyke Show> and I<Richard Diamond, Private Detective> was attempted with Sam played by Mary Tyler Moore on I<Richard Diamond>. Mary Tyler Moore played Laura Petrie on I<The Dick Van Dyke Show>.
+A crossover between I<^St. Elsewhere^> and I<Julia> was attempted, however, the Dr. Morton Chegley characters were played by two different actors who have a nearly fifty year age difference.
+3 <i>ALF</i> does not crossover
+I<The Bob Newhart Show> did not cross with I<ALF> Q<Going Out of My Head Over You> since Alf mentioned I<The Bob Newhart Show>. I<ALF> did not cross with I<Mr. Robot> Q<eps2.4_m4ster-s1ave.aes> since Alf appeared in a fantasy or dream sequence.
+2 Key
+& key
+* Made for television films will have the color of the network.
+* Series with two or more colors were on multiple networks. The main color is the series last network.

60ae6e58f970d46bebb4cb578019bc86a6086dfe
LadyAleena <LadyAleena@users.noreply.github.com>
2020-05-15 03:34:27 -0400

    Update index.pl

--- Movies/index.pl
+++ Movies/index.pl
@@ -1,37 +1,261 @@
 #!/usr/bin/perl
-#This is the Movies index.
 use strict;
 use warnings FATAL => qw( all );
 
+use CGI::Minimal;
 use CGI::Carp qw(fatalsToBrowser);
+use Encode;
+use HTML::Entities qw(encode_entities);
+use Lingua::EN::Inflect qw(PL_N);
 
 use lib '../files/lib';
-use Base::Page qw(page story);
-use Base::LineMagic qw($line_magic);
+use Base::Page      qw(page story);
+use Fancy::Join     qw(grammatical_join);
+use HTML::Elements  qw(section nav div paragraph list form fieldset selection details input anchor);
+use Util::Movie     qw(movie genre movie_option display_option textify_movie
+                       start_year end_year
+                       display_movie display_simple_movie
+                       print_series print_movie);
+use Util::Columns;
+use Util::Convert   qw(idify);
+use Util::Data      qw(file_directory alpha_hash alpha_array first_alpha);
+use Util::Menu      qw(alpha_menu file_menu);
+use Util::Sort      qw(article_sort);
 
-page( 'code' => sub { story(*DATA, { 'line magic' => $line_magic }) });
+my ($sec,$min,$hour,$mday,$mon,$ltyear,$wday,$yday,$isdst) = localtime();
+my $current_year = ($ltyear + 1900);
 
-__DATA__
-Welcome to Lady Aleena's movie lists.
-2 What is a movie?
-A movie is any motion picture no matter its length. A film is a movie that is normally 88 to 120 minutes long (without commercials). A miniseries is a movie shown in two or more parts with each part as long as a film, though not always. A television series is an episodic movie with each episode being under 50 minutes in length (again without commercials). A commercial is a movie that advertises a product or service. There are plenty of other movie forms such as music videos, webisodes, full web series (which I include in television series for now), and more. So if the picture is in motion on your screen, it is a movie.
-2 Movies by alpha
-B<A<Movies by alpha|href="Movies_by_alpha.pl">> started as simply a list of my or my fiancé's favorite movies. Over time I have been adding movies that were on various lists of movies I have collated over the years. At one time I collected as many movies with A<disasters|href="Movies_by_genre.pl?genre=disaster"> and A<monsters|href="Movies_by_genre.pl?genre=monster">, so I added all of them to my list. I also collated A<movie series|href="Movies_by_series.pl">, my favorites at first. I then became interested in all of the horror series I could find, so I added a lot of horror movies to my list so I could present the series. I do not even like A<horror movies|href="#Horror_movies"> much.
-So now my or my fiancé's favorite movies are mixed in with others I have collected. You can see what we really like by what I will admit we A<own|href="../Collections/Movies.pl">.
-You can get more information about the movies from ^Wikipedia^, ^Allmovie^, ^IMDb^, and ^TV.com^ where noted.
-2 Movies by genre
-B<A<Movies by genre|href="Movies_by_genre.pl">> started off as lists of disaster and monster movies and movies about teachers and coaches but has since been expanded to all genres and themes within the genres.
-3 Family and children's movies
-I do not consider family or children a genre so movies that would be categorized as such elsewhere are not listed in those categories here. Those films will be mixed in with more adult movies.
-3 Horror movies
-You will see a lot of horror movies and series on this list. I like to read the stories in horror movies but do not watch many. My fiancé has watched more horror movies than I could handle. I prefer to just read the story lines especially in series of horror movies. You can read more about horror and monster movies and me A<here|href="Movie_opinions.pl#The_horror_genre_and_me">.
-3 Monster movies
-I have created a genre for monster movies. Monsters are in so many speculative fiction genres, it is hard to say a monster is purely horror or fantasy or science fiction, with some monsters as super heroes. The monster genre is a mix of any paranormal or supernatural creatures from angels to zombies.
-3 Reality television series
-I do not like most of the reality television series today. My fiancé does, so I have added those he likes to my lists here. In my opinion most reality series today should be considered soap operas. Some of them started as a special on one channel or another, and they should have stayed that way. A once-off special of some of these subjects would interest me, however, a series of the same thing week after week gets old quickly especially if the series strays from the topic and focuses more on the people and their antics.
-2 Movies by series
-B<A<Movies by series|href="Movies_by_series.pl">> is a mix of film series and stand alone television series. Some series include films, miniseries, and television series.
-3 Night of the Living Dead
-I feel the need to make a special note about the I<Night of the Living Dead>. With the original I<Night of the Living Dead> being public domain, many have tried to connect their films with it. Using the work of others, I have attempted to just make lists of the various series. Only three series are connected to the original I<Night of the Living Dead>. I collated my I<Night of the Living Dead> from the following sources: "A<Genealogy of the Living Dead: UPDATED!|href="http://horror-movies.livejournal.com/2106870.html" target="ex_tab">", "A<The Zombification Family Tree: Legacy of the Living Dead|href="http://www.popmatters.com/column/159439-legacy-of-the-living-dead/" target="ex_tab">", and "A<Zombi series|href="http://en.wikipedia.org/wiki/Zombi_series" target="ex_tab">" on Wikipedia.
-3 Remakes
-If a film is a remake of a film in a series, it is not added to the series since it does not share continuity. A remake will be in a series if the remake has its own sequel or prequel. I may one day add lists of whole franchises I am interested in in the future.
+sub get_cgi_param {
+  my ($cgi, $param) = @_;
+  my $ret = $cgi->param($param);
+  return ($ret ? encode_entities($ret, '<>"') : '');
+}
+
+sub get_genre {
+  my ($genre_type, $opt) = @_;
+  my $text = $genre_type =~ /(?<!fantas)y|show|dram|musical|police procedural|spoof|slasher|western/ ? PL_N($genre_type) :
+             $opt->{'movie'} && $opt->{'movie'} =~ /^yt1/ ? "$genre_type movies" :
+             $genre_type;
+  return $text;
+}
+
+sub get_media {
+  my $media_type = shift;
+  my $text = $media_type eq 'film'    ? 'films' :
+             $media_type eq 'tv'      ? 'television series' :
+             $media_type eq 'tv film' ? 'television films' :
+             $media_type;
+  return $text;
+}
+
+sub movies_beginning {
+  my $first = shift;
+  my $text = $first eq '#' ? 'a number'    :
+             $first eq '!' ? 'punctuation' :
+             $first =~ /^\p{Alpha}/i ? uc($first) :
+             undef;
+  return "List of movies beginning with $text" if $text;
+}
+
+my $cgi = CGI::Minimal->new;
+my $title    = get_cgi_param($cgi, 'title');
+my $year     = get_cgi_param($cgi, 'year');
+my $media    = get_cgi_param($cgi, 'media');
+my $genre    = get_cgi_param($cgi, 'genre');
+my $source   = get_cgi_param($cgi, 'source');
+my $alpha    = get_cgi_param($cgi, 'alpha');
+my $series   = get_cgi_param($cgi, 'series');
+my $opinions = get_cgi_param($cgi, 'opinions');
+my $genre_list = get_cgi_param($cgi, 'genre list');
+
+my @selects = qw(genre media source);
+my %select_options = map { $_ => movie_option($_) } @selects;
+
+my $movies_data = movie('data');
+my $search;
+my @movies;
+if ( $title || $year || $genre || $media || $source || $alpha ) {
+  my @selections;
+  push @selections, $title  if $title;
+  push @selections, $year   if $year;
+  push @selections, get_genre($genre) if $genre;
+  push @selections, get_media($media) if $media;
+  push @selections, PL_N($source) if $source;
+
+  my $selection = @selections ? grammatical_join(' ', map("<b>$_</b>", @selections)) : '';
+  my $selection_text = $selection;
+
+  for my $movie (values %$movies_data) {
+    my $item = $movie->{'title'};
+
+    if ($year) {
+      my $start_year = start_year($movie);
+      my $end_year   = end_year($movie);
+
+      if ($start_year !~ /\d/ || $end_year !~ /\d/) {
+        if ($start_year ne $year || $end_year ne $year) {
+          next;
+        }
+      }
+      else {
+        if ($year < $start_year || $year > $end_year) {
+          next;
+        };
+      }
+    }
+    next if $media  && $movie->{'media'} ne $media;
+    next if $genre  && !grep($genre =~ /$_/i , @{$movie->{'genre'}});
+    next if $source && (!$movie->{'source'} || $movie->{'source'} !~ /\b$source(?!ing)/i);
+    next if $title  && lc($movie->{'title'}) !~ /\Q$title\E/i;
+    next if $alpha  && first_alpha($movie->{'title'}) ne $alpha;
+
+    push @movies, $movie->{'title'};
+  }
+
+  $search = @movies > 0 ? "You searched for $selection_text in my collection." : "No matches found for $selection_text.";
+}
+else {
+  @movies = keys %$movies_data;
+}
+
+my $alpha_movies = alpha_array(\@movies);
+for my $movies (keys %$alpha_movies) {
+  my @movies = map(display_movie($movies_data->{$_}, { 'series' => 1, 'links' => 1, 'crossover' => 1 }), sort { article_sort(lc $a, lc $b) } @{$alpha_movies->{$movies}});
+  @{$alpha_movies->{$movies}} = @movies;
+}
+my $alpha_menu = scalar(@movies) > 10 ?
+                 alpha_menu($alpha_movies, { 'addition' => anchor('Search the list', { href => '#Search' }), 'join' => ' | ' }) :
+                 undef;
+
+my $browse_alpha = alpha_hash($movies_data);
+my $browse_alpha_menu = alpha_menu($browse_alpha, { 'param' => 'alpha', 'join' => ' | ', 'addition' => anchor("&nbsp;Series list&nbsp;", { href => '?series=list' })  });
+
+my $open_directory = file_directory('Movies', 'data');
+
+my $series_select  = movie_option('series');
+my @series_selects = keys %$series_select;
+my $series_alpha_movies = alpha_array(\@series_selects);
+my $series_alpha_menu   = alpha_menu($series_alpha_movies, { 'join' => ' | ' });
+
+my $genre_select    = genre('data');
+my $genre_link_list = [
+  map  { anchor(ucfirst $_, { 'href' => "index.pl?genre+list=$_" }); } (sort keys %$genre_select)
+];
+
+my $magic;
+$magic->{'search form'} = sub {
+  my $tab = 4;
+  section($tab, sub {
+    $tab++;
+    form($tab, sub {
+      $tab++;
+      fieldset($tab, sub {
+        $tab++;
+        input($tab, { 'type' => 'text',   'name' => 'title', 'placeholder' => 'Title', 'style' => 'width: 19em' });
+        input($tab, { 'type' => 'number', 'name' => 'year',  'placeholder' => 'Year',  'style' => 'width: 5em', 'min' => "1911", 'max' => $current_year + 1 });
+        for my $select (@selects) {
+          my @options = ([ucfirst($select), { 'value' => '' }], map(display_option($select, $_), @{$select_options{$select}}));
+          selection($tab, \@options, { 'name' => $select, 'style' => 'width: 10em' });
+        }
+        input($tab, { 'type' => 'submit', 'value' => 'List movies' });
+        input($tab, { 'type' => 'button', 'value' => 'Start over', 'onclick' => "location='index.pl'" });
+        nav(6, "Browse movies: $browse_alpha_menu", { 'class' => 'alpha_menu' });
+      }, { 'legend' => 'Search my collection' });
+    }, { 'action' => 'index.pl', 'method' => 'get', 'id' => 'movie_search_form' });
+  }, { 'id' => 'movie_search' });
+};
+
+$magic->{'genre link list'} = sub {
+  details(6, sub {
+    list(5, 'u', $genre_link_list, { 'class' => 'four' });
+  }, { 'summary' => 'List of genres' });
+};
+
+
+my $heading = length $series && $series_select->{$series}  ? textify_movie($series) :
+              length $series && !$series_select->{$series} ? 'Movie series' :
+              length $genre_list && $genre_select->{$genre_list}  ? ucfirst "$genre_list movies" :
+              length $genre_list && !$genre_select->{$genre_list} ? 'Movie genres' :
+              length $alpha    ? movies_beginning(decode('UTF-8',$alpha)) :
+              length $opinions ? 'Movie opinions' :
+              q(Lady Aleena's movie lists);
+
+page( 'heading' => $heading, 'code' => sub {
+  if ( $search || $alpha ) {
+    section(3, sub {
+      paragraph(4, 'This is a list of films and television series I have collected that either interest my fiancé or me or helps populate other pages. You can browse the list or search it.');
+      &{$magic->{'search form'}};
+      if ( $title || $year || $media || $genre || $source ) {
+        paragraph(4, $search) if $search;
+      }
+    });
+    for my $alpha_movie (sort keys %$alpha_movies) {
+      my $heading = keys %$alpha_movies > 1 ? [2, $alpha_movie, { id => "section_$alpha_movie" }] : undef;
+      section(3, sub {
+        list(5, 'u', $alpha_movies->{$alpha_movie}, { 'class' => 'movie_list' });
+        nav(5, $alpha_menu, { 'class' => 'alpha_menu' }) if $alpha_menu;
+      }, { 'heading' => $heading });
+    }
+  }
+  elsif ( length $series ) {
+    if ($series && $series_select->{$series}) {
+      if ($series_select->{$series} eq 'series') {
+        print_series(4, 1, $series);
+      }
+      else {
+        print_movie(3, 1, $series);
+      }
+    }
+    else {
+      for my $series_alpha (sort keys %$series_alpha_movies) {
+        my $class = number_of_columns(3, scalar @{$series_alpha_movies->{$series_alpha}}, 1);
+        section(3, sub {
+          list(5, 'u', file_menu('series', [sort { article_sort($a, $b) } @{$series_alpha_movies->{$series_alpha}}]), { 'class' => $class, 'style' => 'font-size:smaller' });
+          nav(5, "$series_alpha_menu", { 'class' => 'alpha_menu' });
+        }, { 'heading' => [2, $series_alpha, { 'id' => "section_$series_alpha" }] });
+      }
+    }
+  }
+  elsif ( length $genre_list ) {
+    if ($genre_select->{$genre_list}) {
+      section(3, sub {
+        my @subgenres = (sort grep(!/main/, keys %{$genre_select->{$genre_list}}));
+        unshift @subgenres, 'main' if $genre_select->{$genre_list}{'main'};
+
+        for my $subgenre (@subgenres) {
+          my @movies = map { display_simple_movie(movie($_)) } sort { article_sort(lc $a, lc $b) } @{$genre_select->{$genre_list}{$subgenre}};
+          my $class = scalar(@movies) > 72 ? 'three' : scalar(@movies) > 36 ? 'two' : 'one';
+          my $style = $class eq 'three' ? 'font-size:smaller' : undef;
+          section(4, sub {
+            my $genre = $subgenre ne 'main' ? $subgenre : $genre_list;
+            paragraph(6, "This is a list of $genre movies I have gathered.");
+            list(6, 'u', \@movies, { 'class' => $class, 'style' => $style });
+          }, { 'heading' => $subgenre ne 'main' ? [3, ucfirst $subgenre] : undef });
+        }
+      });
+      section(3, sub {
+        my $list = [
+          map  { anchor(ucfirst $_, { 'href' => "?genre+list=$_" }) }
+          sort
+          grep { !/$genre_list/ } keys %$genre_select
+        ];
+        list(5, 'u', $list, { 'class' => 'four' });
+      }, { 'heading' => [2, 'Other genres'] });
+    }
+    else {
+      my $genre_map = anchor('genre map', { href => '../Miscellany/Genre_map.pl' });
+      section(3, sub {
+        paragraph(6, "Click a genre to see a list of movies. There is a $genre_map to see how the genres are connected.");
+        list(5, 'u', $genre_link_list, { 'class' => 'four' });
+      });
+    }
+  }
+  elsif ( length $opinions && $opinions =~ /^[ryt1]/i ) {
+    open(my $fh, '<', "$open_directory/Opinions.txt") || die $!;
+    story($fh, { 'doc magic' => $magic, 'line magic' => $magic });
+  }
+  else {
+    open(my $fh, '<', "$open_directory/index.txt") || die $!;
+    story($fh, { 'doc magic' => $magic, 'line magic' => $magic });
+  }
+});

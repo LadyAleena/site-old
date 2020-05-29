dc44bc43bb9c2a7298e57709f3d0c686c66b8e42
LadyAleena <LadyAleena@users.noreply.github.com>
2020-05-29 13:56:25 -0400

    Update index.pl
    typo fix

--- Collections/index.pl
+++ Collections/index.pl
@@ -13,7 +13,7 @@ my $magic = collection_magic;
 page( 'code' => sub { story(*DATA, { 'line magic' => $magic }) });
 
 __DATA__
-Welcome to Lady Aleena's B<collections>, which is lists of A<novels|href="Fiction.pl">, A<books|href="Non-fiction.pl">, A<music|href="Music_and_comedy.pl">, A<movies|href="Movies.pl">, A<tie-ins|href="Tie-ins.pl">, and A<programs|href="Programs.pl"> I am willing to admit I own or use. The list of movies here is just those movies I own and should not to be confused with my more general interst in A<movies|href="../Movies">. Tie-ins are books and music connected to movies or television series. I also share my A<fandom|href="../Fandom"> elsewhere.
+Welcome to Lady Aleena's B<collections>, which is lists of A<novels|href="Fiction.pl">, A<books|href="Non-fiction.pl">, A<music|href="Music_and_comedy.pl">, A<movies|href="Movies.pl">, A<tie-ins|href="Tie-ins.pl">, and A<programs|href="Programs.pl"> I am willing to admit I own or use. The list of movies here is just those movies I own and should not to be confused with my more general interest in A<movies|href="../Movies">. Tie-ins are books and music connected to movies or television series. I also share my A<fandom|href="../Fandom"> elsewhere.
 Here is a key for the notations after each title with the exception of programs.
 *| media_types
 * Books

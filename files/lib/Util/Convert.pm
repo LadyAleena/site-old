543d053c7428026972f2d9c99712801af86e5b2d
LadyAleena <LadyAleena@users.noreply.github.com>
2020-06-24 15:00:26 -0400

    Update Convert.pm
    Reorganized subroutines to be in the order they are noted in the POD and added to the POD.

--- files/lib/Util/Convert.pm
+++ files/lib/Util/Convert.pm
@@ -2,14 +2,62 @@ package Util::Convert;
 use strict;
 use warnings;
 use Exporter qw(import);
-our @EXPORT_OK = qw(filify hashtagify idify linkify searchify textify);
+our @EXPORT_OK = qw(textify idify searchify filify hashtagify linkify);
 
 use Encode;
-use HTML::Entities qw(encode_entities);
-use URI::Encode qw(uri_encode);
 use Lingua::EN::Inflect qw(NUMWORDS);
 
-use Base::Path qw(base_path);
+use Util::Path qw(base_path);
+
+sub textify {
+  my ($text, $opt) = @_;
+  my $root_link = base_path('link');
+  $text = decode('UTF-8', $text) if ($opt->{'decode'} && $opt->{'decode'} =~ /^[ytk1]/);
+  $text =~ s/$root_link\///;
+  $text =~ s/_/ /g;
+  $text =~ s/\b(M[rsx]|Mrs|Dr|St|[SJ]r)\b(?!\.)/$1./g;
+  # The following is an more robust version of the preceding, but it is overkill.
+  # $text =~ s/(?<!Rev\s)\b(M[rsx]|Mrs|[FPBD]r|Rev|Ven|St|[SJ]r|Esq)\b(?!\.)/$1./g;
+  $text =~ s/\s&\s/ &amp; /g;
+  $text =~ s/\.{3}/&#8230;/g;
+  $text =~ s/(\w|\b|\s|^)'(\w|\b|\s|$)/$1&#700;$2/g;
+  $text =~ s/<.+?>//g      unless ($opt->{'html'}   && $opt->{'html'}   =~ /^[ytk1]/);
+  $text =~ s/\s\(.*?\)$//  unless ($opt->{'parens'} && $opt->{'parens'} =~ /^[ytk1]/);
+  $text =~ s/(.)\.\w{2,5}?$/$1/ unless $text =~ /\w\.(?:com|net|org)$/;
+#  $text =~ s/(?<!\A)((?<! )\p{uppercase})/ $1/g; # from Kenosis, kcott, and tye on PerlMonks
+  return encode('UTF-8', $text);
+}
+
+sub idify {
+  my @base = @_;
+  my @ids = map {
+    $_ =~ s/<.+?>//g;
+    $_ =~ s/^(\d+([stnrdh]{2}|))/NUMWORDS($1)/e;
+    $_ =~ s/(.)\.\w{2,5}?$/$1/;
+    $_ =~ s/&amp/and/g;
+    $_ =~ s/&/and/g;
+    $_ =~ s/Æ/Ae/g;
+    $_ =~ s/Ç/C/g;
+    $_ =~ s/Ü/U/g;
+    $_ =~ s/(è|é|ê)/e/g;
+    $_ =~ s/#/No/g;
+    $_ =~ s/ /_/g;
+    $_ =~ s/[^\w:.\-]//g;
+    $_;
+  } grep {defined($_)} @base;
+  return encode('UTF-8',join('_',@ids));
+}
+
+sub searchify {
+  my ($search, $section) = @_;
+  $search =~ s/<.+?>//g;
+  $search =~ s/(?:_|\s|%20)/+/g;
+  $search =~ s/^#/%23/;
+  $search =~ s/&/%26/;
+  $search =~ s/(.)\.\w{2,5}?$/$1/;
+  $search .= '#'.idify(@$section) if $section;
+  return $search;
+}
 
 sub filify {
   my ($filename) = @_;
@@ -21,62 +69,20 @@ sub filify {
 
 sub hashtagify {
   my ($hashtag) = @_;
+  $hashtag =~ s/<.+?>//g;
+  $hashtag =~ s/(.)\.\w{2,5}?$/$1/;
+  $hashtag =~ s/&amp/and/g;
   $hashtag =~ s/&/and/g;
   $hashtag =~ s/\W//g;
   $hashtag =~ s/^/#/;
   return $hashtag;
 }
 
-sub idify {
-  my @base = @_;
-  my @ids = map {
-    $_ =~ s/<.+?>//g;
-    $_ =~ s/^(\d+([stnrdh]{2}|))/NUMWORDS($1)/e;
-    $_ =~ s/\.\w{2,5}?$//;
-    $_ =~ s/&amp/and/g;
-    $_ =~ s/&/and/g;
-    $_ =~ s/Æ/Ae/g;
-    $_ =~ s/Ç/C/g;
-    $_ =~ s/Ü/U/g;
-    $_ =~ s/(é|ê)/e/g;
-    $_ =~ s/#/No/g;
-    $_ =~ s/ /_/g;
-    $_ =~ s/[^\w:.\-]//g;
-    $_;
-  } grep {defined($_)} @base;
-  return encode('UTF-8',join('_',@ids));
-}
-
 sub linkify {
   my ($link) = @_;
-#  $link =~ s/&/%26/g;
-   return $link;
-}
-
-sub searchify {
-  my ($text, $section) = @_;
-  my $search = $text;
-  $search =~ s/(?:_|\s|%20)/+/g;
-  $search =~ s/^#/%23/;
-  $search =~ s/&/%26/;
-  $search .= '#'.idify(@$section) if $section;
-  return $search;
-}
-
-sub textify {
-  my ($text, $opt) = @_;
-  my $root_link = base_path('link');
-  $text =~ s/$root_link\///;
-  $text =~ s/_/ /g;
-  $text =~ s/(?<!Rev\s)\b([DMSJ][rsx]|Mrs|Rev)\b(?!\.)/$1./g;
-  $text =~ s/\s&\s/ &amp; /g;
-  $text =~ s/\.{3}/&#8230;/g;
-  $text =~ s/(\w|\b|\s|^)'(\w|\b|\s|$)/$1&#700;$2/g;
-  $text =~ s/<.+?>//g      unless ($opt->{'html'}   && $opt->{'html'}   =~ /^[ytk1]/);
-  $text =~ s/\s\(.*?\)$//  unless ($opt->{'parens'} && $opt->{'parens'} =~ /^[ytk1]/);
-  $text =~ s/\.\w{2,5}?$// unless $text =~ /\.(?:com|net|org)$/;
-#  $text =~ s/(?<!\A)((?<! )\p{uppercase})/ $1/g; # from Kenosis, kcott, and tye on PerlMonks
-  return encode('UTF-8',$text);
+  $link =~ s/<.+?>//g;
+  $link =~ s/ /_/g;
+  return $link;
 }
 
 =head1 NAME
@@ -93,15 +99,20 @@ B<Util::Convert> converts strings into various formats.
   my $file    = filify($string);     # returns Mr_&_Mrs_Smith
   my $hashtag = hashtagify($string); # returns #MrandMrsSmith
 
+=head1 General notes
+
+All of the subroutines remove html tags from the string with C<textify> having the option to leave them in the string.
+They also strip off file extensions from the ends of strings with the exception of C<filify>.
+
 =head1 textify
 
-C<textify> has two parameters.
+C<textify> returns an HTML friendly UTF-8 encoded string and has two parameters.
 
 The first parameter is the text that you want to make into an HTML friendly string.
 
   textify('<i>This & That</i> (2020)') # returns This &amp; That
 
-The second paramter is a hash with two options.
+The second paramter is a hash with three options. The options can be set to 'yes', 'true', 'keep', or the number 1.
 
 If C<html> is specified, then HTML is not stripped out of the text string.
 
@@ -111,23 +122,40 @@ If C<parens> is specified, parenteses are not stripped out of the text string.
 
   textify('<i>This & That</i> (2020)',  { parens => 'yes' }) # returns "This &amp; That (2020)"
 
+If C<decode> is specified, the string will be UTF-8 decoded before it is reencoded to UTF-8.
+
+  textify('This & That', { decode => 'yes' })
+
 =head1 idify
 
-C<idify> accepts a list of paramters that will be joined together to make one id.
+C<idify> returns a string for use as an id. It accepts a list of paramters that will be joined together to make one id.
 
+  idify('This & That')  # returns This_and_That
   idify('This', 'That') # returns This_That
 
 =head1 searchify
 
-C<searchify> has two parameters.
+C<searchify> returns a string for use as a URL search parameter and has two parameters.
 
 The first paramenter is the text that you want to make into a search item.
 
-  searchify('This and That') # returns This+%26+That
+  searchify('This & That') # returns This+%26+That
 
-The second parameter is the optional section that is made into an id.
+The second parameter is the optional section that is made into an id. This parameter uses L</"idify">, so must be an array reference.
 
-  searchify('This and That', 'The other thing') # returns This+%26+That#The_other_thing
+  searchify('This & That', ['The other thing']) # returns This+%26+That#The_other_thing
+
+=head1 filify
+
+C<filify> returns a string to be used as a file name. All characters not allowed in file names are removed from the string.
+
+  filify('This & That') # returns This_&_That
+
+=head1 hashtagify
+
+C<hashtagify> returns a string that can be used as a hashtag.
+
+  hashtagify('This & That') # returns #ThisandThat
 
 =head1 AUTHOR
 
